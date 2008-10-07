d452cc572ba13d01c8a96f311a1db5f99e25caf7
Simo Sorce <idra@samba.org>
2008-10-07 10:01:38 -0400

    Forgot to add debug.c

---
+++ server/util/debug.c
@@ -0,0 +1,20 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int debug_level = 2;
+
+void debug_fn(const char *format, ...)
+{
+    va_list ap;
+    char *s = NULL;
+
+    va_start(ap, format);
+    vasprintf(&s, format, ap);
+    va_end(ap);
+
+    /*write(state.fd, s, strlen(s));*/
+    fprintf(stderr, s);
+    free(s);
+}
