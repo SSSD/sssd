77454c07ba109a3ea6af0da86ba954b28b1fd02f
Simo Sorce <ssorce@redhat.com>
2009-02-25 16:43:57 -0500

    Remove our copies of the samba libraries.
    Packages are already available in debian unstable and will soon land in Fedora.
    See BUILD.TXT for details.
    We still keep libreplace as we still use its configure macros, until we find
    time to extract only waht we need and have our own macros.

--- ldb/python.mk
+++
@@ -1,8 +0,0 @@
-[PYTHON::swig_ldb]
-LIBRARY_REALNAME = ldb.$(SHLIBEXT)
-PUBLIC_DEPENDENCIES = LIBLDB LIBEVENTS PYTALLOC
-
-swig_ldb_OBJ_FILES = $(ldbsrcdir)/pyldb.o
-$(swig_ldb_OBJ_FILES): CFLAGS+=-I$(ldbsrcdir)/include
-
-$(swig_ldb_OBJ_FILES): CFLAGS+=$(CFLAG_NO_CAST_QUAL)
