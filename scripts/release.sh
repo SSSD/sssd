ee2f522ebd4ded82f5ce46ff27a66a14cfce8274
Jakub Hrozek <jhrozek@redhat.com>
2009-05-21 12:31:58 +0200

    Fix release.sh
    Fix version extraction in release script
    Use make dist-gzip inside of git-archived directory

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-version=`grep "^AC_INIT" server/configure.ac | tr -d "AC_INIT(sssd, " | tr -d ")"`
-v_=`echo ${version} | tr "." "_"`
+function config()
+{
+    autoreconf -i -f || return $?
+    ./configure
+}
 
-git archive --format=tar --prefix=sssd-${version}/ sssd-${v_} | gzip > sssd-${version}.tar.gz
+SAVED_PWD=$PWD
+version=$(grep "^AC_INIT" configure.ac | grep -E -o \[[0-9.]+\] | tr -d [])
+tag=$(echo ${version} | tr "." "_")
+
+trap "cd $SAVED_PWD; rm -rf sssd-${version} sssd-${version}.tar" EXIT
+
+git archive --format=tar --prefix=sssd-${version}/ sssd-${tag} > sssd-${version}.tar
+if [ $? -ne 0 ]; then
+    echo "Cannot perform git-archive, check if tag $tag is present in git tree"
+    exit 1
+fi
+tar xf sssd-${version}.tar
+
+pushd sssd-${version}
+config || exit 1
+make dist-gzip || exit 1  # also builds docs
+popd
+
+mv sssd-${version}/sssd-${version}.tar.gz .
 gpg --detach-sign --armor sssd-${version}.tar.gz
 
