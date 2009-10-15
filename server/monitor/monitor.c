SSSD/sssd

commit 5e2301b8a75d10e5cbbe11e26e5192b894af6ad7

Temporarily disable automatic config file reread
The backends do not honor the reloadConfig SBUS message right now,
so if an admin changes the sssd.conf file, it will update only the
monitor, potentially leaving the SSSD as a whole in a bad state.

This patch will simply comment out monitor_config_file() for the
time being until https://fedorahosted.org/sssd/ticket/91 is fixed.

server/monitor/monitor.c
@@ -1871,12 +1871,18 @@ int monitor_process_init(struct mt_ctx *ctx,
     int ret, i;
     struct sss_domain_info *dom;
 
+#if 0
+    This feature is incomplete and can leave the SSSD in a bad state if the
+    config file is changed while the SSSD is running.
+
+    Uncomment this once the backends are honoring reloadConfig()
+
     /* Watch for changes to the confdb config file */
     ret = monitor_config_file(ctx, ctx, config_file, monitor_signal_reconf);
     if (ret != EOK) {
         return ret;
     }
-
+#endif
     /* Watch for changes to the DNS resolv.conf */
     ret = monitor_config_file(ctx, ctx, RESOLV_CONF_PATH,
                               monitor_update_resolv);

commit 93362b533abfc41ad4d8c3e91399549a545b9d51

Remove warnings caused by 5e2301b8a75d10e5cbbe11e26e5192b894af6ad7
There were unused functions still being compiled. This will
suppress them until we turn live configuration updates back on.

server/monitor/monitor.c
@@ -144,8 +144,11 @@ static int add_new_provider(struct mt_ctx *ctx, const char *name);
 
 static int mark_service_as_started(struct mt_svc *svc);
 
+#if 0
 static int monitor_signal_reconf(struct config_file_ctx *file_ctx,
                                  const char *filename);
+#endif
+
 static int update_monitor_config(struct mt_ctx *ctx);
 static int monitor_cleanup(void);
 
@@ -755,6 +758,10 @@ done:
     dbus_message_unref(reply);
 }
 
+#if 0
+This function should be re-enabled once live configuration updates
+are working properly.
+
 static int monitor_signal_reconf(struct config_file_ctx *file_ctx,
                                  const char *filename)
 {
@@ -772,6 +779,7 @@ static int monitor_signal_reconf(struct config_file_ctx *file_ctx,
     /* Update the monitor's configuration and signal children */
     return update_monitor_config(file_ctx->mt_ctx);
 }
+#endif
 
 static int service_signal_dns_reload(struct mt_svc *svc);
 static int monitor_update_resolv(struct config_file_ctx *file_ctx,