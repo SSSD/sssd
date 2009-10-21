f0d7e2a1cacdffcbc84e5ad2bdd04f405dcaeb65
Martin Nagy <mnagy@redhat.com>
2009-10-21 17:24:13 +0200

    Add a new set of helpful common functions for tests
    The leak_check_setup() and leak_check_teardown() functions can be added
    to a test case with tcase_add_checked_fixture(). They will make sure
    that all tests are checked for memory leaks. However, since talloc is
    hierarchical and automatically frees the children, this will not catch
    all cases, but might still be helpful.
    
    The check_leaks(ctx, bytes) function takes a talloc context as an
    argument and the number of bytes it should be using up (children
    included). The total byte size used up by the context is determined by
    the talloc_total_size() function. If the size doesn't agree,
    check_leaks() will print out a talloc report on the context and makes
    the current test fail.
    
    The check_leaks_push() and check_leaks_pop() both take a talloc context
    as an argument. Every time push is called, the context is "pushed" onto
    an internal stack and it's current size is noted. When the context is
    later "poped", the pop function will make sure that the size is the same
    as when it was pushed. It will also check that it's not called
    out-of-order or if the stack isn't empty.

---
+++ server/tests/common.h
@@ -0,0 +1,19 @@
+#ifndef __TESTS_COMMON_H__
+#define __TESTS_COMMON_H__
+
+#include <talloc.h>
+
+#define check_leaks(ctx, bytes) _check_leaks((ctx), (bytes), __location__)
+void _check_leaks(TALLOC_CTX *ctx,
+                  size_t bytes,
+                  const char *location);
+
+void check_leaks_push(TALLOC_CTX *ctx);
+
+#define check_leaks_pop(ctx) _check_leaks_pop((ctx), __location__)
+void _check_leaks_pop(TALLOC_CTX *ctx, const char *location);
+
+void leak_check_setup(void);
+void leak_check_teardown(void);
+
+#endif /* !__TESTS_COMMON_H__ */
