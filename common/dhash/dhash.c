8878997baaec88d344bd1cb9eabd261cfe4b532c
Stephen Gallagher <sgallagh@redhat.com>
2010-02-18 14:43:39 -0500

    Properly license libdhash

--- common/dhash/dhash.c
+++ common/dhash/dhash.c
@@ -1,3 +1,27 @@
+/*
+    Authors:
+        John Dennis <jdennis.redhat.com>
+        Esmond Pitt <ejp@ausmelb.oz.AU>
+
+    This implementation was based on a 3/7/1989 public domain submission
+    to comp.sources.misc by Esmond Pitt <ejp@ausmelb.oz.AU>.
+
+    Copyright (C) 2009 Red Hat
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /*****************************************************************************/
 /******************************** Documentation ******************************/
 /*****************************************************************************/
