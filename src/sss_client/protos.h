ba8937d83675c7d69808d1d3df8f823afdc5ce2a
Stephen Gallagher <sgallagh@redhat.com>
2010-02-18 14:13:54 -0500

    Fix licensing issues for sss_client

--- src/sss_client/protos.h
+++ src/sss_client/protos.h
@@ -1,12 +1,26 @@
 /*
- * System Security Services Daemon. NSS Interface
- *
- * Copyright (C) Simo Sorce 2007
- *
- * You can used this header file in any way you see fit provided copyright
- * notices are preserved.
- *
- */
+   SSSD
+
+   Client Interface for NSS and PAM.
+
+   Authors:
+        Simo Sorce <ssorce@redhat.com>
+
+   Copyright (C) Red Hat, Inc 2007
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #if 0
 /* SHADOW database NSS interface */
