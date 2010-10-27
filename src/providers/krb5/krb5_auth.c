595de25496d9f62cae66acfef77930a2e4ac78a2
Stephen Gallagher <sgallagh@redhat.com>
2010-10-27 15:39:37 -0400

    Fix incorrect free of req in krb5_auth.c

--- src/providers/krb5/krb5_auth.c
+++ src/providers/krb5/krb5_auth.c
@@ -870,7 +870,7 @@ static void krb5_resolve_kpasswd_done(struct tevent_req *subreq)
     int ret;
 
     ret = be_resolve_server_recv(subreq, &state->kr->kpasswd_srv);
-    talloc_zfree(req);
+    talloc_zfree(subreq);
     if (ret) {
         /* all kpasswd servers have been tried and none was found good, but the
          * kdc seems ok. Password changes are not possible but
