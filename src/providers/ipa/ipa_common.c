e2be78260b50868409a1e1c823def9735ad65982
Jakub Hrozek <jhrozek@redhat.com>
2011-04-29 11:49:35 -0400

    Fix order of arguments in select_principal_from_keytab() call

--- src/providers/ipa/ipa_common.c
+++ src/providers/ipa/ipa_common.c
@@ -362,9 +362,9 @@ int ipa_get_id_options(struct ipa_options *ipa_opts,
     }
 
     ret = select_principal_from_keytab(tmpctx,
+                                       desired_primary, desired_realm,
                                        dp_opt_get_string(ipa_opts->id->basic,
                                                          SDAP_KRB5_KEYTAB),
-                                       desired_primary, desired_realm,
                                        NULL, &primary, &realm);
     if (ret != EOK) {
         goto done;
