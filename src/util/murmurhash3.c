866ca0b4fb3e3e2f4631c769c56652d3176ca648
Simo Sorce <simo@redhat.com>
2012-01-09 17:07:11 -0500

    util: Fix murmurhash3 on machines with old glibc

--- src/util/murmurhash3.c
+++ src/util/murmurhash3.c
@@ -11,6 +11,16 @@
 #include <endian.h>
 #include <string.h>
 
+/* support RHEL5 lack of definitions */
+#ifndef le32toh
+# if __BYTE_ORDER == __LITTLE_ENDIAN
+#  define le32toh(x) (x)
+# else
+#  define le32toh(x) __bswap_32 (x)
+# endif
+#endif
+
+
 static uint32_t rotl(uint32_t x, int8_t r)
 {
     return (x << r) | (x >> (32 - r));
