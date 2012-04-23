53dae47b4e8995be624c74cb2d4838c6856b0ba4
Pavel Březina <pbrezina@redhat.com>
2012-04-23 14:42:19 +0200

    fix copy and paste error in comment

--- src/confdb/confdb.c
+++ src/confdb/confdb.c
@@ -911,7 +911,7 @@ static int confdb_get_domain_internal(struct confdb_ctx *cdb,
         goto done;
     }
 
-    /* Override the service cache timeout, if specified */
+    /* Override the autofs cache timeout, if specified */
     ret = get_entry_as_uint32(res->msgs[0], &domain->autofsmap_timeout,
                               CONFDB_DOMAIN_AUTOFS_CACHE_TIMEOUT,
                               entry_cache_timeout);
