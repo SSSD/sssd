245cc346966b02aa032de092ff8910420e8305fa
Lukas Slebodnik <lslebodn@redhat.com>
2013-05-09 15:10:32 +0200

    Fixes compilation without selinux.
    Compilation fail if ./configure is called with arguments
    --with-selinux --with-semanage and selinux header files are not
    installed. We didn't not catch this in fedora, because krb5-devel depends on
    libselinux-devel, but other distribution can package it differently.
    And API from selinux.h is not used in file ipa_selinux.c

--- src/providers/ipa/ipa_selinux.c
+++ src/providers/ipa/ipa_selinux.c
@@ -21,7 +21,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <selinux/selinux.h>
 #include <security/pam_modules.h>
 
 #include "db/sysdb_selinux.h"
