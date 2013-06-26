a6f0c94f544705e4377b7ce62a867534fe7dcd9e
Sumit Bose <sbose@redhat.com>
2013-06-26 13:11:35 +0200

    Revert "Implicitly activate the PAC responder for AD provider"
    This reverts commit 7527ec8ab2b79ec576ace2da9e2c158b849adfa6.

--- src/monitor/monitor.c
+++ src/monitor/monitor.c
@@ -863,8 +863,7 @@ static errno_t add_implicit_services(struct confdb_ctx *cdb, TALLOC_CTX *mem_ctx
                 continue;
             }
 
-            if (strcasecmp(id_provider, "IPA") == 0
-                    || strcasecmp(id_provider, "AD") == 0) {
+            if (strcasecmp(id_provider, "IPA") == 0) {
                 add_pac = true;
             }
         } else {
