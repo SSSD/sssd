546f1e38fa7ec1d86dd44117dda45f456fb00d39
Lukas Slebodnik <lslebodn@redhat.com>
2013-08-30 23:19:11 +0200

    Include sys/types.h for types id_t and uid_t

--- src/responder/pac/pacsrv_utils.c
+++ src/responder/pac/pacsrv_utils.c
@@ -18,6 +18,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <sys/types.h>
 #include <stdbool.h>
 #include <util/data_blob.h>
 #include <gen_ndr/security.h>
