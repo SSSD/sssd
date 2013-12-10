4c106dc57de95ae1e9b41ec56f6c866d7098bbdf
Sumit Bose <sbose@redhat.com>
2013-12-10 10:14:02 +0100

    Add sysdb_attrs_get_int32_t

--- src/db/sysdb.c
+++ src/db/sysdb.c
@@ -366,6 +366,32 @@ int sysdb_attrs_get_string(struct sysdb_attrs *attrs, const char *name,
     return EOK;
 }
 
+int sysdb_attrs_get_int32_t(struct sysdb_attrs *attrs, const char *name,
+                             int32_t *value)
+{
+    struct ldb_message_element *el;
+    int ret;
+    char *endptr;
+    int32_t val;
+
+    ret = sysdb_attrs_get_el_ext(attrs, name, false, &el);
+    if (ret) {
+        return ret;
+    }
+
+    if (el->num_values != 1) {
+        return ERANGE;
+    }
+
+    errno = 0;
+    val = strtoint32((const char *) el->values[0].data, &endptr, 10);
+    if (errno != 0) return errno;
+    if (*endptr) return EINVAL;
+
+    *value = val;
+    return EOK;
+}
+
 int sysdb_attrs_get_uint32_t(struct sysdb_attrs *attrs, const char *name,
                              uint32_t *value)
 {
