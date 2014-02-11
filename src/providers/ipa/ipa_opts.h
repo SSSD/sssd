63bbcff6b00ca09e468f56b764a5ae419624bbfd
Jakub Hrozek <jhrozek@redhat.com>
2014-02-11 15:36:04 +0100

    IPA: Default to krb5_use_fast=try
    Reviewed-by: Lukáš Slebodník <lslebodn@redhat.com>
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    Reviewed-by: Nathaniel McCallum <npmccallum@redhat.com>
    Reviewed-by: Alexander Bokovoy <abokovoy@redhat.com>

--- src/providers/ipa/ipa_opts.h
+++ src/providers/ipa/ipa_opts.h
@@ -275,7 +275,7 @@ struct dp_option ipa_def_krb5_opts[] = {
     { "krb5_renewable_lifetime", DP_OPT_STRING, NULL_STRING, NULL_STRING },
     { "krb5_lifetime", DP_OPT_STRING, NULL_STRING, NULL_STRING },
     { "krb5_renew_interval", DP_OPT_STRING, NULL_STRING, NULL_STRING },
-    { "krb5_use_fast", DP_OPT_STRING, NULL_STRING, NULL_STRING },
+    { "krb5_use_fast", DP_OPT_STRING, { "try" }, NULL_STRING },
     { "krb5_fast_principal", DP_OPT_STRING, NULL_STRING, NULL_STRING },
     { "krb5_canonicalize", DP_OPT_BOOL, BOOL_TRUE, BOOL_TRUE },
     { "krb5_use_enterprise_principal", DP_OPT_BOOL, BOOL_FALSE, BOOL_FALSE },
