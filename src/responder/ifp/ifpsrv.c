c4a85f65db3713bb5b9ef12b0cebaddace84a8e8
Pavel Březina <pbrezina@redhat.com>
2014-03-28 12:51:08 +0100

    IFP: do not create client socket
    There is no need for client socket in IFP responder,
    since it uses D-Bus for communication with clients.
    
    Resolves:
    https://fedorahosted.org/sssd/ticket/2290
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    (cherry picked from commit 0a6fa194bad18f417dc8542d3b8f654f898375c5)

--- src/responder/ifp/ifpsrv.c
+++ src/responder/ifp/ifpsrv.c
@@ -205,7 +205,7 @@ int ifp_process_init(TALLOC_CTX *mem_ctx,
     ifp_cmds = get_ifp_cmds();
     ret = sss_process_init(mem_ctx, ev, cdb,
                            ifp_cmds,
-                           SSS_IFP_SOCKET_NAME, NULL,
+                           NULL, NULL,
                            CONFDB_IFP_CONF_ENTRY,
                            SSS_IFP_SBUS_SERVICE_NAME,
                            SSS_IFP_SBUS_SERVICE_VERSION,
