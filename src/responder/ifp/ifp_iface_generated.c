6ad198aad0406bf6649540ff99c699cdc3f5f3b7
Jakub Hrozek <jhrozek@redhat.com>
2014-04-15 15:42:15 +0200

    IFP: Add GetUserAttrs call
    Adds a DBus method that allows the caller to retrieve attributes of a
    user. The synopsis of the call is as follows:
        <method name="GetUserAttr">
            <arg type="s" name="user" direction="in"/>
            <arg type="as" name="attr" direction="in"/>
            <arg type="a{sv}" name="values" direction="out"/>
        </method>
    
    The return value is an array (one attribute per array member) of
    dictionaries. The key of the dictionary is the attribute name, the value
    is a variant containing the attribute values as strings.
    
    If an attribute does not exist or is not permitted to be read, no error
    is returned. If the users does not exist, the method returns an error.
    
    In future patches this function will be marked as obsolete in favor of
    object-oriented approach.
    
    ifp_user_get_attr_unpack_msg is a separate function to allow extending
    it in a later patch.
    
    The function to check the cache validity duplicates quite a bit of code
    with the NSS responder. The refactoring would be nice to get done along
    with #843.
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    Reviewed-by: Stef Walter <stefw@redhat.com>
    (cherry picked from commit 2fbe9b9373dcdc28558da07690e57ff7a162a11d)

--- src/responder/ifp/ifp_iface_generated.c
+++ src/responder/ifp/ifp_iface_generated.c
@@ -5,6 +5,19 @@
 #include "sbus/sssd_dbus_meta.h"
 #include "ifp_iface_generated.h"
 
+/* arguments for org.freedesktop.sssd.infopipe.GetUserAttr */
+const struct sbus_arg_meta infopipe_iface_GetUserAttr__in[] = {
+    { "user", "s" },
+    { "attr", "as" },
+    { NULL, }
+};
+
+/* arguments for org.freedesktop.sssd.infopipe.GetUserAttr */
+const struct sbus_arg_meta infopipe_iface_GetUserAttr__out[] = {
+    { "values", "a{sv}" },
+    { NULL, }
+};
+
 /* methods for org.freedesktop.sssd.infopipe */
 const struct sbus_method_meta infopipe_iface__methods[] = {
     {
@@ -12,6 +25,13 @@ const struct sbus_method_meta infopipe_iface__methods[] = {
         NULL, /* no in_args */
         NULL, /* no out_args */
         offsetof(struct infopipe_iface, Ping),
+        NULL, /* no invoker */
+    },
+    {
+        "GetUserAttr", /* name */
+        infopipe_iface_GetUserAttr__in,
+        infopipe_iface_GetUserAttr__out,
+        offsetof(struct infopipe_iface, GetUserAttr),
         NULL, /* no invoker */
     },
     { NULL, }
