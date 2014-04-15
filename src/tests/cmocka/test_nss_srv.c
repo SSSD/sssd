4f3e9da9a27624017866579701dd9f30b976a533
Jakub Hrozek <jhrozek@redhat.com>
2014-04-15 17:24:55 +0200

    RESPONDERS: Add a new request sss_parse_inp_send
    The responders were copying code to parse input and on encountering an
    uknown domain, send the discover subdomain request. This patch adds a
    reusable request that can always be called in responders and in case the
    name can be parsed, just shortcut.
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    (cherry picked from commit 7caf7ed4f2eae1ec1c0717b4ee6ce78bdacd5926)
    
    Conflicts:
    	Makefile.am

--- src/tests/cmocka/test_nss_srv.c
+++ src/tests/cmocka/test_nss_srv.c
@@ -4,7 +4,7 @@
 
     Copyright (C) 2013 Red Hat
 
-    SSSD tests: Common utilities for tests that exercise domains
+    SSSD tests: NSS responder tests
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- src/util/util_errors.c
+++ src/util/util_errors.c
@@ -48,6 +48,7 @@ struct err_string error_to_str[] = {
     { "Dynamic DNS update failed" }, /* ERR_DYNDNS_FAILED */
     { "Dynamic DNS update timed out" }, /* ERR_DYNDNS_TIMEOUT */
     { "Dynamic DNS update not possible while offline" }, /* ERR_DYNDNS_OFFLINE */
+    { "Cannot parse input" }, /* ERR_INPUT_PARSE */
     { "Entry not found" }, /* ERR_NOT_FOUND */
     { "Domain not found" }, /* ERR_DOMAIN_NOT_FOUND */
     { "Missing configuration file" }, /* ERR_MISSING_CONF */
