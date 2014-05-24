29874756c8629e75581a21794ab8e890764c37fe
Lukas Slebodnik <lslebodn@redhat.com>
2014-05-24 17:48:04 +0200

    SBUS: Define DBUS_ERROR_INIT for old version of dbus
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>
    (cherry picked from commit 18647db9e275e7aa3e002551237abae756a2afb9)
    
    Conflicts:
    	src/sbus/sssd_dbus.h

--- src/sbus/sssd_dbus.h
+++ src/sbus/sssd_dbus.h
@@ -30,6 +30,10 @@ struct sbus_request;
 #include <sys/types.h>
 #include "util/util.h"
 
+#ifndef DBUS_ERROR_INIT
+#define DBUS_ERROR_INIT { NULL, NULL, TRUE, 0, 0, 0, 0, NULL }
+#endif /* DBUS_ERROR_INIT */
+
 typedef int (*sbus_msg_handler_fn)(struct sbus_request *dbus_req,
                                    void *instance_data);
 
