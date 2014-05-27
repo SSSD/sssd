e2f9f66e256e6964c30e1313c5fca86d300fd2ff
Lukas Slebodnik <lslebodn@redhat.com>
2014-05-27 23:05:08 +0200

    PAM: Fix compilation of pam_test_client with openpam
    linuxpam and openpam use different functions for text based conversation.
    
    Reviewed-by: Sumit Bose <sbose@redhat.com>
    (cherry picked from commit 9e17b0516d2b640d34cbb50933bd065e65bfab9d)

--- src/sss_client/pam_test_client.c
+++ src/sss_client/pam_test_client.c
@@ -27,12 +27,26 @@
 #include <string.h>
 
 #include <security/pam_appl.h>
-#include <security/pam_misc.h>
 
+#ifdef HAVE_SECURITY_PAM_MISC_H
+# include <security/pam_misc.h>
+#elif defined(HAVE_SECURITY_OPENPAM_H)
+# include <security/openpam.h>
+#endif
+
+#ifdef HAVE_SECURITY_PAM_MISC_H
 static struct pam_conv conv = {
     misc_conv,
     NULL
 };
+#elif defined(HAVE_SECURITY_OPENPAM_H)
+static struct pam_conv conv = {
+    openpam_ttyconv,
+    NULL
+};
+#else
+# error "Missing text based pam conversation function"
+#endif
 
 int main(int argc, char *argv[]) {
 
