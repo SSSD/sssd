ee4ba51f2fcfc8d8b807c3de6eaac554281165d2
Sumit Bose <sbose@redhat.com>
2014-07-18 22:34:23 +0200

    KRB5: add missing debug-to-stderr option to krb5_child
    Without this option krb5_child cannot be run in interactive mode.
    
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

--- src/providers/krb5/krb5_child.c
+++ src/providers/krb5/krb5_child.c
@@ -1979,6 +1979,9 @@ int main(int argc, const char *argv[])
          _("Show timestamps with microseconds"), NULL},
         {"debug-fd", 0, POPT_ARG_INT, &debug_fd, 0,
          _("An open file descriptor for the debug logs"), NULL},
+        {"debug-to-stderr", 0, POPT_ARG_NONE | POPT_ARGFLAG_DOC_HIDDEN,
+         &debug_to_stderr, 0,
+         _("Send the debug output to stderr directly."), NULL },
         POPT_TABLEEND
     };
 
