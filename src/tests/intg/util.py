447d32b6f572e8ba3100668cb8677c19c0085be6
Lukas Slebodnik <lslebodn@redhat.com>
2015-07-23 12:32:29 +0200

    CI: Set env variable for all tabs in screen
    Previously, only one tab had configured all environment variables
    and current working directory was set to source directory.
    It's better to open shell in "chroot" directory for troubleshooting purposes.
    
    This patch also open pre-defined tabs with useful
    directories with sssd cache, sssd log files, "chroot" directory
    
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

--- src/tests/intg/util.py
+++ src/tests/intg/util.py
@@ -20,6 +20,7 @@
 import re
 import os
 import subprocess
+import config
 
 UNINDENT_RE = re.compile("^ +", re.MULTILINE)
 
@@ -42,13 +43,20 @@ def run_shell():
         Execute an interactive shell under "screen", preserving environment.
         For use as a breakpoint for debugging.
     """
+    my_env = os.environ.copy()
+    my_env["ROOT_DIR"] = config.PREFIX
+
+    # screen filter out LD_* evniroment varibles.
+    # Back-up them and set them later in screenrc
+    my_env["_LD_LIBRARY_PATH"] = os.getenv("LD_LIBRARY_PATH", "")
+    my_env["_LD_PRELOAD"] = os.getenv("LD_PRELOAD", "")
+
     subprocess.call([
-        "screen", "-D", "-m", "bash", "-c",
-        "PATH='" + os.getenv("PATH", "") + "' " +
-        "LD_LIBRARY_PATH='" + os.getenv("LD_LIBRARY_PATH", "") + "' " +
-        "LD_PRELOAD='" + os.getenv("LD_PRELOAD", "") + "' " +
-        "bash -i"
-    ])
+        "screen", "-DAm", "-S", "sssd_cwrap_session", "-c",
+        ".config/screenrc"],
+        env=my_env
+    )
+
 
 
 def first_dir(*args):
