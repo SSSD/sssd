43e06ff39584570817949dc5de118d2b7ca854c1
Michal Židek <mzidek@redhat.com>
2015-10-19 15:38:08 +0200

    util: Continue if setlocale fails
    setlocale needs some environment variables
    to be set in order to work. These variables
    are not present in some special cases. We
    should not fail completely in these cases
    but continue with the compatible C locale.
    
    Resolves:
    https://fedorahosted.org/sssd/ticket/2785
    
    Reviewed-by: Michal Židek <mzidek@redhat.com>

--- src/tools/tools_util.c
+++ src/tools/tools_util.c
@@ -259,7 +259,9 @@ int set_locale(void)
 
     c = setlocale(LC_ALL, "");
     if (c == NULL) {
-        return EIO;
+        /* If setlocale fails, continue with the default
+         * locale. */
+        DEBUG(SSSDBG_MINOR_FAILURE, "Unable to set locale\n");
     }
 
     errno = 0;
