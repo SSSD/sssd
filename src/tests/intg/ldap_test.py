c54c097445cdcbc2774c97a18549d516fc0b63cb
Lukas Slebodnik <lslebodn@redhat.com>
2015-11-06 12:47:24 +0100

    Revert "intg: Invalidate memory cache before removing files"
    It is not necessary to invalidate memory cache before removing
    them. The sssd_client can handle it without any problem.
    
    This reverts commit eabc1732ef91548616a699b7e9f8d30e5e7b8dd3.
    
    Reviewed-by: Michal Židek <mzidek@redhat.com>
    (cherry picked from commit 782d39e3916d16b8dbba6ae97aca1db2f3c35d76)

--- src/tests/intg/ldap_test.py
+++ src/tests/intg/ldap_test.py
@@ -174,7 +174,6 @@ def cleanup_sssd_process():
             time.sleep(1)
     except:
         pass
-    subprocess.call(["sss_cache", "-E"])
     for path in os.listdir(config.DB_PATH):
         os.unlink(config.DB_PATH + "/" + path)
     for path in os.listdir(config.MCACHE_PATH):
