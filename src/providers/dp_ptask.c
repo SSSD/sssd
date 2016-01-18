e89c2cb5ec77d57ed93952dae08df51738834faf
Sumit Bose <sbose@redhat.com>
2016-01-18 13:20:16 +0100

    DP_TASK: add be_ptask_get_timeout()
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

--- src/providers/dp_ptask.c
+++ src/providers/dp_ptask.c
@@ -384,6 +384,11 @@ time_t be_ptask_get_period(struct be_ptask *task)
     return task->period;
 }
 
+time_t be_ptask_get_timeout(struct be_ptask *task)
+{
+    return task->timeout;
+}
+
 struct be_ptask_sync_ctx {
     be_ptask_sync_t fn;
     void *pvt;
