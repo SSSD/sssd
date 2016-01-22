52ea2caa4d21a980902cd0f2fd77ceba25062a8c
Sumit Bose <sbose@redhat.com>
2016-01-22 18:14:45 +0100

    sdap: improve filtering of multiple results in GC lookups
    The Global Catalog of AD contains some information about all users and
    groups in an AD forest. Users from different domain in the forest can
    have the same name. The most obvious example is the Administrator user
    which is present in all domains. Although SSSD uses a domain specific
    search base for looking up users in the GC the search might still return
    multiple results if there is a user with the same name in one of the
    child (or grand-child ...) domains because of the hierarchic nature of
    the LDAP tree. Limiting the search depth would not help because users
    can be created in deeply nested OUs.
    
    Currently SSSD expects in this case that the user object is store in
    CN=Users or below. This works for all default users like Administrator
    but in general users can be created anywhere in the directory tree. If a
    user is created outside of CN=Users and there is a user with the same
    name in a child domain the initgroups command to look up the
    group-memberships of the user fails because it is not clear which of the
    two results should be used (initgroups for the child domain user works
    fine).
    
    This patch adds an additional scheme to select the right result based on
    the domain component attribute name 'dc'. This attribute indicates an
    additional component in the domain name and hence a child domain. So as
    long as the result contains a dc component following out search base it
    cannot be the object we are looking for. This scheme includes the old
    CN=Users based one but since it is more expensive I kept the old scheme
    which so far worked all the time and only use the new one if the old one
    fails.
    
    Resolves https://fedorahosted.org/sssd/ticket/2961
    
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>
    (cherry picked from commit 5ff7a765434ed0b4d37564ade26d7761d06f81c3)

--- src/tests/cmocka/test_sysdb_subdomains.c
+++ src/tests/cmocka/test_sysdb_subdomains.c
@@ -509,6 +509,76 @@ static void test_sysdb_link_ad_multidom(void **state)
 
 }
 
+static void test_try_to_find_expected_dn(void **state)
+{
+    int ret;
+    struct sysdb_attrs *result;
+    struct sysdb_attrs *usr_attrs[10] = { NULL };
+    struct sss_domain_info *dom;
+    struct subdom_test_ctx *test_ctx =
+        talloc_get_type(*state, struct subdom_test_ctx);
+
+    dom = find_domain_by_name(test_ctx->tctx->dom,
+                              "child2.test_sysdb_subdomains_2", true);
+    assert_non_null(dom);
+
+    usr_attrs[0] = sysdb_new_attrs(test_ctx);
+    assert_non_null(usr_attrs[0]);
+
+    ret = sysdb_attrs_add_string(usr_attrs[0], SYSDB_ORIG_DN,
+                  "uid=user,cn=abc,dc=c2,dc=child2,dc=test_sysdb_subdomains_2");
+    assert_int_equal(ret, EOK);
+
+    ret = sysdb_try_to_find_expected_dn(NULL, NULL, NULL, 0, NULL);
+    assert_int_equal(ret, EINVAL);
+
+    ret = sysdb_try_to_find_expected_dn(dom, "dc", usr_attrs, 1, &result);
+    assert_int_equal(ret, ENOENT);
+
+    ret = sysdb_try_to_find_expected_dn(dom, "xy", usr_attrs, 1, &result);
+    assert_int_equal(ret, EOK);
+    assert_ptr_equal(result, usr_attrs[0]);
+
+    usr_attrs[1] = sysdb_new_attrs(test_ctx);
+    assert_non_null(usr_attrs[1]);
+
+    ret = sysdb_attrs_add_string(usr_attrs[1], SYSDB_ORIG_DN,
+                 "uid=user1,cn=abc,dc=child2,dc=test_sysdb_subdomains_2");
+    assert_int_equal(ret, EOK);
+
+    usr_attrs[2] = sysdb_new_attrs(test_ctx);
+    assert_non_null(usr_attrs[2]);
+
+    ret = sysdb_attrs_add_string(usr_attrs[2], SYSDB_ORIG_DN,
+                 "uid=user2,cn=abc,dc=c2,dc=child2,dc=test_sysdb_subdomains_2");
+    assert_int_equal(ret, EOK);
+
+    ret = sysdb_try_to_find_expected_dn(dom, "dc", usr_attrs, 3, &result);
+    assert_int_equal(ret, EOK);
+    assert_ptr_equal(result, usr_attrs[1]);
+
+    ret = sysdb_try_to_find_expected_dn(dom, "xy", usr_attrs, 3, &result);
+    assert_int_equal(ret, EINVAL);
+
+    /* Make sure cn=users match is preferred */
+    talloc_free(usr_attrs[2]);
+    usr_attrs[2] = sysdb_new_attrs(test_ctx);
+    assert_non_null(usr_attrs[2]);
+
+    ret = sysdb_attrs_add_string(usr_attrs[2], SYSDB_ORIG_DN,
+                 "uid=user2,cn=abc,cn=users,dc=child2,dc=test_sysdb_subdomains_2");
+    assert_int_equal(ret, EOK);
+
+    ret = sysdb_try_to_find_expected_dn(dom, "dc", usr_attrs, 3, &result);
+    assert_int_equal(ret, EOK);
+    assert_ptr_equal(result, usr_attrs[2]);
+
+
+    talloc_free(usr_attrs[0]);
+    talloc_free(usr_attrs[1]);
+    talloc_free(usr_attrs[2]);
+}
+
 int main(int argc, const char *argv[])
 {
     int rv;
@@ -540,6 +610,9 @@ int main(int argc, const char *argv[])
                                         test_sysdb_subdom_setup,
                                         test_sysdb_subdom_teardown),
         cmocka_unit_test_setup_teardown(test_sysdb_link_ad_multidom,
+                                        test_sysdb_subdom_setup,
+                                        test_sysdb_subdom_teardown),
+        cmocka_unit_test_setup_teardown(test_try_to_find_expected_dn,
                                         test_sysdb_subdom_setup,
                                         test_sysdb_subdom_teardown),
     };
