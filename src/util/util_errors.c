75d66aea7accc842e68c88f085f9053112b20ecc
Pavel Březina <pbrezina@redhat.com>
2016-01-28 13:30:33 +0100

    ERRORS: Add ERR_TERMINATED
    To indicate that data provider request was unexpectedly terminated.
    
    Reviewed-by: Sumit Bose <sbose@redhat.com>
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>
    Reviewed-by: Lukáš Slebodník <lslebodn@redhat.com>

--- src/util/util_errors.c
+++ src/util/util_errors.c
@@ -28,6 +28,7 @@ struct err_string error_to_str[] = {
     { "Invalid Error" },        /* ERR_INVALID */
     { "Internal Error" },       /* ERR_INTERNAL */
     { "SSSD is offline" },       /* ERR_OFFLINE */
+    { "Terminated" },       /* ERR_TERMINATED */
     { "Account Unknown" },      /* ERR_ACCOUNT_UNKNOWN */
     { "Invalid credential type" },  /* ERR_INVALID_CRED_TYPE */
     { "No credentials available" }, /* ERR_NO_CREDS */
