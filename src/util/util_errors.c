af717c5b022d5c28141333fc02d5d9e1f322505c
Lukas Slebodnik <lslebodn@redhat.com>
2016-02-17 17:58:10 +0100

    UTIL: Backport error code ERR_ACCOUNT_LOCKED
    Required by:
    https://fedorahosted.org/sssd/ticket/2839
    
    Reviewed-by: Michal Židek <mzidek@redhat.com>

--- src/util/util_errors.c
+++ src/util/util_errors.c
@@ -82,6 +82,7 @@ struct err_string error_to_str[] = {
     { "Address family not supported" }, /* ERR_ADDR_FAMILY_NOT_SUPPORTED */
     { "Message sender is the bus" }, /* ERR_SBUS_SENDER_BUS */
     { "Subdomain is inactive" }, /* ERR_SUBDOM_INACTIVE */
+    { "Account is locked" }, /* ERR_ACCOUNT_LOCKED */
     { "AD renewal child failed" }, /* ERR_RENEWAL_CHILD */
     { "ERR_LAST" } /* ERR_LAST */
 };
