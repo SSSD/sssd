SSSD/sssd

commit 5a5f1e1053415efaa99bb4d5bc7ce7ac0a95b757

IPA: Check master domain record before subdomain records
For one-way trusts we need to know the flat name of the IPA domain when
we process subdomains, hence we need to swap the processing order and
read the master domain record sooner.

Previsouly the order was:
    - ranges
    - subdomains
    - if on client, views
    - master

Now the order is:
    - ranges
    - master
    - subdomains
    - if on client, views

Reviewed-by: Sumit Bose <sbose@redhat.com>

src/providers/ipa/ipa_subdomains.c
@@ -1223,13 +1223,7 @@ static void ipa_get_view_name_done(struct tevent_req *req)
 
     }
 
-    ret = ipa_check_master(ctx);
-    if (ret == EAGAIN) {
-        return;
-    } else if (ret != EOK) {
-        goto done;
-    }
-
+    ret = EOK;
 done:
     if (ret == EOK) {
         dp_error = DP_ERR_OK;
@@ -1322,13 +1316,7 @@ static void ipa_subdomains_handler_done(struct tevent_req *req)
         }
     }
 
-    ret = ipa_check_master(ctx);
-    if (ret == EAGAIN) {
-        return;
-    } else if (ret != EOK) {
-        goto done;
-    }
-
+    ret = EOK;
 done:
     if (ret == EOK) {
         dp_error = DP_ERR_OK;
@@ -1403,6 +1391,17 @@ static void ipa_subdomains_handler_ranges_done(struct tevent_req *req)
         goto done;
     }
 
+    ret = ipa_check_master(ctx);
+    if (ret == EAGAIN) {
+        DEBUG(SSSDBG_TRACE_ALL, "Checking master record..\n");
+        return;
+    } else if (ret != EOK) {
+        DEBUG(SSSDBG_OP_FAILURE, "ipa_check_master failed.\n");
+        goto done;
+    }
+    /* Master domain is up-to-date. Continue checking subdomains */
+
+    DEBUG(SSSDBG_TRACE_ALL, "Master record up2date, checking subdomains\n");
     ret = ipa_subdomains_handler_get_start(ctx, ctx->sd_ctx->search_bases,
                                            IPA_SUBDOMAINS_SLAVE);
     if (ret == EAGAIN) {
@@ -1463,6 +1462,21 @@ static void ipa_subdomains_handler_master_done(struct tevent_req *req)
 
         ret = sysdb_master_domain_add_info(ctx->sd_ctx->be_ctx->domain,
                                            realm, flat, id, NULL);
+        if (ret != EOK) {
+            goto done;
+        }
+
+        /* There is only one master record. Don't bother checking other IPA
+         * search bases; move to checking subdomains instead
+         */
+        ret = ipa_subdomains_handler_get_start(ctx,
+                                               ctx->sd_ctx->search_bases,
+                                               IPA_SUBDOMAINS_SLAVE);
+        if (ret == EAGAIN) {
+            return;
+        }
+
+        /* Either no search bases or an error. End the request in both cases */
     } else {
         ret = ipa_subdomains_handler_get_cont(ctx, IPA_SUBDOMAINS_MASTER);
         if (ret == EAGAIN) {

commit 57d8b4b9254442a568838cb60ea16068965f2df0

IPA: terminate properly if view name lookup fails
Since commit 5a5f1e1053415efaa99bb4d5bc7ce7ac0a95b757 the view name
lookup is the last step in the subdomain lookup request. In case of an
error the request should be finished and no previous step should be
called again.

Resolves https://fedorahosted.org/sssd/ticket/2993

Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

src/providers/ipa/ipa_subdomains.c
@@ -840,13 +840,7 @@ static void ipa_get_view_name_done(struct tevent_req *req)
         if (ret == EOPNOTSUPP || ret == EIO) {
             DEBUG(SSSDBG_TRACE_FUNC, "get_view_name request failed, looks " \
                                      "like server does not support views.\n");
-            ret = ipa_check_master(ctx);
-            if (ret == EAGAIN) {
-                return;
-            } else if (ret != EOK) {
-                goto done;
-            }
-
+            ret = EOK;
         } else {
             DEBUG(SSSDBG_OP_FAILURE, "get_view_name request failed.\n");
         }