2f75ad013f8410397e4efbf0adadc2e69621f12a
Pavel Březina <pbrezina@redhat.com>
2016-06-16 13:53:42 +0200

    SBUS ERRORS: Add unknown domain
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

--- src/sbus/sssd_dbus_errors.h
+++ src/sbus/sssd_dbus_errors.h
@@ -25,6 +25,7 @@
 
 #define SBUS_ERROR_INTERNAL "org.freedesktop.sssd.Error.Internal"
 #define SBUS_ERROR_NOT_FOUND "org.freedesktop.sssd.Error.NotFound"
+#define SBUS_ERROR_UNKNOWN_DOMAIN "org.freedesktop.sssd.Error.UnknownDomain"
 
 #define SBUS_ERROR_DP_FATAL "org.freedesktop.sssd.Error.DataProvider.Fatal"
 #define SBUS_ERROR_DP_OFFLINE "org.freedesktop.sssd.Error.DataProvider.Offline"
