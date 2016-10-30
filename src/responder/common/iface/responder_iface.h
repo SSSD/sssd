c109f063b4469818fd335b8b509f0458e7b33b0a
Jakub Hrozek <jhrozek@redhat.com>
2016-10-30 07:05:43 +0100

    RESPONDER: Add a responder sbus interface to set domain state
    Adds a generic responder s-bus interface that all responders implement.
    The interface currently contains methods that make it possible for a sssd
    domain to be marked as active or inconsistent by a back end.
    
    In the future, this commit will be superseded by sbus signals.
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>

---
+++ src/responder/common/iface/responder_iface.h
@@ -0,0 +1,37 @@
+/*
+    Copyright (C) 2016 Red Hat
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _RESPONDER_IFACE_H_
+#define _RESPONDER_IFACE_H_
+
+#include "responder/common/iface/responder_iface_generated.h"
+
+#define RESPONDER_PATH "/org/freedesktop/sssd/responder"
+
+struct sbus_iface_map *responder_get_sbus_interface(void);
+
+/* org.freedesktop.sssd.Responder.Domain */
+
+int sss_resp_domain_active(struct sbus_request *req,
+                           void *data,
+                           const char *domain_name);
+
+int sss_resp_domain_inconsistent(struct sbus_request *req,
+                                 void *data,
+                                 const char *domain_name);
+
+#endif /* _RESPONDER_IFACE_H_ */
