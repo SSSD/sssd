26866484a985adbc7edf2e79a1e95b3bb6b8624c
Jakub Hrozek <jhrozek@redhat.com>
2016-12-02 17:51:54 +0100

    RESPONDER: Include the files provider in NEEDS_CHECK_PROVIDER
    It makes no sense to contact the Data Provider with the files provider
    except when the files provider is updating itself.
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>

--- src/responder/common/responder.h
+++ src/responder/common/responder.h
@@ -48,9 +48,14 @@ extern hash_table_t *dp_requests;
  * So we set umask to 0111. */
 #define SCKT_RSP_UMASK 0111
 
-/* if there is a provider other than the special local */
+/* Neither the local provider nor the files provider have a back
+ * end in the traditional sense and can always just consult
+ * the responder's cache
+ */
 #define NEED_CHECK_PROVIDER(provider) \
-    (provider != NULL && strcmp(provider, "local") != 0)
+    (provider != NULL && \
+     (strcmp(provider, "local") != 0 && \
+      strcmp(provider, "files") != 0))
 
 /* needed until nsssrv.h is updated */
 struct cli_request {
