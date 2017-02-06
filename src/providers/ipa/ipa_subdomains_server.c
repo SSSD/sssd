c44728a02d5e2c9eaced11e74820a6ae6a985f61
Sumit Bose <sbose@redhat.com>
2017-02-06 10:28:46 +0100

    IPA: add certmap support
    Read certificate mapping data from the IPA server and configure the
    certificate mapping library accordingly.
    
    Related to https://pagure.io/SSSD/sssd/issue/3050
    
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>
    Reviewed-by: Lukáš Slebodník <lslebodn@redhat.com>

--- src/providers/ipa/ipa_subdomains_server.c
+++ src/providers/ipa/ipa_subdomains_server.c
@@ -362,6 +362,10 @@ ipa_ad_ctx_new(struct be_ctx *be_ctx,
     ad_id_ctx->sdap_id_ctx->opts->idmap_ctx =
         id_ctx->sdap_id_ctx->opts->idmap_ctx;
 
+    /* Set up the certificate mapping context */
+    ad_id_ctx->sdap_id_ctx->opts->certmap_ctx =
+        id_ctx->sdap_id_ctx->opts->certmap_ctx;
+
     *_ad_id_ctx = ad_id_ctx;
     return EOK;
 }
