9d8d34e2f7e611771777a4ef1cd732a8ac3ba92a
Sumit Bose <sbose@redhat.com>
2017-04-04 14:35:47 +0200

    utils: add sss_domain_is_forest_root()
    Related to https://pagure.io/SSSD/sssd/issue/3361

--- src/util/domain_info_utils.c
+++ src/util/domain_info_utils.c
@@ -844,6 +844,11 @@ void sss_domain_set_state(struct sss_domain_info *dom,
           "Domain %s is %s\n", dom->name, domain_state_str(dom));
 }
 
+bool sss_domain_is_forest_root(struct sss_domain_info *dom)
+{
+    return (dom->forest_root == dom);
+}
+
 bool is_email_from_domain(const char *email, struct sss_domain_info *dom)
 {
     const char *p;
