f74408e37a3007aa41b19ab2afb693a91694da42
Justin Stephenson <jstephen@redhat.com>
2017-07-23 17:33:35 -0400

    SSSCTL: Add cache-expire command
    Add sssctl cache-expire as a wrapper for the sss_cache utility to
    invalidate cached objects.
    
    Reviewed-by: Lukáš Slebodník <lslebodn@redhat.com>
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>
    Reviewed-by: Michal Židek <mzidek@redhat.com>

--- src/tools/sssctl/sssctl_data.c
+++ src/tools/sssctl/sssctl_data.c
@@ -30,6 +30,7 @@
 #define SSS_BACKUP_DIR SSS_STATEDIR "/backup"
 #define SSS_BACKUP_USER_OVERRIDES SSS_BACKUP_DIR "/sssd_user_overrides.bak"
 #define SSS_BACKUP_GROUP_OVERRIDES SSS_BACKUP_DIR "/sssd_group_overrides.bak"
+#define SSS_CACHE "sss_cache"
 
 struct sssctl_data_opts {
     int override;
@@ -288,4 +289,47 @@ errno_t sssctl_cache_upgrade(struct sss_cmdline *cmdline,
     }
 
     return EOK;
+}
+
+errno_t sssctl_cache_expire(struct sss_cmdline *cmdline,
+                            struct sss_tool_ctx *tool_ctx,
+                            void *pvt)
+{
+    errno_t ret;
+    char *cmd_args = NULL;
+    const char *cachecmd = SSS_CACHE;
+    char *cmd = NULL;
+    int i;
+
+    if (cmdline->argc == 0) {
+        ret = sssctl_run_command(cachecmd);
+        goto done;
+    }
+
+    cmd_args = talloc_strdup(tool_ctx, "");
+    if (cmd_args == NULL) {
+        ret = ENOMEM;
+        goto done;
+    }
+
+    for (i = 0; i < cmdline->argc; i++) {
+        cmd_args = talloc_strdup_append(cmd_args, cmdline->argv[i]);
+        if (i != cmdline->argc - 1) {
+            cmd_args = talloc_strdup_append(cmd_args, " ");
+        }
+    }
+
+    cmd = talloc_asprintf(tool_ctx, "%s %s", cachecmd, cmd_args);
+    if (cmd == NULL) {
+        ret = ENOMEM;
+        goto done;
+    }
+
+    ret = sssctl_run_command(cmd);
+
+done:
+    talloc_free(cmd_args);
+    talloc_free(cmd);
+
+    return ret;
 }
