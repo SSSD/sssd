06b88cd58177c9d21f86b95a915e5f5f4e8bead7
Fabiano Fidêncio <fidencio@redhat.com>
2017-08-14 15:28:41 +0200

    NEGCACHE: Add some comments about each step of sss_ncache_prepopulate()
    The comments help to understand which part of the code is dealing with
    users or groups of specific or non-specific domain filters.
    
    Related: https://pagure.io/SSSD/sssd/issue/3460
    
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>
    
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>
    (cherry picked from commit b54d79cf3c8017e186b5ea7cdc383746233db39b)
    (cherry picked from commit 1d3d2a86b89aa3a24dbe8ffa770f1a28b645ee07)

--- src/responder/common/negcache.c
+++ src/responder/common/negcache.c
@@ -663,7 +663,7 @@ errno_t sss_ncache_prepopulate(struct sss_nc_ctx *ncache,
     TALLOC_CTX *tmpctx = talloc_new(NULL);
     int i;
 
-    /* Populate domain-specific negative cache entries */
+    /* Populate domain-specific negative cache user entries */
     for (dom = domain_list; dom; dom = get_next_domain(dom, 0)) {
         conf_path = talloc_asprintf(tmpctx, CONFDB_DOMAIN_PATH_TMPL,
                                     dom->name);
@@ -715,6 +715,7 @@ errno_t sss_ncache_prepopulate(struct sss_nc_ctx *ncache,
         }
     }
 
+    /* Populate non domain-specific negative cache user entries */
     ret = confdb_get_string_as_list(cdb, tmpctx, CONFDB_NSS_CONF_ENTRY,
                                     CONFDB_NSS_FILTER_USERS, &filter_list);
     if (ret == ENOENT) {
@@ -779,6 +780,7 @@ errno_t sss_ncache_prepopulate(struct sss_nc_ctx *ncache,
         }
     }
 
+    /* Populate domain-specific negative cache group entries */
     filter_set = false;
     for (dom = domain_list; dom; dom = get_next_domain(dom, 0)) {
         conf_path = talloc_asprintf(tmpctx, CONFDB_DOMAIN_PATH_TMPL, dom->name);
@@ -823,6 +825,7 @@ errno_t sss_ncache_prepopulate(struct sss_nc_ctx *ncache,
         }
     }
 
+    /* Populate non domain-specific negative cache group entries */
     ret = confdb_get_string_as_list(cdb, tmpctx, CONFDB_NSS_CONF_ENTRY,
                                     CONFDB_NSS_FILTER_GROUPS, &filter_list);
     if (ret == ENOENT) {
