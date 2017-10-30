2856dac5818265a6b4e42d768b73c65e333d14ff
Jakub Hrozek <jhrozek@redhat.com>
2017-10-30 20:52:42 +0100

    CACHE_REQ: Add plugin methods required for the domain-locator request
    Adds three new cache_req plugin methods:
        - cache_req_dp_get_domain_check_fn - check if it is time to run the
                                             locator request again
        - cache_req_dp_get_domain_send/recv_fn - run the locator itself
    
    The reason we added also the checker is that when the locator runs,
    we add a temporary entry into the negative cache that would denote that
    the locator ran and the ordinary domain negative cache (UID negcache,
    GID negcache, ..) were set for the domains and can be still used to
    skip domains that we know do not contain the account without calling
    the getAccountDomain handler again.
    
    If we didn't have this checker, requesting an entry from a domain
    further down the domain list would always call the locator, only
    to always receive the same results.
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    Reviewed-by: Sumit Bose <sbose@redhat.com>

--- src/responder/common/cache_req/plugins/cache_req_group_by_id.c
+++ src/responder/common/cache_req/plugins/cache_req_group_by_id.c
@@ -155,7 +155,10 @@ const struct cache_req_plugin cache_req_group_by_id = {
     .ncache_filter_fn = cache_req_group_by_id_ncache_filter,
     .lookup_fn = cache_req_group_by_id_lookup,
     .dp_send_fn = cache_req_group_by_id_dp_send,
-    .dp_recv_fn = cache_req_common_dp_recv
+    .dp_recv_fn = cache_req_common_dp_recv,
+    .dp_get_domain_check_fn = NULL,
+    .dp_get_domain_send_fn = NULL,
+    .dp_get_domain_recv_fn = NULL,
 };
 
 struct tevent_req *
