a24954cc19285b197fb287bfa7aa01949c92b17d
Lukas Slebodnik <lslebodn@redhat.com>
2017-11-08 08:13:02 +0100

    CHILD: Pass information about logger to children
    Variables debug_to_file or debug_to_stderr were not set
    because back-end already user parameter --logger=%s.
    And therefore logs were not sent to files.
    
    It could only work in case of direct usage of --debug-to-files in back-end via
    command configuration option.
    
    Resolves:
    https://pagure.io/SSSD/sssd/issue/3433
    
    Reviewed-by: Fabiano Fidêncio <fidencio@redhat.com>

--- src/providers/krb5/krb5_child.c
+++ src/providers/krb5/krb5_child.c
@@ -3020,7 +3020,7 @@ int main(int argc, const char *argv[])
     int opt;
     poptContext pc;
     int debug_fd = -1;
-    char *opt_logger = NULL;
+    const char *opt_logger = NULL;
     errno_t ret;
     krb5_error_code kerr;
     uid_t fast_uid;
@@ -3097,6 +3097,7 @@ int main(int argc, const char *argv[])
         if (ret != EOK) {
             DEBUG(SSSDBG_CRIT_FAILURE, "set_debug_file_from_fd failed.\n");
         }
+        opt_logger = sss_logger_str[FILES_LOGGER];
     }
 
     sss_set_logger(opt_logger);
