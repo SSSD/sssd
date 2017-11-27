dbe54141c36006809cc5fcf9ad729405c0a9292c
Sumit Bose <sbose@redhat.com>
2017-11-27 13:45:14 +0100

    krb5: show error message for krb5_init_context() failures
    If there are typos in /etc/krb5.conf (or one of the included config
    snippets) krb5_init_context(), the initial call always needed to do any
    other operation with libkrb5, fails because /etc/krb5.conf cannot be
    parsed.
    
    Currently the related debug/syslog messages might be misleading, e.g.
    failed to read keytab. This is because SSSD does not use a global krb5
    context but creates a fresh one for every new request or operation (to
    always use the latest settings from /etc/krb5.conf) and typically there
    is an error message indicating that the related operation failed but not
    giving more details.
    
    Since krb5_init_context() is fundamental for Kerberos support this patch
    tries to add as much details as libkrb5 provides in the logs if the call
    fails.
    
    Resolves:
    https://pagure.io/SSSD/sssd/issue/3586
    
    Reviewed-by: Lukáš Slebodník <lslebodn@redhat.com>
    Reviewed-by: Robbie Harwood <rharwood@redhat.com>
    (cherry picked from commit 2c10819750a8d920ab755eba1278e6e20e684e93)

--- src/util/sss_krb5.c
+++ src/util/sss_krb5.c
@@ -111,7 +111,7 @@ errno_t select_principal_from_keytab(TALLOC_CTX *mem_ctx,
         return ENOMEM;
     }
 
-    kerr = krb5_init_context(&krb_ctx);
+    kerr = sss_krb5_init_context(&krb_ctx);
     if (kerr) {
         DEBUG(SSSDBG_OP_FAILURE, "Failed to init kerberos context\n");
         ret = EFAULT;
@@ -1094,9 +1094,9 @@ bool sss_krb5_realm_has_proxy(const char *realm)
         return false;
     }
 
-    kerr = krb5_init_context(&context);
+    kerr = sss_krb5_init_context(&context);
     if (kerr != 0) {
-        DEBUG(SSSDBG_OP_FAILURE, "krb5_init_context failed.\n");
+        DEBUG(SSSDBG_OP_FAILURE, "sss_krb5_init_context failed.\n");
         return false;
     }
 
@@ -1134,4 +1134,23 @@ done:
     krb5_free_context(context);
 
     return res;
+}
+
+krb5_error_code sss_krb5_init_context(krb5_context *context)
+{
+    krb5_error_code kerr;
+    const char *msg;
+
+    kerr = krb5_init_context(context);
+    if (kerr != 0) {
+        /* It is safe to call (sss_)krb5_get_error_message() with NULL as first
+         * argument. */
+        msg = sss_krb5_get_error_message(NULL, kerr);
+        DEBUG(SSSDBG_FATAL_FAILURE,
+              "Failed to init kerberos context [%s]\n", msg);
+        sss_log(SSS_LOG_CRIT, "Failed to init kerberos context [%s]\n", msg);
+        sss_krb5_free_error_message(NULL, msg);
+    }
+
+    return kerr;
 }
