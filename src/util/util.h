d2a7513ae8f9fe17ede754837805caf51893caa0
Sumit Bose <sbose@redhat.com>
2018-09-18 18:15:02 +0200

    PAM: add p11_wait_for_card_timeout option
    If the --wait_for_card is used to call p11_child the PAM responder
    should be prepared to wait longer until p11_child can return
    successfully.
    
    Related to https://pagure.io/SSSD/sssd/issue/3650

--- src/util/util.h
+++ src/util/util.h
@@ -724,6 +724,7 @@ errno_t create_preauth_indicator(void);
 #define P11_CHILD_LOG_FILE "p11_child"
 #define P11_CHILD_PATH SSSD_LIBEXEC_PATH"/p11_child"
 #define P11_CHILD_TIMEOUT_DEFAULT 10
+#define P11_WAIT_FOR_CARD_TIMEOUT_DEFAULT 60
 #endif  /* SSSD_LIBEXEC_PATH */
 
 #endif /* __SSSD_UTIL_H__ */
