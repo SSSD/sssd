49be8974b490c368d349752f3196af0c9ed28dd5
Sumit Bose <sbose@redhat.com>
2018-09-18 09:53:37 +0200

    pam_sss: add option require_cert_auth
    With this new option pam_sss will wait until a Smartcard is available
    and then try to authenticate with the help of the Smartcard.
    
    Related https://pagure.io/SSSD/sssd/issue/3650
    
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

--- src/sss_client/sss_cli.h
+++ src/sss_client/sss_cli.h
@@ -363,6 +363,7 @@ enum pam_item_type {
     SSS_PAM_ITEM_CLI_LOCALE,
     SSS_PAM_ITEM_CLI_PID,
     SSS_PAM_ITEM_REQUESTED_DOMAINS,
+    SSS_PAM_ITEM_FLAGS,
 };
 
 #define PAM_CLI_FLAGS_USE_FIRST_PASS (1 << 0)
@@ -374,6 +375,7 @@ enum pam_item_type {
 #define PAM_CLI_FLAGS_ALLOW_MISSING_NAME (1 << 6)
 #define PAM_CLI_FLAGS_PROMPT_ALWAYS (1 << 7)
 #define PAM_CLI_FLAGS_TRY_CERT_AUTH (1 << 8)
+#define PAM_CLI_FLAGS_REQUIRE_CERT_AUTH (1 << 9)
 
 #define SSS_NSS_MAX_ENTRIES 256
 #define SSS_NSS_HEADER_SIZE (sizeof(uint32_t) * 4)
