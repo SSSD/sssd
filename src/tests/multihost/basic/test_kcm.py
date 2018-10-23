61798a6f9e730241a0b80c396449442fda2ba696
Jakub Hrozek <jhrozek@redhat.com>
2018-10-23 16:16:18 +0200

    TESTS: Add a multihost test for changing sssd-kcm debug level by just restarting the KCM service
    Related:
    https://pagure.io/SSSD/sssd/issue/3862
    
    Adds a test for the previous patches to make sure just restarting
    sssd-kcm with changes to the [kcm] section applies the configuration
    options from the kcm section.

--- src/tests/multihost/basic/test_kcm.py
+++ src/tests/multihost/basic/test_kcm.py
@@ -2,14 +2,45 @@
 from sssd.testlib.common.utils import SSHClient
 import paramiko
 import pytest
+import os
+from utils_config import set_param, remove_section
 
 
 class TestSanityKCM(object):
     """ KCM Sanity Test cases """
-    def test_kinit_kcm(self, multihost):
+    def _kcm_service_op(self, multihost, svc_op):
+        systemd_kcm_op = 'systemctl %s sssd-kcm' % (svc_op)
+        multihost.master[0].run_command(systemd_kcm_op)
+
+    def _start_kcm(self, multihost):
+        self._kcm_service_op(multihost, 'start')
+
+    def _stop_kcm(self, multihost):
+        self._kcm_service_op(multihost, 'stop')
+
+    def _restart_kcm(self, multihost):
+        self._kcm_service_op(multihost, 'restart')
+
+    def _remove_kcm_log_file(self, multihost):
+        multihost.master[0].run_command('rm -f /var/log/sssd/sssd_kcm.log')
+
+    def _kcm_log_length(self, multihost):
+        basename = 'sssd_kcm.log'
+        kcm_log_file = '/var/log/sssd/' + basename
+        local_kcm_log_file = '/tmp/kcm.log'
+        try:
+            multihost.master[0].transport.get_file(kcm_log_file,
+                                                   local_kcm_log_file)
+        except FileNotFoundError:
+            return 0
+
+        nlines = sum(1 for line in open(local_kcm_log_file))
+        os.remove(local_kcm_log_file)
+        return nlines
+
+    def test_kinit_kcm(self, multihost, enable_kcm):
         """ Run kinit with KRB5CCNAME=KCM: """
-        start_kcm = 'systemctl start sssd-kcm'
-        multihost.master[0].run_command(start_kcm)
+        self._start_kcm(multihost)
         try:
             ssh = SSHClient(multihost.master[0].sys_hostname,
                             username='foo3', password='Secret123')
@@ -43,3 +74,51 @@ class TestSanityKCM(object):
         else:
             assert True
             ssh.close()
+
+    def test_kcm_debug_level_set(self, multihost, enable_kcm):
+        """
+        Test that just adding a [kcm] section and restarting the kcm
+        service enables debugging without having to restart the
+        whole sssd
+        """
+        # Start from a known-good state where the configuration is refreshed
+        # by the monitor and logging is completely disabled
+        multihost.master[0].service_sssd('stop')
+        self._stop_kcm(multihost)
+        self._remove_kcm_log_file(multihost)
+        set_param(multihost, 'kcm', 'debug_level', '0')
+        multihost.master[0].service_sssd('start')
+        self._start_kcm(multihost)
+
+        log_lines_pre = self._kcm_log_length(multihost)
+
+        # Debugging is disabled, kinit and make sure that no debug messages
+        # were produced
+        try:
+            ssh = SSHClient(multihost.master[0].sys_hostname,
+                            username='foo3', password='Secret123')
+        except paramiko.ssh_exception.AuthenticationException:
+            pytest.fail("Authentication Failed as user %s" % ('foo3'))
+        else:
+            ssh.execute_cmd('kdestroy')
+            ssh.close()
+
+        log_lines_nodebug = self._kcm_log_length(multihost)
+        assert log_lines_nodebug == log_lines_pre
+
+        # Enable debugging, restart only the kcm service, make sure some
+        # debug messages were produced
+        set_param(multihost, 'kcm', 'debug_level', '9')
+        self._restart_kcm(multihost)
+
+        try:
+            ssh = SSHClient(multihost.master[0].sys_hostname,
+                            username='foo3', password='Secret123')
+        except paramiko.ssh_exception.AuthenticationException:
+            pytest.fail("Authentication Failed as user %s" % ('foo3'))
+        else:
+            ssh.execute_cmd('kdestroy')
+            ssh.close()
+
+        log_lines_debug = self._kcm_log_length(multihost)
+        assert log_lines_debug > log_lines_pre + 100
