6945a91996f6f447209096329b0ef90c9569f7db
Pavel Březina <pbrezina@redhat.com>
2019-02-05 14:13:18 +0100

    netgroups: honor cache_refresh_percent
    The value of cache refresh percent was not passed to cache_req.
    
    Steps to reproduce:
    1. Configure SSSD like so:
    ```ini
    [nss]
    entry_cache_nowait_percentage=10
    memcache_timeout=0
    
    [domain/mydomain]
    entry_cache_timeout = 30
    ```
    
    2. Run SSSD
    3. Fetch netgroup for the first time
    4. Wait 15 seconds (midpoint refresh will be triggered from 10-29 seconds interval)
    5. Fetch netgroup again, see in logs that midpoint refresh was triggered
    
    Resolves:
    https://pagure.io/SSSD/sssd/issue/3947
    
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

--- src/responder/nss/nss_enum.c
+++ src/responder/nss/nss_enum.c
@@ -93,7 +93,8 @@ nss_setent_internal_send(TALLOC_CTX *mem_ctx,
     /* Create new object. */
     state->enum_ctx->is_ready = false;
     subreq = cache_req_send(req, ev, cli_ctx->rctx, cli_ctx->rctx->ncache,
-                            0, CACHE_REQ_POSIX_DOM, NULL, data);
+                            state->nss_ctx->cache_refresh_percent,
+                            CACHE_REQ_POSIX_DOM, NULL, data);
     if (subreq == NULL) {
         DEBUG(SSSDBG_CRIT_FAILURE, "Unable to send cache request!\n");
         ret = ENOMEM;
