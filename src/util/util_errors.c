d575d85c06093a560441aa2c177284bbe9891300
Alexey Tikhonov <atikhono@redhat.com>
2019-02-14 16:46:43 +0100

    Util: fixed mistype in error string representation
    Resolves: https://pagure.io/SSSD/sssd/issue/3916
    
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

--- src/util/util_errors.c
+++ src/util/util_errors.c
@@ -105,7 +105,7 @@ struct err_string error_to_str[] = {
     { "No proxy server for secrets available"}, /* ERR_SEC_NO_PROXY */
     { "The maximum number of stored secrets has been reached" }, /* ERR_SEC_INVALID_TOO_MANY_SECRETS */
     { "The secret payload size is too large" }, /* ERR_SEC_PAYLOAD_SIZE_IS_TOO_LARGE */
-    { "No authentication methode available" }, /* ERR_NO_AUTH_METHOD_AVAILABLE */
+    { "No authentication method available" }, /* ERR_NO_AUTH_METHOD_AVAILABLE */
     { "Smartcard authentication not supported" }, /* ERR_SC_AUTH_NOT_SUPPORTED */
     { "Malformed input KCM packet" }, /* ERR_KCM_MALFORMED_IN_PKT */
     { "KCM operation not implemented" }, /* ERR_KCM_OP_NOT_IMPLEMENTED */
