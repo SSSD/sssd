d7a7ef8839985ce0e1f347aceb03ae71ed456e52
Sumit Bose <sbose@redhat.com>
2019-03-27 21:05:06 +0100

    PAM: add initial prompting configuration
    Add new section for sssd.conf to allow more flexible prompting during
    authentication.
    
    Related to https://pagure.io/SSSD/sssd/issue/3264
    
    Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

--- src/responder/pam/pamsrv_cmd.c
+++ src/responder/pam/pamsrv_cmd.c
@@ -1008,6 +1008,14 @@ static void pam_reply(struct pam_auth_req *preq)
         }
     }
 
+    if (pd->cmd == SSS_PAM_PREAUTH) {
+        ret = pam_eval_prompting_config(pctx, pd);
+        if (ret != EOK) {
+            DEBUG(SSSDBG_OP_FAILURE, "Failed to add prompting information, "
+                                     "using defaults.\n");
+        }
+    }
+
     /*
      * Export non-overridden shell to tlog-rec-session when opening the session
      */
