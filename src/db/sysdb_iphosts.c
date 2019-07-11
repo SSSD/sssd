25c8cf242ba8669b018d37446101fc69bba8a59c
Samuel Cabrero <scabrero@suse.de>
2019-07-11 14:29:53 +0200

    SYSDB: Add support for enumerating hosts
    Signed-off-by: Samuel Cabrero <scabrero@suse.de>

--- src/db/sysdb_iphosts.c
+++ src/db/sysdb_iphosts.c
@@ -832,3 +832,44 @@ fail:
 
     return ret;
 }
+
+errno_t
+sysdb_enumhostent(TALLOC_CTX *mem_ctx,
+                  struct sss_domain_info *domain,
+                  struct ldb_result **_res)
+{
+    errno_t ret;
+    TALLOC_CTX *tmp_ctx;
+    static const char *attrs[] = {
+        SYSDB_NAME,
+        SYSDB_NAME_ALIAS,
+        SYSDB_IP_HOST_ATTR_ADDRESS,
+        NULL,
+    };
+    struct ldb_result *res = NULL;
+    struct ldb_message **msgs;
+    size_t msgs_count;
+
+    tmp_ctx = talloc_new(NULL);
+    if (tmp_ctx == NULL) {
+        return ENOMEM;
+    }
+
+    ret = sysdb_search_hosts(mem_ctx, domain, "",
+                             attrs, &msgs_count, &msgs);
+    if (ret == EOK) {
+        res = talloc_zero(mem_ctx, struct ldb_result);
+        if (res == NULL) {
+            ret = ENOMEM;
+            goto done;
+        }
+        res->count = msgs_count;
+        res->msgs = talloc_steal(res, msgs);
+    }
+
+    *_res = res;
+
+done:
+    talloc_free(tmp_ctx);
+    return ret;
+}
