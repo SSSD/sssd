SSSD/sssd

commit 8aa0dfdf6e36fa90855c0f35a4dfa57139ad6504

util/crypto/nss/nss_nite: params sanitization
 - `key` params made const
 - added omitted sanity checks of pointer params

Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

src/util/crypto/nss/nss_nite.c
@@ -37,7 +37,7 @@ struct cipher_mech {
 };
 
 int sss_encrypt(TALLOC_CTX *mem_ctx, enum encmethod enctype,
-                uint8_t *key, size_t keylen,
+                const uint8_t *key, size_t keylen,
                 const uint8_t *plaintext, size_t plainlen,
                 uint8_t **ciphertext, size_t *cipherlen)
 {
@@ -171,7 +171,7 @@ done:
 }
 
 int sss_decrypt(TALLOC_CTX *mem_ctx, enum encmethod enctype,
-                uint8_t *key, size_t keylen,
+                const uint8_t *key, size_t keylen,
                 const uint8_t *ciphertext, size_t cipherlen,
                 uint8_t **plaintext, size_t *plainlen)
 {

src/util/crypto/libcrypto/crypto_nite.c
@@ -43,7 +43,7 @@ struct cipher_mech {
 };
 
 int sss_encrypt(TALLOC_CTX *mem_ctx, enum encmethod enctype,
-                uint8_t *key, size_t keylen,
+                const uint8_t *key, size_t keylen,
                 const uint8_t *plaintext, size_t plainlen,
                 uint8_t **ciphertext, size_t *cipherlen)
 {
@@ -60,7 +60,7 @@ int sss_encrypt(TALLOC_CTX *mem_ctx, enum encmethod enctype,
     size_t slen;
     int ret;
 
-    if (!plaintext || !plainlen) return EINVAL;
+    if (!plaintext || !plainlen || !ciphertext || !cipherlen) return EINVAL;
 
     if (enctype != AES256CBC_HMAC_SHA256) return EINVAL;
     cipher = mechs[AES256CBC_HMAC_SHA256].cipher();
@@ -165,7 +165,7 @@ done:
 }
 
 int sss_decrypt(TALLOC_CTX *mem_ctx, enum encmethod enctype,
-                uint8_t *key, size_t keylen,
+                const uint8_t *key, size_t keylen,
                 const uint8_t *ciphertext, size_t cipherlen,
                 uint8_t **plaintext, size_t *plainlen)
 {
@@ -183,7 +183,7 @@ int sss_decrypt(TALLOC_CTX *mem_ctx, enum encmethod enctype,
     size_t slen;
     int ret;
 
-    if (!ciphertext || !cipherlen) return EINVAL;
+    if (!ciphertext || !cipherlen || !plaintext || !plainlen) return EINVAL;
 
     if (enctype != AES256CBC_HMAC_SHA256) return EINVAL;
     cipher = mechs[AES256CBC_HMAC_SHA256].cipher();

src/util/crypto/sss_crypto.h
@@ -63,11 +63,11 @@ enum encmethod {
 };
 
 int sss_encrypt(TALLOC_CTX *mem_ctx, enum encmethod enctype,
-                uint8_t *key, size_t keylen,
+                const uint8_t *key, size_t keylen,
                 const uint8_t *plaintext, size_t plainlen,
                 uint8_t **ciphertext, size_t *cipherlen);
 int sss_decrypt(TALLOC_CTX *mem_ctx, enum encmethod enctype,
-                uint8_t *key, size_t keylen,
+                const uint8_t *key, size_t keylen,
                 const uint8_t *ciphertext, size_t cipherlen,
                 uint8_t **plaintext, size_t *plainlen);
 

commit ec7bb3c25f7a1df56713dcdc0b386b985b82bafc

crypto/nss: some nss_ctx_init() params made const
This patch fixes compilation issues introduced in 8aa0dfd :
as `key` parameter of sss_encrypt() and sss_decrypt() became const,
changes in signature of nss_ctx_init() were required to follow up.
For more details see https://github.com/SSSD/sssd/pull/846

src/util/crypto/nss/nss_crypto.h
@@ -28,12 +28,6 @@
 #include <base64.h>
 #include <talloc.h>
 
-#define MAKE_SECITEM(sdata, slen, sitem) do { \
-    (sitem)->type = (siBuffer); \
-    (sitem)->data = (sdata);    \
-    (sitem)->len  = (slen);     \
-} while(0)
-
 struct sss_nss_crypto_ctx {
     PK11SlotInfo *slot;
     PK11Context  *ectx;
@@ -58,8 +52,8 @@ enum crypto_mech_op {
 
 int nss_ctx_init(TALLOC_CTX *mem_ctx,
                  struct crypto_mech_data *mech_props,
-                 uint8_t *key, int keylen,
-                 uint8_t *iv, int ivlen,
+                 const uint8_t *key, int keylen,
+                 const uint8_t *iv, int ivlen,
                  struct sss_nss_crypto_ctx **_cctx);
 int nss_crypto_init(struct crypto_mech_data *mech_props,
                     enum crypto_mech_op crypto_op,

src/util/crypto/nss/nss_nite.c
@@ -90,7 +90,9 @@ int sss_encrypt(TALLOC_CTX *mem_ctx, enum encmethod enctype,
 
     if (ivlen != 0) {
         ret = sss_generate_csprng_buffer(out, ivlen);
-        if (ret) return ret;
+        if (ret != EOK) {
+            goto done;
+        }
     }
 
     ret = nss_ctx_init(tmp_ctx, enc, key, keylen, out, ivlen, &cctx);

src/util/crypto/nss/nss_util.c
@@ -23,6 +23,8 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <string.h>
+
 #include "config.h"
 
 #include <prinit.h>
@@ -145,10 +147,30 @@ done:
     return ret;
 }
 
+static inline int sss_fill_secitem(const uint8_t *sdata, int slen,
+                                   SECItem *sitem)
+{
+    sitem->type = siBuffer;
+
+    /* This is not public function, so no sanity checks of parameters */
+    sitem->len = slen;
+
+    if (sdata == NULL) {
+        sitem->data = NULL;
+    } else {
+        sitem->data = talloc_memdup(sitem, sdata, slen);
+        if (sitem->data == NULL) {
+            return ENOMEM;
+        }
+    }
+
+    return EOK;
+}
+
 int nss_ctx_init(TALLOC_CTX *mem_ctx,
                  struct crypto_mech_data *mech_props,
-                 uint8_t *key, int keylen,
-                 uint8_t *iv, int ivlen,
+                 const uint8_t *key, int keylen,
+                 const uint8_t *iv, int ivlen,
                  struct sss_nss_crypto_ctx **_cctx)
 {
     struct sss_nss_crypto_ctx *cctx;
@@ -177,7 +199,10 @@ int nss_ctx_init(TALLOC_CTX *mem_ctx,
             goto done;
         }
         if (key) {
-            MAKE_SECITEM(key, keylen, cctx->key);
+            ret = sss_fill_secitem(key, keylen, cctx->key);
+            if (ret != EOK) {
+                goto done;
+            }
         } else {
             ret = generate_random_key(cctx, cctx->slot,
                                       mech_props, &cctx->key);
@@ -198,7 +223,10 @@ int nss_ctx_init(TALLOC_CTX *mem_ctx,
             goto done;
         }
         if (iv) {
-            MAKE_SECITEM(iv, ivlen, cctx->iv);
+            ret = sss_fill_secitem(iv, ivlen, cctx->iv);
+            if (ret != EOK) {
+                goto done;
+            }
         } else {
             ret = generate_random_key(cctx, cctx->slot,
                                       mech_props, &cctx->iv);
@@ -262,10 +290,10 @@ int nss_crypto_init(struct crypto_mech_data *mech_props,
         cctx->sparam = SECITEM_AllocItem(NULL, NULL, 0);
         if (cctx->sparam == NULL) {
             DEBUG(SSSDBG_CRIT_FAILURE, "Failure to allocate SECItem\n");
-            ret = EIO;
+            ret = ENOMEM;
             goto done;
         }
-        MAKE_SECITEM(NULL, 0, cctx->sparam);
+        cctx->sparam->type = siBuffer;
     }
 
     /* Create cipher context */