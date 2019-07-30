2d549c059efa8605793e98e9ececf0be73ec59da
Samuel Cabrero <scabrero@suse.de>
2019-07-30 18:36:34 +0200

    AD: Add support for iphost enumeration
    Signed-off-by: Samuel Cabrero <scabrero@suse.de>
    
    Reviewed-by: Sumit Bose <sbose@redhat.com>

--- src/providers/ad/ad_resolver.c
+++ src/providers/ad/ad_resolver.c
@@ -21,7 +21,9 @@
 */
 
 #include "providers/ad/ad_common.h"
+#include "providers/ad/ad_domain_info.h"
 #include "providers/ad/ad_resolver.h"
+#include "providers/ldap/sdap_async_resolver_enum.h"
 
 static errno_t
 ad_resolver_setup_enumeration(struct be_ctx *be_ctx,
@@ -213,8 +215,16 @@ ad_resolver_setup_tasks(struct be_ctx *be_ctx,
 }
 
 struct ad_resolver_enum_state {
-    int dummy;
+    struct ad_resolver_ctx *resolver_ctx;
+    struct sdap_id_op *sdap_op;
+    struct tevent_context *ev;
+
+    const char *realm;
+    struct sdap_domain *sdom;
+    struct sdap_domain *sditer;
 };
+
+static void ad_resolver_enumeration_conn_done(struct tevent_req *subreq);
 
 struct tevent_req *
 ad_resolver_enumeration_send(TALLOC_CTX *mem_ctx,
@@ -224,15 +234,232 @@ ad_resolver_enumeration_send(TALLOC_CTX *mem_ctx,
                              void *pvt)
 {
     struct ad_resolver_enum_state *state;
+    struct ad_resolver_ctx *ctx;
     struct tevent_req *req;
+    struct tevent_req *subreq;
+    errno_t ret;
+    struct sdap_id_ctx *sdap_id_ctx;
 
     req = tevent_req_create(mem_ctx, &state, struct ad_resolver_enum_state);
     if (req == NULL) {
         return NULL;
     }
 
+    ctx = talloc_get_type(pvt, struct ad_resolver_ctx);
+    if (ctx == NULL) {
+        DEBUG(SSSDBG_CRIT_FAILURE, "Cannot retrieve ad_resolver_ctx!\n");
+        ret = EFAULT;
+        goto fail;
+    }
+
+    sdap_id_ctx = ctx->ad_id_ctx->sdap_id_ctx;
+
+    state->resolver_ctx = ctx;
+    state->ev = ev;
+    state->sdom = sdap_id_ctx->opts->sdom;
+    state->sditer = state->sdom;
+    state->realm = dp_opt_get_cstring(ctx->ad_id_ctx->ad_options->basic,
+                                      AD_KRB5_REALM);
+    if (state->realm == NULL) {
+        DEBUG(SSSDBG_CONF_SETTINGS, "Missing realm\n");
+        ret = EINVAL;
+        goto fail;
+    }
+
+    state->sdap_op = sdap_id_op_create(state, sdap_id_ctx->conn->conn_cache);
+    if (state->sdap_op == NULL) {
+        DEBUG(SSSDBG_OP_FAILURE, "sdap_id_op_create failed.\n");
+        ret = ENOMEM;
+        goto fail;
+    }
+
+    subreq = sdap_id_op_connect_send(state->sdap_op, state, &ret);
+    if (subreq == NULL) {
+        DEBUG(SSSDBG_OP_FAILURE, "sdap_id_op_connect_send failed: %d(%s).\n",
+                                  ret, strerror(ret));
+        goto fail;
+    }
+    tevent_req_set_callback(subreq, ad_resolver_enumeration_conn_done, req);
+
+    return req;
+
+fail:
+    tevent_req_error(req, ret);
+    tevent_req_post(req, ev);
+    return req;
+}
+
+static void ad_resolver_enumeration_master_done(struct tevent_req *subreq);
+
+static void
+ad_resolver_enumeration_conn_done(struct tevent_req *subreq)
+{
+    struct tevent_req *req = tevent_req_callback_data(subreq,
+                                                      struct tevent_req);
+    struct ad_resolver_enum_state *state = tevent_req_data(req,
+                                                 struct ad_resolver_enum_state);
+    struct sdap_id_ctx *id_ctx = state->resolver_ctx->ad_id_ctx->sdap_id_ctx;
+    int ret, dp_error;
+
+    ret = sdap_id_op_connect_recv(subreq, &dp_error);
+    talloc_zfree(subreq);
+    if (ret != EOK) {
+        if (dp_error == DP_ERR_OFFLINE) {
+            DEBUG(SSSDBG_TRACE_FUNC,
+                  "Backend is marked offline, retry later!\n");
+            tevent_req_done(req);
+        } else {
+            DEBUG(SSSDBG_MINOR_FAILURE,
+                  "Domain enumeration failed to connect to " \
+                   "LDAP server: (%d)[%s]\n", ret, strerror(ret));
+            tevent_req_error(req, ret);
+        }
+        return;
+    }
+
+    subreq = ad_master_domain_send(state, state->ev, id_ctx->conn,
+                                   state->sdap_op, state->sdom->dom->name);
+    if (subreq == NULL) {
+        DEBUG(SSSDBG_OP_FAILURE, "ad_master_domain_send failed.\n");
+        tevent_req_error(req, ret);
+        return;
+    }
+    tevent_req_set_callback(subreq, ad_resolver_enumeration_master_done, req);
+}
+
+static errno_t
+ad_resolver_enum_sdom(struct tevent_req *req,
+                      struct sdap_domain *sd,
+                      struct sdap_resolver_ctx *sdap_resolver_ctx,
+                      struct ad_id_ctx *id_ctx);
+
+static void
+ad_resolver_enumeration_master_done(struct tevent_req *subreq)
+{
+    errno_t ret;
+    struct tevent_req *req = tevent_req_callback_data(subreq,
+                                                      struct tevent_req);
+    struct ad_resolver_enum_state *state = tevent_req_data(req,
+                                                struct ad_resolver_enum_state);
+    char *flat_name;
+    char *master_sid;
+    char *forest;
+    struct ad_id_ctx *ad_id_ctx;
+
+    ret = ad_master_domain_recv(subreq, state,
+                                &flat_name, &master_sid, NULL, &forest);
+    talloc_zfree(subreq);
+    if (ret != EOK) {
+        DEBUG(SSSDBG_OP_FAILURE, "Cannot retrieve master domain info\n");
+        tevent_req_error(req, ret);
+        return;
+    }
+
+    ret = sysdb_master_domain_add_info(state->sdom->dom, state->realm,
+                                       flat_name, master_sid, forest, NULL);
+    if (ret != EOK) {
+        DEBUG(SSSDBG_OP_FAILURE, "Cannot save master domain info\n");
+        tevent_req_error(req, ret);
+        return;
+    }
+
+    ad_id_ctx = talloc_get_type(state->sdom->pvt, struct ad_id_ctx);
+    if (ad_id_ctx == NULL) {
+        DEBUG(SSSDBG_CRIT_FAILURE, "Cannot retrieve ad_id_ctx!\n");
+        tevent_req_error(req, EINVAL);
+        return;
+    }
+
+    ret = ad_resolver_enum_sdom(req, state->sdom,
+                                state->resolver_ctx->sdap_resolver_ctx,
+                                ad_id_ctx);
+    if (ret != EOK) {
+        DEBUG(SSSDBG_OP_FAILURE,
+                "Could not enumerate domain %s\n", state->sdom->dom->name);
+        tevent_req_error(req, ret);
+        return;
+    }
+
+    /* Execution will resume in ad_enumeration_done */
+}
+
+static void ad_resolver_enum_sdom_done(struct tevent_req *subreq);
+
+static errno_t
+ad_resolver_enum_sdom(struct tevent_req *req,
+                      struct sdap_domain *sd,
+                      struct sdap_resolver_ctx *sdap_resolver_ctx,
+                      struct ad_id_ctx *id_ctx)
+{
+    struct tevent_req *subreq;
+    struct ad_resolver_enum_state *state = tevent_req_data(req,
+                                                struct ad_resolver_enum_state);
+
+    /* iphosts are searched for in LDAP */
+    subreq = sdap_dom_resolver_enum_send(state, state->ev,
+                                         sdap_resolver_ctx,
+                                         id_ctx->sdap_id_ctx,
+                                         sd,
+                                         id_ctx->ldap_ctx);
+    if (subreq == NULL) {
+        /* The ptask API will reschedule the enumeration on its own on
+         * failure */
+        DEBUG(SSSDBG_OP_FAILURE,
+              "Failed to schedule enumeration, retrying later!\n");
+        return ENOMEM;
+    }
+    tevent_req_set_callback(subreq, ad_resolver_enum_sdom_done, req);
+
+    return EOK;
+}
+
+static void
+ad_resolver_enum_sdom_done(struct tevent_req *subreq)
+{
+    errno_t ret;
+    struct tevent_req *req = tevent_req_callback_data(subreq,
+                                                      struct tevent_req);
+    struct ad_resolver_enum_state *state = tevent_req_data(req,
+                                                struct ad_resolver_enum_state);
+
+    ret = sdap_dom_resolver_enum_recv(subreq);
+    talloc_zfree(subreq);
+    if (ret != EOK) {
+        DEBUG(SSSDBG_OP_FAILURE,
+              "Could not enumerate domain %s\n", state->sditer->dom->name);
+        tevent_req_error(req, ret);
+        return;
+    }
+
+    do {
+        state->sditer = state->sditer->next;
+    } while (state->sditer &&
+             state->sditer->dom->enumerate == false);
+
+    if (state->sditer != NULL) {
+        struct ad_id_ctx *ad_id_ctx;
+
+        ad_id_ctx = talloc_get_type(state->sditer->pvt, struct ad_id_ctx);
+        if (ad_id_ctx == NULL) {
+            DEBUG(SSSDBG_CRIT_FAILURE, "Cannot retrieve ad_id_ctx!\n");
+            tevent_req_error(req, EINVAL);
+            return;
+        }
+
+        ret = ad_resolver_enum_sdom(req, state->sditer,
+                                    state->resolver_ctx->sdap_resolver_ctx,
+                                    ad_id_ctx);
+        if (ret != EOK) {
+            DEBUG(SSSDBG_OP_FAILURE, "Could not enumerate domain %s\n",
+                  state->sditer->dom->name);
+            tevent_req_error(req, ret);
+            return;
+        }
+
+        return;
+    }
+
     tevent_req_done(req);
-    return tevent_req_post(req, ev);
 }
 
 errno_t
