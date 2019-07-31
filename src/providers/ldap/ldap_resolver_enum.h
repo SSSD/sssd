ae6d042cbb6b8a2bfecc37edeb3a1509796f6846
Samuel Cabrero <scabrero@suse.de>
2019-07-31 17:08:08 +0200

    LDAP: Implement iphost cleanup for expired cache entries
    Signed-off-by: Samuel Cabrero <scabrero@suse.de>
    
    Reviewed-by: Sumit Bose <sbose@redhat.com>

--- src/providers/ldap/ldap_resolver_enum.h
+++ src/providers/ldap/ldap_resolver_enum.h
@@ -38,4 +38,7 @@ ldap_resolver_enumeration_send(TALLOC_CTX *mem_ctx,
 errno_t
 ldap_resolver_enumeration_recv(struct tevent_req *req);
 
+errno_t
+ldap_resolver_cleanup(struct sdap_resolver_ctx *ctx);
+
 #endif /* LDAP_RESOLVER_ENUM_H_ */
