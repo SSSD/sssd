28ab338fb45a4187fe64c39bd46b6a77fa5b08b4
Lukas Slebodnik <lslebodn@redhat.com>
2019-09-02 23:15:24 +0000

    pysss_murmur: Fix DeprecationWarning PY_SSIZE_T_CLEAN
    src/tests/pysss_murmur-test.py:93: DeprecationWarning: PY_SSIZE_T_CLEAN will be required for '#' formats
      hash_val = pysss_murmur.murmurhash3(sid_str, 0, seed)
    src/tests/pysss_murmur-test.py:96: DeprecationWarning: PY_SSIZE_T_CLEAN will be required for '#' formats
      hash_val = pysss_murmur.murmurhash3(sid_str, len(sid_str), seed)
    
    These were introduced by https://bugs.python.org/issue36381 to warn about
    an upcoming Python C API change. The meaning of PY_SSIZE_T_CLEAN is described
    in https://python.readthedocs.io/en/stable/c-api/arg.html#strings-and-buffers.
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>\

--- src/python/pysss_murmur.c
+++ src/python/pysss_murmur.c
@@ -20,6 +20,7 @@
 
 #include "config.h"
 
+#define PY_SSIZE_T_CLEAN 1
 #include <Python.h>
 
 #include "util/sss_python.h"
@@ -38,7 +39,7 @@ static PyObject * py_murmurhash3(PyObject *module, PyObject *args)
     long key_len;
     long long seed;
     uint32_t hash;
-    int input_len;
+    Py_ssize_t input_len;
 
     if (!PyArg_ParseTuple(args, sss_py_const_p(char, "z#lL"),
                           &key, &input_len, &key_len, &seed)) {
