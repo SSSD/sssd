ad37fe34ea40bf3be81dd0061c44f6dd3efc5d80
Sumit Bose <sbose@redhat.com>
2019-10-29 12:16:40 +0100

    ipa: add failover to override lookups
    In the ipa_id_get_account_info request failover handling was missing.
    
    Related to https://pagure.io/SSSD/sssd/issue/4114
    
    Reviewed-by: Michal Židek <mzidek@redhat.com>

--- src/providers/ipa/ipa_id.c
+++ src/providers/ipa/ipa_id.c
@@ -640,7 +640,22 @@ static void ipa_id_get_account_info_got_override(struct tevent_req *subreq)
     ret = ipa_get_ad_override_recv(subreq, &dp_error, state,
                                    &state->override_attrs);
     talloc_zfree(subreq);
+
     if (ret != EOK) {
+        ret = sdap_id_op_done(state->op, ret, &dp_error);
+
+        if (dp_error == DP_ERR_OK && ret != EOK) {
+            /* retry */
+            subreq = sdap_id_op_connect_send(state->op, state, &ret);
+            if (subreq == NULL) {
+                DEBUG(SSSDBG_OP_FAILURE, "sdap_id_op_connect_send failed.\n");
+                goto fail;
+            }
+            tevent_req_set_callback(subreq, ipa_id_get_account_info_connected,
+                                    req);
+            return;
+        }
+
         DEBUG(SSSDBG_OP_FAILURE, "IPA override lookup failed: %d\n", ret);
         goto fail;
     }
