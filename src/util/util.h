f25a517ed432778d4484f48ca2aaf0670311e66a
MIZUTA Takeshi <mizuta.takeshi@fujitsu.com>
2020-02-05 17:39:53 +0900

    monitor: Fix check process about multiple starts of sssd when pidfile remains
    If PIDFile is invalid in sssd.service, pidfile remains if sssd terminates abnormally.
    Also, if /var/run is not tmpfs, the pidfile will remain when the OS is forcibly stopped.
    
    In check process about multiple starts of sssd, only the existence of pidfile is checked.
    Fix not only to check if pidfile exists, but also to check if PID exists.
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>

--- src/util/util.h
+++ src/util/util.h
@@ -183,6 +183,7 @@ struct main_context {
 errno_t server_common_rotate_logs(struct confdb_ctx *confdb,
                                   const char *conf_entry);
 int die_if_parent_died(void);
+int check_pidfile(const char *file);
 int pidfile(const char *file);
 int server_setup(const char *name, int flags,
                  uid_t uid, gid_t gid,
