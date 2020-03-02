SSSD/sssd

commit 735af71a8e169f17fa5462db610a1567c9618e29

be: remember last good server's name instead of fo_server structure
This fo_server may be freed when collapsing servers from SRV lookup
in `collapse_srv_lookup`. This would cause crash when we try to
dereference the pointer.

Resolves:
https://pagure.io/SSSD/sssd/issue/3976

Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

src/providers/backend.h
@@ -58,7 +58,7 @@ struct be_svc_data {
     const char *name;
     struct fo_service *fo_service;
 
-    struct fo_server *last_good_srv;
+    char *last_good_srv;
     time_t last_status_change;
     bool run_callbacks;
 

src/providers/data_provider_fo.c
@@ -588,6 +588,7 @@ errno_t be_resolve_server_process(struct tevent_req *subreq,
     errno_t ret;
     time_t srv_status_change;
     struct be_svc_callback *callback;
+    char *srvname;
 
     ret = fo_resolve_service_recv(subreq, state, &state->srv);
     switch (ret) {
@@ -667,12 +668,21 @@ errno_t be_resolve_server_process(struct tevent_req *subreq,
 
     /* now call all svc callbacks if server changed or if it is explicitly
      * requested or if the server is the same but changed status since last time*/
-    if (state->srv != state->svc->last_good_srv ||
+    if (state->svc->last_good_srv == NULL ||
+        strcmp(fo_get_server_name(state->srv), state->svc->last_good_srv) != 0 ||
         state->svc->run_callbacks ||
         srv_status_change > state->svc->last_status_change) {
-        state->svc->last_good_srv = state->srv;
         state->svc->last_status_change = srv_status_change;
         state->svc->run_callbacks = false;
+
+        srvname = talloc_strdup(state->svc, fo_get_server_name(state->srv));
+        if (srvname == NULL) {
+            DEBUG(SSSDBG_CRIT_FAILURE, "Unable to copy server name\n");
+            return ENOMEM;
+        }
+
+        talloc_free(state->svc->last_good_srv);
+        state->svc->last_good_srv = srvname;
 
         DLIST_FOR_EACH(callback, state->svc->callbacks) {
             callback->fn(callback->private_data, state->srv);

src/providers/data_provider/dp_iface_failover.c
@@ -274,7 +274,6 @@ dp_failover_active_server(TALLOC_CTX *mem_ctx,
                           const char **_server)
 {
     struct be_svc_data *svc;
-    const char *server;
     bool found = false;
 
     DLIST_FOR_EACH(svc, be_ctx->be_fo->svcs) {
@@ -289,17 +288,7 @@ dp_failover_active_server(TALLOC_CTX *mem_ctx,
         return ENOENT;
     }
 
-    if (svc->last_good_srv == NULL) {
-        server = "";
-    } else {
-        server = fo_get_server_name(svc->last_good_srv);
-        if (server == NULL) {
-            DEBUG(SSSDBG_CRIT_FAILURE, "Unable to get server name\n");
-            return ERR_INTERNAL;
-        }
-    }
-
-    *_server = server;
+    *_server = svc->last_good_srv == NULL ? "" : svc->last_good_srv;
 
     return EOK;
 }

commit fd1339306abe252e5f1a9f39afae476296e7d71e

failover: make sure we switch to another server if only port differs
This is a regression introduced in 735af71a8e169f17fa5462db610a1567c9618e29.
After this commit we checked only server name instead of name and port combo.

Steps to reproduce:
1. Configure SSSD to use two servers with same name but different ports
```
[domain/LDAP]
debug_level=0xFFF0
id_provider = ldap
ldap_uri = ldap://$SERVER1:12345,ldap://$SERVER1:389
ldap_tls_cacertdir = /etc/openldap/certs
ldap_search_base = dc=example,dc=com
```

2. The first server port is unreachable, the second is working.
3. Run sssd and try to resolve user.
4. Without the patch SSSD tries to connect to the first server twice
   because new URI is not constructed and goes offline.

Resolves:
https://pagure.io/SSSD/sssd/issue/4112

Reviewed-by: Michal Židek <mzidek@redhat.com>
(cherry picked from commit 08f0159078aa00d04533ca227679e25d64c452e1)

src/providers/backend.h
@@ -59,6 +59,7 @@ struct be_svc_data {
     struct fo_service *fo_service;
 
     char *last_good_srv;
+    int last_good_port;
     time_t last_status_change;
     bool run_callbacks;
 

src/providers/data_provider_fo.c
@@ -670,6 +670,7 @@ errno_t be_resolve_server_process(struct tevent_req *subreq,
      * requested or if the server is the same but changed status since last time*/
     if (state->svc->last_good_srv == NULL ||
         strcmp(fo_get_server_name(state->srv), state->svc->last_good_srv) != 0 ||
+        fo_get_server_port(state->srv) != state->svc->last_good_port ||
         state->svc->run_callbacks ||
         srv_status_change > state->svc->last_status_change) {
         state->svc->last_status_change = srv_status_change;
@@ -683,6 +684,7 @@ errno_t be_resolve_server_process(struct tevent_req *subreq,
 
         talloc_free(state->svc->last_good_srv);
         state->svc->last_good_srv = srvname;
+        state->svc->last_good_port = fo_get_server_port(state->srv);
 
         DLIST_FOR_EACH(callback, state->svc->callbacks) {
             callback->fn(callback->private_data, state->srv);