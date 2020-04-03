41220021d21b7711a416b5aeb8446c14dd931140
Pavel Březina <pbrezina@redhat.com>
2020-04-03 13:13:47 +0200

    dp: free methods if target is not configured
    Reviewed-by: Alexey Tikhonov <atikhonov@redhat.com>

--- src/providers/data_provider/dp_targets.c
+++ src/providers/data_provider/dp_targets.c
@@ -290,6 +290,12 @@ static errno_t dp_target_init(struct be_ctx *be_ctx,
          * configured so we shall just continue. */
         DEBUG(SSSDBG_CONF_SETTINGS, "Target [%s] is not supported by "
               "module [%s].\n", target->name, target->module_name);
+
+        /* Target is not initialized in this case so we can free
+         * its resources. However this is not an error so we return EOK. */
+        talloc_zfree(target->methods);
+        target->initialized = false;
+
         ret = EOK;
         goto done;
     } else if (ret != EOK) {
@@ -300,7 +306,7 @@ static errno_t dp_target_init(struct be_ctx *be_ctx,
 
 done:
     if (ret != EOK) {
-        talloc_free(target->methods);
+        talloc_zfree(target->methods);
     }
 
     return ret;
