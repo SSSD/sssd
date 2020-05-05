225fe9950f2807d5fb226f6b3be1ff4cefd731f0
Alexey Tikhonov <atikhono@redhat.com>
2020-05-05 19:34:47 +0200

    DEBUG: changed "debug_prg_name" format
    Removed wrapping "[sssd[...]]" from "debug_prg_name" as this doesn't
    carry any information but eats 8 characters of debug line.
    For example instead of `[[sssd[ldap_child[12492]]]]` logs will have
    `[ldap_child[12492]]`
    
    I also was considering to remove "debug_prg_name" from the output
    completely but gave up this idea. It makes sense to have program name
    in the output to be able to combine few logs together (sorted by
    timestamp).
    
    Reviewed-by: Pawel Polawski <ppolawsk@redhat.com>
    (cherry picked from commit 00e7b1ada3d1c1071eac79b65c17cd2701c2ae6a)
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>

--- src/responder/ssh/sshsrv.c
+++ src/responder/ssh/sshsrv.c
@@ -229,7 +229,7 @@ int main(int argc, const char *argv[])
 
     sss_set_logger(opt_logger);
 
-    ret = server_setup("sssd[ssh]", 0, uid, gid,
+    ret = server_setup("ssh", 0, uid, gid,
                        CONFDB_SSH_CONF_ENTRY, &main_ctx);
     if (ret != EOK) {
         return 2;
