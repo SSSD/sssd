1889ca60a9c642f0cca60b20a5b94de7a66924f6
Pavel Březina <pbrezina@redhat.com>
2020-08-11 13:27:42 +0200

    ad: connect to the first available server for cldap ping
    Resolves:
    https://github.com/SSSD/sssd/issues/3743
    
    Reviewed-by: Sumit Bose <sbose@redhat.com>

--- src/providers/ad/ad_srv.c
+++ src/providers/ad/ad_srv.c
@@ -116,378 +116,6 @@ static errno_t ad_sort_servers_by_dns(TALLOC_CTX *mem_ctx,
     return EOK;
 }
 
-struct ad_get_dc_servers_state {
-    struct fo_server_info *servers;
-    size_t num_servers;
-};
-
-static void ad_get_dc_servers_done(struct tevent_req *subreq);
-
-static struct tevent_req *ad_get_dc_servers_send(TALLOC_CTX *mem_ctx,
-                                                 struct tevent_context *ev,
-                                                 struct resolv_ctx *resolv_ctx,
-                                                 const char *discovery_domain,
-                                                 const char *site)
-{
-    struct ad_get_dc_servers_state *state = NULL;
-    struct tevent_req *req = NULL;
-    struct tevent_req *subreq = NULL;
-    const char **domains = NULL;
-    errno_t ret;
-
-    req = tevent_req_create(mem_ctx, &state,
-                            struct ad_get_dc_servers_state);
-    if (req == NULL) {
-        DEBUG(SSSDBG_CRIT_FAILURE, "tevent_req_create() failed\n");
-        return NULL;
-    }
-
-    domains = talloc_zero_array(state, const char *, 3);
-    if (domains == NULL) {
-        ret = ENOMEM;
-        goto immediately;
-    }
-
-    if (site == NULL) {
-        DEBUG(SSSDBG_TRACE_FUNC, "Looking up domain controllers in domain "
-              "%s\n", discovery_domain);
-
-        domains[0] = talloc_strdup(domains, discovery_domain);
-        if (domains[0] == NULL) {
-            ret = ENOMEM;
-            goto immediately;
-        }
-    } else {
-        DEBUG(SSSDBG_TRACE_FUNC, "Looking up domain controllers in domain "
-              "%s and site %s\n", discovery_domain, site);
-
-        domains[0] = ad_site_dns_discovery_domain(domains,
-                                                  site, discovery_domain);
-        if (domains[0] == NULL) {
-            ret = ENOMEM;
-            goto immediately;
-        }
-
-        domains[1] = talloc_strdup(domains, discovery_domain);
-        if (domains[1] == NULL) {
-            ret = ENOMEM;
-            goto immediately;
-        }
-    }
-
-    subreq = fo_discover_srv_send(state, ev, resolv_ctx,
-                                  "ldap", FO_PROTO_TCP, domains);
-    if (subreq == NULL) {
-        ret = ENOMEM;
-        goto immediately;
-    }
-
-    tevent_req_set_callback(subreq, ad_get_dc_servers_done, req);
-
-    return req;
-
-immediately:
-    tevent_req_error(req, ret);
-    tevent_req_post(req, ev);
-
-    return req;
-}
-
-static void ad_get_dc_servers_done(struct tevent_req *subreq)
-{
-    struct ad_get_dc_servers_state *state = NULL;
-    struct tevent_req *req = NULL;
-    char *domain = NULL;
-    errno_t ret;
-
-    req = tevent_req_callback_data(subreq, struct tevent_req);
-    state = tevent_req_data(req, struct ad_get_dc_servers_state);
-
-    ret = fo_discover_srv_recv(state, subreq, &domain, NULL,
-                               &state->servers, &state->num_servers);
-    talloc_zfree(subreq);
-    if (ret != EOK) {
-        goto done;
-    }
-
-    DEBUG(SSSDBG_TRACE_FUNC, "Found %zu domain controllers in domain %s\n",
-                              state->num_servers, domain);
-
-done:
-    if (ret != EOK) {
-        tevent_req_error(req, ret);
-        return;
-    }
-
-    tevent_req_done(req);
-}
-
-static int ad_get_dc_servers_recv(TALLOC_CTX *mem_ctx,
-                                  struct tevent_req *req,
-                                  struct fo_server_info **_dcs,
-                                  size_t *_num_dcs)
-{
-    struct ad_get_dc_servers_state *state = NULL;
-    state = tevent_req_data(req, struct ad_get_dc_servers_state);
-
-    TEVENT_REQ_RETURN_ON_ERROR(req);
-
-    *_dcs = talloc_steal(mem_ctx, state->servers);
-    *_num_dcs = state->num_servers;
-
-    return EOK;
-}
-
-struct ad_get_client_site_state {
-    struct tevent_context *ev;
-    struct be_resolv_ctx *be_res;
-    enum host_database *host_db;
-    struct sdap_options *opts;
-    const char *ad_domain;
-    bool ad_use_ldaps;
-    struct fo_server_info *dcs;
-    size_t num_dcs;
-    size_t dc_index;
-    struct fo_server_info dc;
-
-    struct sdap_handle *sh;
-    char *site;
-    char *forest;
-};
-
-static errno_t ad_get_client_site_next_dc(struct tevent_req *req);
-static void ad_get_client_site_connect_done(struct tevent_req *subreq);
-static void ad_get_client_site_done(struct tevent_req *subreq);
-
-struct tevent_req *ad_get_client_site_send(TALLOC_CTX *mem_ctx,
-                                           struct tevent_context *ev,
-                                           struct be_resolv_ctx *be_res,
-                                           enum host_database *host_db,
-                                           struct sdap_options *opts,
-                                           const char *ad_domain,
-                                           bool ad_use_ldaps,
-                                           struct fo_server_info *dcs,
-                                           size_t num_dcs)
-{
-    struct ad_get_client_site_state *state = NULL;
-    struct tevent_req *req = NULL;
-    errno_t ret;
-
-    req = tevent_req_create(mem_ctx, &state,
-                            struct ad_get_client_site_state);
-    if (req == NULL) {
-        DEBUG(SSSDBG_CRIT_FAILURE, "tevent_req_create() failed\n");
-        return NULL;
-    }
-
-    if (be_res == NULL || host_db == NULL || opts == NULL) {
-        ret = EINVAL;
-        goto immediately;
-    }
-
-    state->ev = ev;
-    state->be_res = be_res;
-    state->host_db = host_db;
-    state->opts = opts;
-    state->ad_domain = ad_domain;
-    state->ad_use_ldaps = ad_use_ldaps;
-    state->dcs = dcs;
-    state->num_dcs = num_dcs;
-
-    state->dc_index = 0;
-    ret = ad_get_client_site_next_dc(req);
-    if (ret == EOK) {
-        ret = ENOENT;
-        goto immediately;
-    } else if (ret != EAGAIN) {
-        goto immediately;
-    }
-
-    return req;
-
-immediately:
-    if (ret == EOK) {
-        tevent_req_done(req);
-    } else {
-        tevent_req_error(req, ret);
-    }
-    tevent_req_post(req, ev);
-
-    return req;
-}
-
-static errno_t ad_get_client_site_next_dc(struct tevent_req *req)
-{
-    struct ad_get_client_site_state *state = NULL;
-    struct tevent_req *subreq = NULL;
-    errno_t ret;
-
-    state = tevent_req_data(req, struct ad_get_client_site_state);
-
-    if (state->dc_index >= state->num_dcs) {
-        ret = EOK;
-        goto done;
-    }
-
-    state->dc = state->dcs[state->dc_index];
-
-    subreq = sdap_connect_host_send(state, state->ev, state->opts,
-                                    state->be_res->resolv,
-                                    state->be_res->family_order,
-                                    state->host_db,
-                                    "cldap",
-                                    state->dc.host,
-                                    state->dc.port,
-                                    false);
-    if (subreq == NULL) {
-        ret = ENOMEM;
-        goto done;
-    }
-
-    tevent_req_set_callback(subreq, ad_get_client_site_connect_done, req);
-
-    state->dc_index++;
-    ret = EAGAIN;
-
-done:
-    return ret;
-}
-
-static void ad_get_client_site_connect_done(struct tevent_req *subreq)
-{
-    struct ad_get_client_site_state *state = NULL;
-    struct tevent_req *req = NULL;
-    static const char *attrs[] = {AD_AT_NETLOGON, NULL};
-    char *filter = NULL;
-    char *ntver = NULL;
-    errno_t ret;
-
-    req = tevent_req_callback_data(subreq, struct tevent_req);
-    state = tevent_req_data(req, struct ad_get_client_site_state);
-
-    ret = sdap_connect_host_recv(state, subreq, &state->sh);
-    talloc_zfree(subreq);
-    if (ret != EOK) {
-        DEBUG(SSSDBG_MINOR_FAILURE, "Unable to connect to domain controller "
-              "[%s:%d]\n", state->dc.host, state->dc.port);
-
-        ret = ad_get_client_site_next_dc(req);
-        if (ret == EOK) {
-            ret = ENOENT;
-        }
-
-        goto done;
-    }
-
-    ntver = sss_ldap_encode_ndr_uint32(state, NETLOGON_NT_VERSION_5EX |
-                                       NETLOGON_NT_VERSION_WITH_CLOSEST_SITE);
-    if (ntver == NULL) {
-        ret = ENOMEM;
-        goto done;
-    }
-
-    filter = talloc_asprintf(state, "(&(%s=%s)(%s=%s))",
-                             AD_AT_DNS_DOMAIN, state->ad_domain,
-                             AD_AT_NT_VERSION, ntver);
-    if (filter == NULL) {
-        ret = ENOMEM;
-        goto done;
-    }
-
-    subreq = sdap_get_generic_send(state, state->ev, state->opts, state->sh,
-                                   "", LDAP_SCOPE_BASE, filter,
-                                   attrs, NULL, 0,
-                                   dp_opt_get_int(state->opts->basic,
-                                                  SDAP_SEARCH_TIMEOUT),
-                                   false);
-    if (subreq == NULL) {
-        ret = ENOMEM;
-        goto done;
-    }
-
-    tevent_req_set_callback(subreq, ad_get_client_site_done, req);
-
-    ret = EAGAIN;
-
-done:
-    if (ret == EOK) {
-        tevent_req_done(req);
-    } else if (ret != EAGAIN) {
-        tevent_req_error(req, ret);
-    }
-
-    return;
-}
-
-static void ad_get_client_site_done(struct tevent_req *subreq)
-{
-    struct ad_get_client_site_state *state = NULL;
-    struct tevent_req *req = NULL;
-    struct sysdb_attrs **reply = NULL;
-    size_t reply_count;
-    errno_t ret;
-
-    req = tevent_req_callback_data(subreq, struct tevent_req);
-    state = tevent_req_data(req, struct ad_get_client_site_state);
-
-    ret = sdap_get_generic_recv(subreq, state, &reply_count, &reply);
-    talloc_zfree(subreq);
-
-    /* we're done with this LDAP, close connection */
-    talloc_zfree(state->sh);
-    if (ret != EOK) {
-        DEBUG(SSSDBG_OP_FAILURE, "Unable to get netlogon information\n");
-
-        ret = ad_get_client_site_next_dc(req);
-        if (ret == EOK) {
-            ret = ENOENT;
-        }
-        goto done;
-    }
-
-    if (reply_count == 0) {
-        DEBUG(SSSDBG_OP_FAILURE, "No netlogon information retrieved\n");
-        ret = ENOENT;
-        goto done;
-    }
-
-    ret = netlogon_get_domain_info(state, reply[0], true, NULL, &state->site,
-                                   &state->forest);
-    if (ret != EOK) {
-        DEBUG(SSSDBG_OP_FAILURE, "Unable to retrieve site name [%d]: %s\n",
-                                  ret, strerror(ret));
-        ret = ENOENT;
-        goto done;
-    }
-
-    DEBUG(SSSDBG_TRACE_FUNC, "Found site: %s\n", state->site);
-    DEBUG(SSSDBG_TRACE_FUNC, "Found forest: %s\n", state->forest);
-
-done:
-    if (ret != EOK) {
-        tevent_req_error(req, ret);
-        return;
-    }
-
-    tevent_req_done(req);
-}
-
-int ad_get_client_site_recv(TALLOC_CTX *mem_ctx,
-                            struct tevent_req *req,
-                            const char **_site,
-                            const char **_forest)
-{
-    struct ad_get_client_site_state *state = NULL;
-    state = tevent_req_data(req, struct ad_get_client_site_state);
-
-    TEVENT_REQ_RETURN_ON_ERROR(req);
-
-    *_site = talloc_steal(mem_ctx, state->site);
-    *_forest = talloc_steal(mem_ctx, state->forest);
-
-    return EOK;
-}
-
 struct ad_srv_plugin_ctx {
     struct be_ctx *be_ctx;
     struct be_resolv_ctx *be_res;
@@ -610,8 +238,7 @@ struct ad_srv_plugin_state {
     size_t num_backup_servers;
 };
 
-static void ad_srv_plugin_dcs_done(struct tevent_req *subreq);
-static void ad_srv_plugin_site_done(struct tevent_req *subreq);
+static void ad_srv_plugin_ping_done(struct tevent_req *subreq);
 static void ad_srv_plugin_servers_done(struct tevent_req *subreq);
 
 /* 1. Do a DNS lookup to find any DC in domain
@@ -677,15 +304,16 @@ struct tevent_req *ad_srv_plugin_send(TALLOC_CTX *mem_ctx,
 
     DEBUG(SSSDBG_TRACE_FUNC, "About to find domain controllers\n");
 
-    subreq = ad_get_dc_servers_send(state, ev, ctx->be_res->resolv,
-                                    state->discovery_domain,
-                                    state->ctx->current_site);
+    subreq = ad_cldap_ping_send(state, ev, ctx->opts, ctx->be_res,
+                                ctx->host_dbs, ctx->ad_domain,
+                                state->discovery_domain,
+                                state->ctx->current_site);
     if (subreq == NULL) {
         ret = ENOMEM;
         goto immediately;
     }
 
-    tevent_req_set_callback(subreq, ad_srv_plugin_dcs_done, req);
+    tevent_req_set_callback(subreq, ad_srv_plugin_ping_done, req);
 
     return req;
 
@@ -696,52 +324,7 @@ immediately:
     return req;
 }
 
-static void ad_srv_plugin_dcs_done(struct tevent_req *subreq)
-{
-    struct ad_srv_plugin_state *state = NULL;
-    struct tevent_req *req = NULL;
-    struct fo_server_info *dcs = NULL;
-    size_t num_dcs = 0;
-    errno_t ret;
-
-    req = tevent_req_callback_data(subreq, struct tevent_req);
-    state = tevent_req_data(req, struct ad_srv_plugin_state);
-
-    ret = ad_get_dc_servers_recv(state, subreq, &dcs, &num_dcs);
-    talloc_zfree(subreq);
-    if (ret != EOK) {
-        goto done;
-    }
-
-    DEBUG(SSSDBG_TRACE_FUNC, "About to locate suitable site\n");
-
-    subreq = ad_get_client_site_send(state, state->ev,
-                                     state->ctx->be_res,
-                                     state->ctx->host_dbs,
-                                     state->ctx->opts,
-                                     state->discovery_domain,
-                                     state->ctx->ad_use_ldaps,
-                                     dcs, num_dcs);
-    if (subreq == NULL) {
-        ret = ENOMEM;
-        goto done;
-    }
-
-    tevent_req_set_callback(subreq, ad_srv_plugin_site_done, req);
-
-    ret = EAGAIN;
-
-done:
-    if (ret == EOK) {
-        tevent_req_done(req);
-    } else if (ret != EAGAIN) {
-        tevent_req_error(req, ret);
-    }
-
-    return;
-}
-
-static void ad_srv_plugin_site_done(struct tevent_req *subreq)
+static void ad_srv_plugin_ping_done(struct tevent_req *subreq)
 {
     struct ad_srv_plugin_state *state = NULL;
     struct tevent_req *req = NULL;
@@ -752,8 +335,9 @@ static void ad_srv_plugin_site_done(struct tevent_req *subreq)
     req = tevent_req_callback_data(subreq, struct tevent_req);
     state = tevent_req_data(req, struct ad_srv_plugin_state);
 
-    ret = ad_get_client_site_recv(state, subreq, &state->site, &state->forest);
+    ret = ad_cldap_ping_recv(state, subreq, &state->site, &state->forest);
     talloc_zfree(subreq);
+
     /* Ignore AD site found by dns discovery if specific site is set in
      * configuration file. */
     if (state->ctx->ad_site_override != NULL) {
