b35532cd83bad734977d244ef2992566fb9cd3ec
Alexey Tikhonov <atikhono@redhat.com>
2020-10-02 16:23:16 +0200

    UTILS: adds helper to convert authtok type to str
    This patch introduces helper to convert authtok type to string
    and makes use of this helper in `pam_print_data()`
    
    Reviewed-by: Sumit Bose <sbose@redhat.com>

--- src/util/sss_pam_data.c
+++ src/util/sss_pam_data.c
@@ -171,8 +171,12 @@ void pam_print_data(int l, struct pam_data *pd)
     DEBUG(l, "tty: %s\n", PAM_SAFE_ITEM(pd->tty));
     DEBUG(l, "ruser: %s\n", PAM_SAFE_ITEM(pd->ruser));
     DEBUG(l, "rhost: %s\n", PAM_SAFE_ITEM(pd->rhost));
-    DEBUG(l, "authtok type: %d\n", sss_authtok_get_type(pd->authtok));
-    DEBUG(l, "newauthtok type: %d\n", sss_authtok_get_type(pd->newauthtok));
+    DEBUG(l, "authtok type: %d (%s)\n",
+          sss_authtok_get_type(pd->authtok),
+          sss_authtok_type_to_str(sss_authtok_get_type(pd->authtok)));
+    DEBUG(l, "newauthtok type: %d (%s)\n",
+          sss_authtok_get_type(pd->newauthtok),
+          sss_authtok_type_to_str(sss_authtok_get_type(pd->newauthtok)));
     DEBUG(l, "priv: %d\n", pd->priv);
     DEBUG(l, "cli_pid: %d\n", pd->cli_pid);
     DEBUG(l, "logon name: %s\n", PAM_SAFE_ITEM(pd->logon_name));
