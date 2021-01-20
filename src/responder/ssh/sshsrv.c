5bb1b5d586558f5fe3b4d5679b81249c95bd9bee
Sumit Bose <sbose@redhat.com>
2021-01-20 17:48:44 +0100

    responders: add callback to schedule_get_domains_task()
    To allow responders to run dedicated code at the end of the initial
    getDomains request a callback is added.
    
    Resolves: https://github.com/SSSD/sssd/issues/5469

--- src/responder/ssh/sshsrv.c
+++ src/responder/ssh/sshsrv.c
@@ -126,7 +126,7 @@ int ssh_process_init(TALLOC_CTX *mem_ctx,
         goto fail;
     }
 
-    ret = schedule_get_domains_task(rctx, rctx->ev, rctx, NULL);
+    ret = schedule_get_domains_task(rctx, rctx->ev, rctx, NULL, NULL, NULL);
     if (ret != EOK) {
         DEBUG(SSSDBG_FATAL_FAILURE, "schedule_get_domains_tasks failed.\n");
         goto fail;
