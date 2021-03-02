08a1bb72f5ba09603f12468662c10fc0e0f793aa
Alexey Tikhonov <atikhono@redhat.com>
2021-03-02 21:23:24 +0100

    DEBUG: replace gettimeofday() with time() if usec isn't needed
    gettimeofday() is much slower than time() and accounts for ~2% of total
    time consumed by DEBUG.
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    Reviewed-by: Tomáš Halman <thalman@redhat.com>

--- src/util/debug.c
+++ src/util/debug.c
@@ -273,6 +273,7 @@ void sss_vdebug_fn(const char *file,
 {
     struct timeval tv;
     struct tm tm;
+    time_t t;
 
 #ifdef WITH_JOURNALD
     errno_t ret;
@@ -299,8 +300,13 @@ void sss_vdebug_fn(const char *file,
 #endif
 
     if (debug_timestamps) {
-        gettimeofday(&tv, NULL);
-        localtime_r(&tv.tv_sec, &tm);
+        if (debug_microseconds) {
+            gettimeofday(&tv, NULL);
+            t = tv.tv_sec;
+        } else {
+            t = time(NULL);
+        }
+        localtime_r(&t, &tm);
         debug_printf("(%d-%02d-%02d %2d:%02d:%02d",
                      tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
                      tm.tm_hour, tm.tm_min, tm.tm_sec);
