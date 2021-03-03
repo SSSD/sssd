c94ccdbef11508dff2a8b3c797b17cf7ebd78a58
Alexey Tikhonov <atikhono@redhat.com>
2021-03-03 21:29:07 +0100

    Moved ldb_debug_messages() out of UTILS to SYSDB

--- src/util/debug.c
+++ src/util/debug.c
@@ -345,32 +345,6 @@ void sss_debug_fn(const char *file,
     va_end(ap);
 }
 
-void ldb_debug_messages(void *context, enum ldb_debug_level level,
-                        const char *fmt, va_list ap)
-{
-    int loglevel = SSSDBG_UNRESOLVED;
-
-    switch(level) {
-    case LDB_DEBUG_FATAL:
-        loglevel = SSSDBG_FATAL_FAILURE;
-        break;
-    case LDB_DEBUG_ERROR:
-        loglevel = SSSDBG_CRIT_FAILURE;
-        break;
-    case LDB_DEBUG_WARNING:
-        loglevel = SSSDBG_TRACE_FUNC;
-        break;
-    case LDB_DEBUG_TRACE:
-        loglevel = SSSDBG_TRACE_LDB;
-        break;
-    }
-
-    if (DEBUG_IS_SET(loglevel)) {
-        sss_vdebug_fn(__FILE__, __LINE__, "ldb", loglevel, APPEND_LINE_FEED,
-                      fmt, ap);
-    }
-}
-
 /* In cases SSSD used to run as the root user, but runs as the SSSD user now,
  * we need to chown the log files
  */
