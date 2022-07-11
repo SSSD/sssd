e6a00c585313ff2b387a4f462e7d98072d075780
Paul Donohue <git@PaulSD.com>
2022-07-11 13:40:39 -0400

    Add ldap_connection_idle_timeout to subdomain_inherit
    Reviewed-by: Alexey Tikhonov <atikhono@redhat.com>
    Reviewed-by: Sumit Bose <sbose@redhat.com>

--- src/providers/ldap/sdap.c
+++ src/providers/ldap/sdap.c
@@ -270,6 +270,7 @@ static void sdap_inherit_basic_options(char **inherit_opt_list,
 {
     int inherit_options[] = {
         SDAP_PURGE_CACHE_TIMEOUT,
+        SDAP_IDLE_TIMEOUT,
         SDAP_AD_USE_TOKENGROUPS,
         SDAP_KRB5_KEYTAB,
         SDAP_OPTS_BASIC     /* sentinel */
