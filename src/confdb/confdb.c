cc87838cb5970a8dabf20fba2bbbfd9a7408fa6b
Pavel Březina <pbrezina@redhat.com>
2022-10-11 12:10:25 +0200

    confdb: avoid syslog message when no domains are enabled
    This syslog message would also appear when calling other tools like
    sss_cache which is confusing. We return specific error code instead
    and let the error be syslogged in the monitor in monitor.c:main (this
    is already implemented).
    
    Resolves: https://github.com/SSSD/sssd/issues/6387
    
    :fixes: A regression when running sss_cache when no SSSD domain is
      enabled would produce a syslog critical message was fixed.

--- src/confdb/confdb.c
+++ src/confdb/confdb.c
@@ -1800,10 +1800,10 @@ int confdb_get_domains(struct confdb_ctx *cdb,
     ret = confdb_get_enabled_domain_list(cdb, tmp_ctx, &domlist);
     if (ret == ENOENT) {
         DEBUG(SSSDBG_FATAL_FAILURE, "No domains configured, fatal error!\n");
-        sss_log(SSS_LOG_CRIT, "No domains configured, fatal error!\n");
+        ret = ERR_NO_DOMAIN_ENABLED;
         goto done;
     }
-    if (ret != EOK ) {
+    if (ret != EOK) {
         DEBUG(SSSDBG_FATAL_FAILURE, "Fatal error retrieving domains list!\n");
         goto done;
     }
@@ -2472,9 +2472,9 @@ int confdb_expand_app_domains(struct confdb_ctx *cdb)
     ret = confdb_get_enabled_domain_list(cdb, tmp_ctx, &domlist);
     if (ret == ENOENT) {
         DEBUG(SSSDBG_FATAL_FAILURE, "No domains configured, fatal error!\n");
-        sss_log(SSS_LOG_CRIT, "No domains configured, fatal error!\n");
+        ret = ERR_NO_DOMAIN_ENABLED;
         goto done;
-    } else if (ret != EOK ) {
+    } else if (ret != EOK) {
         DEBUG(SSSDBG_FATAL_FAILURE, "Fatal error retrieving domains list!\n");
         goto done;
     }
