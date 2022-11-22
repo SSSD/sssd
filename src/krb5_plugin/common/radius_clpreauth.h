5b4da3a00cc33c1df34fe8eccec0daba9d04f506
Pavel Březina <pbrezina@redhat.com>
2022-11-22 22:10:29 +0100

    passkey: fix copyrights and comments in krb plugin
    Reviewed-by: Iker Pedrosa <ipedrosa@redhat.com>
    Reviewed-by: Justin Stephenson <jstephen@redhat.com>

--- src/krb5_plugin/common/radius_clpreauth.h
+++ src/krb5_plugin/common/radius_clpreauth.h
@@ -2,7 +2,7 @@
     Authors:
         Pavel Březina <pbrezina@redhat.com>
 
-    Copyright (C) 2022 Red Hat
+    Copyright (C) 2023 Red Hat
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- src/tests/cmocka/test_krb5_passkey_plugin.c
+++ src/tests/cmocka/test_krb5_passkey_plugin.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2022 Red Hat
+    Copyright (C) 2023 Red Hat
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
