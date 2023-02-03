a4f600a8e831e459f6126d8cfad01164c8ca4124
Alexey Tikhonov <atikhono@redhat.com>
2023-02-03 11:35:42 +0100

    SSS_CLIENT: fix error codes returned by common read/write/check helpers.
    It's kind of expected that in case `(POLLERR | POLLHUP | POLLNVAL)`
    error condition is detected, regular `POLLIN/POLLOUT` won't be set.
    Error code set by error condition should have a priority. This enables
    users of this helper to retry attempt (as designed).
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    Reviewed-by: Sumit Bose <sbose@redhat.com>

--- src/sss_client/common.c
+++ src/sss_client/common.c
@@ -161,8 +161,7 @@ static enum sss_status sss_cli_send_req(enum sss_cli_command cmd,
         case 1:
             if (pfd.revents & (POLLERR | POLLHUP | POLLNVAL)) {
                 *errnop = EPIPE;
-            }
-            if (!(pfd.revents & POLLOUT)) {
+            } else if (!(pfd.revents & POLLOUT)) {
                 *errnop = EBUSY;
             }
             break;
@@ -273,8 +272,7 @@ static enum sss_status sss_cli_recv_rep(enum sss_cli_command cmd,
             }
             if (pfd.revents & (POLLERR | POLLNVAL)) {
                 *errnop = EPIPE;
-            }
-            if (!(pfd.revents & POLLIN)) {
+            } else if (!(pfd.revents & POLLIN)) {
                 *errnop = EBUSY;
             }
             break;
@@ -725,8 +723,7 @@ static enum sss_status sss_cli_check_socket(int *errnop,
         case 1:
             if (pfd.revents & (POLLERR | POLLHUP | POLLNVAL)) {
                 *errnop = EPIPE;
-            }
-            if (!(pfd.revents & (POLLIN | POLLOUT))) {
+            } else if (!(pfd.revents & (POLLIN | POLLOUT))) {
                 *errnop = EBUSY;
             }
             break;
