7c20552fa97cd3ac8df3cab5801f579719d4bc4d
Justin Stephenson <jstephen@redhat.com>
2023-02-27 13:28:14 -0500

    pam_sss: Add passkey kerberos preauth support
    Reviewed-by: Iker Pedrosa <ipedrosa@redhat.com>
    Reviewed-by: Sumit Bose <sbose@redhat.com>

--- src/sss_client/pam_message.h
+++ src/sss_client/pam_message.h
@@ -64,6 +64,7 @@ struct pam_items {
     char *oauth2_url_complete;
     char *oauth2_pin;
     char *first_factor;
+    char *passkey_key;
     char *passkey_prompt_pin;
     bool password_prompting;
 
