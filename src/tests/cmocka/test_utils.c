28ca00b8144b0d84785bdeeafad9929a573ab3d3
Sumit Bose <sbose@redhat.com>
2023-03-02 17:10:52 +0100

    krb5: add joined/parent domain to [domain_realm]
    So far only discovered sub-domains were adding to the [domain_realm]
    section of one of the krb5 config snippets SSSD is generating. To fix
    recent issues which were caused my missing entries of the joined domain
    this domain is now added as well.
    
    Resolves: https://github.com/SSSD/sssd/issues/6600
    
    Reviewed-by: Alejandro López <allopez@redhat.com>
    Reviewed-by: Justin Stephenson <jstephen@redhat.com>
    (cherry picked from commit ebc1e460efcf4101459248a22253f0c52b9c39ab)

--- src/tests/cmocka/test_utils.c
+++ src/tests/cmocka/test_utils.c
@@ -2033,6 +2033,8 @@ static void test_sss_get_domain_mappings_content(void **state)
     assert_int_equal(ret, EOK);
     assert_string_equal(content,
                         "[domain_realm]\n"
+                        ".configured.dom = CONFIGURED.DOM\n"
+                        "configured.dom = CONFIGURED.DOM\n"
                         ".subdom1.dom = SUBDOM1.DOM\n"
                         "subdom1.dom = SUBDOM1.DOM\n"
                         ".subdom2.dom = SUBDOM2.DOM\n"
@@ -2058,6 +2060,8 @@ static void test_sss_get_domain_mappings_content(void **state)
     assert_int_equal(ret, EOK);
     assert_string_equal(content,
                         "[domain_realm]\n"
+                        ".configured.dom = CONFIGURED.DOM\n"
+                        "configured.dom = CONFIGURED.DOM\n"
                         ".subdom1.dom = SUBDOM1.DOM\n"
                         "subdom1.dom = SUBDOM1.DOM\n"
                         ".subdom2.dom = SUBDOM2.DOM\n"
