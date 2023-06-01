cccee32d03ce3c0bbf72d4d5076b63327edd32f4
Pavel Březina <pbrezina@redhat.com>
2023-06-01 14:23:06 +0200

    tests: drop tier from system tests
    It is replaced by importance marker, which defaults to medium.
    
    Reviewed-by: Jakub Vávra <jvavra@redhat.com>
    Reviewed-by: Tomáš Halman <thalman@redhat.com>

--- src/tests/system/tests/test_shadow.py
+++ src/tests/system/tests/test_shadow.py
@@ -12,7 +12,6 @@ from sssd_test_framework.roles.ldap import LDAP
 from sssd_test_framework.topology import KnownTopology
 
 
-@pytest.mark.tier(0)
 @pytest.mark.ticket(bz=1507035)
 @pytest.mark.topology(KnownTopology.LDAP)
 @pytest.mark.parametrize("method", ["su", "ssh"])
