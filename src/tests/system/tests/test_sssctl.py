9e832d0d7c3372da1724b8a760708e59dce7e9cc
Patrik Rosecky <prosecky@redhat.com>
2023-09-05 11:08:32 +0200

    Tests: alltest/test_sssctl_local.py converted to system/tests/sssctl.py

--- src/tests/system/tests/test_sssctl.py
+++ src/tests/system/tests/test_sssctl.py
@@ -67,3 +67,72 @@ def test_sssctl__check_invalid_id_provider(client: Client):
         "[rule/sssd_checks]: Attribute 'id_provider' in section 'domain/test' has an invalid value: invalid"
         in output.stdout_lines[1]
     )
+
+
+@pytest.mark.ticket(bz=1640576)
+@pytest.mark.builtwith("files-provider")
+@pytest.mark.topology(KnownTopology.Client)
+def test_sssctl__user_show_cache_expiration_time(client: Client):
+    """
+    :title: sssctl user-show reports correct expiration time of local user
+    :setup:
+        1. Add local users
+        2. Configure local domain
+        3. Start SSSD
+    :steps:
+        1. Call sssctl user-show $user
+        2. Check correct output
+    :expectedresults:
+        1. Called successfully
+        2. Output is as expected
+    :customerscenario: True
+    """
+    client.local.user("local1").add()
+    client.local.user("local2").add()
+    client.local.user("local3").add()
+
+    client.sssd.common.local()
+    client.sssd.default_domain = "local"
+    client.sssd.domain["id_provider"] = "files"
+    client.sssd.domain["passwd_files"] = "/etc/passwd"
+
+    client.sssd.start()
+
+    for user in {"local1", "local2", "local3"}:
+        cmd = client.sssctl.user_show(user=user)
+        assert cmd.rc == 0, "Command call failed"
+        assert "Cache entry expiration time: Never" in cmd.stdout, "Wrong output"
+
+
+@pytest.mark.ticket(bz=1599207)
+@pytest.mark.builtwith("files-provider")
+@pytest.mark.topology(KnownTopology.Client)
+def test_sssctl__handle_implicit_domain(client: Client):
+    """
+    :title: sssctl handle implicit domain
+    :setup:
+        1. Add local users
+        2. Set sssd "enable_files_domain" to "true"
+        3. Start SSSD
+    :steps:
+        1. Call getent passwd user -s sss
+        2. Call sssctl user-show --user=$user
+        3. Check correct output
+    :expectedresults:
+        1. Called successfully
+        2. Called successfully
+        3. Output is correct
+    :customerscenario: True
+    """
+    client.local.user("local1").add()
+    client.local.user("local2").add()
+    client.local.user("local3").add()
+
+    client.sssd.sssd["enable_files_domain"] = "true"
+    client.sssd.start()
+
+    for user in {"local1", "local2", "local3"}:
+        assert client.tools.getent.passwd(user, service="sss") is not None
+        cmd = client.sssctl.user_show(user=user)
+        assert cmd.rc == 0
+        assert "Cache entry creation date" in cmd.stdout
