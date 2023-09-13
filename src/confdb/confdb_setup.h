8e052162164d5e4ad62061c27eba355e4ca77483
Alexey Tikhonov <atikhono@redhat.com>
2023-09-13 18:59:46 +0200

    Get rid of `--genconf` and `--genconf-section` monitor options.
    The only usage was 'sssd-kcm.service', but it was wrong since 'sssd_kcm'
    should be usable without other SSSD packages being installed (see #6926)

--- src/confdb/confdb_setup.h
+++ src/confdb/confdb_setup.h
@@ -27,21 +27,6 @@
 
 #include "util/util_errors.h"
 
-#define CONFDB_BASE_LDIF \
-     "dn: @ATTRIBUTES\n" \
-     "cn: CASE_INSENSITIVE\n" \
-     "dc: CASE_INSENSITIVE\n" \
-     "dn: CASE_INSENSITIVE\n" \
-     "name: CASE_INSENSITIVE\n" \
-     "objectclass: CASE_INSENSITIVE\n" \
-     "\n" \
-     "dn: @INDEXLIST\n" \
-     "@IDXATTR: cn\n" \
-     "\n" \
-     "dn: @MODULES\n" \
-     "@LIST: server_sort\n" \
-     "\n"
-
 struct confdb_ctx;
 
 errno_t confdb_setup(TALLOC_CTX *mem_ctx,
