SSSD/sssd

commit cca497b4cbbbf05c4f9181b7d8113cde81754831

confdb: Make it possible to use config snippets
Resolves:
https://fedorahosted.org/sssd/ticket/2247

Signed-off-by: Lukas Slebodnik <lslebodn@redhat.com>

Reviewed-by: Lukáš Slebodník <lslebodn@redhat.com>
Reviewed-by: Jakub Hrozek <jhrozek@redhat.com>

src/confdb/confdb_setup.c
@@ -127,14 +127,14 @@ static int confdb_create_base(struct confdb_ctx *cdb)
     return EOK;
 }
 
-static int confdb_init_db(const char *config_file, struct confdb_ctx *cdb)
+static int confdb_init_db(const char *config_file, const char *config_dir,
+                          struct confdb_ctx *cdb)
 {
     TALLOC_CTX *tmp_ctx;
     int ret;
     int sret = EOK;
     int version;
     char timestr[21];
-    char *lasttimestr;
     bool in_transaction = false;
     const char *config_ldif;
     const char *vals[2] = { timestr, NULL };
@@ -206,9 +206,6 @@ static int confdb_init_db(const char *config_file, struct confdb_ctx *cdb)
         goto done;
     }
 
-    /* Determine if the conf file has changed since we last updated
-     * the confdb
-     */
     ret = sss_ini_get_stat(init_data);
     if (ret != EOK) {
         DEBUG(SSSDBG_FATAL_FAILURE,
@@ -225,22 +222,13 @@ static int confdb_init_db(const char *config_file, struct confdb_ctx *cdb)
               "Failed to convert time_t to string ??\n");
         ret = errno ? errno : EFAULT;
     }
-    ret = confdb_get_string(cdb, tmp_ctx, "config", "lastUpdate",
-                            NULL, &lasttimestr);
-    if (ret == EOK) {
 
-        /* check if we lastUpdate and last file modification change differ*/
-        if ((lasttimestr != NULL) && (strcmp(lasttimestr, timestr) == 0)) {
-            /* not changed, get out, nothing more to do */
-            ret = EOK;
-            goto done;
-        }
-    } else {
-        DEBUG(SSSDBG_FATAL_FAILURE, "Failed to get lastUpdate attribute.\n");
-        goto done;
-    }
+    /* FIXME: Determine if the conf file or any snippet has changed
+     * since we last updated the confdb or if some snippet was
+     * added or removed.
+     */
 
-    ret = sss_ini_get_config(init_data, config_file);
+    ret = sss_ini_get_config(init_data, config_file, config_dir);
     if (ret != EOK) {
         DEBUG(SSSDBG_FATAL_FAILURE, "Failed to load configuration\n");
         goto done;
@@ -358,6 +346,7 @@ done:
 errno_t confdb_setup(TALLOC_CTX *mem_ctx,
                      const char *cdb_file,
                      const char *config_file,
+                     const char *config_dir,
                      struct confdb_ctx **_cdb)
 {
     TALLOC_CTX *tmp_ctx;
@@ -413,7 +402,7 @@ errno_t confdb_setup(TALLOC_CTX *mem_ctx,
         goto done;
     }
 
-    ret = confdb_init_db(config_file, cdb);
+    ret = confdb_init_db(config_file, config_dir, cdb);
     if (ret != EOK) {
         DEBUG(SSSDBG_FATAL_FAILURE, "ConfDB initialization has failed "
               "[%d]: %s\n", ret, sss_strerror(ret));

Makefile.am
@@ -3849,6 +3849,7 @@ SSSD_USER_DIRS = \
     $(DESTDIR)$(pubconfpath)/krb5.include.d \
     $(DESTDIR)$(gpocachepath) \
     $(DESTDIR)$(sssdconfdir) \
+    $(DESTDIR)$(sssdconfdir)/conf.d \
     $(DESTDIR)$(sssddefaultconfdir) \
     $(DESTDIR)$(logpath) \
     $(NULL)
@@ -3883,7 +3884,8 @@ endif
 	$(INSTALL) -d -m 0755 $(DESTDIR)$(mcpath) $(DESTDIR)$(pipepath) \
             $(DESTDIR)$(pubconfpath) \
             $(DESTDIR)$(pubconfpath)/krb5.include.d $(DESTDIR)$(gpocachepath)
-	$(INSTALL) -d -m 0711 $(DESTDIR)$(sssdconfdir)
+	$(INSTALL) -d -m 0711 $(DESTDIR)$(sssdconfdir) \
+                          $(DESTDIR)$(sssdconfdir)/conf.d
 
 if HAVE_DOXYGEN
 docs:

contrib/sssd.spec.in
@@ -782,6 +782,7 @@ done
 %attr(755,sssd,sssd) %dir %{gpocachepath}
 %attr(750,sssd,sssd) %dir %{_var}/log/%{name}
 %attr(711,sssd,sssd) %dir %{_sysconfdir}/sssd
+%attr(711,sssd,sssd) %dir %{_sysconfdir}/sssd/conf.d
 %ghost %attr(0600,sssd,sssd) %config(noreplace) %{_sysconfdir}/sssd/sssd.conf
 %if (0%{?use_systemd} == 1)
 %attr(755,root,root) %dir %{_sysconfdir}/systemd/system/sssd.service.d

src/confdb/confdb.h
@@ -42,6 +42,7 @@
 #define CONFDB_FILE "config.ldb"
 #define SSSD_DEFAULT_CONFIG_FILE SSSD_DEFAULT_CONF_DIR"/sssd.conf"
 #define SSSD_CONFIG_FILE SSSD_CONF_DIR"/sssd.conf"
+#define CONFDB_DEFAULT_CONFIG_DIR SSSD_CONF_DIR"/conf.d"
 #define SSSD_MIN_ID 1
 #define SSSD_LOCAL_MINID 1000
 #define CONFDB_DEFAULT_SHELL_FALLBACK "/bin/sh"

src/confdb/confdb_setup.h
@@ -48,6 +48,7 @@
 errno_t confdb_setup(TALLOC_CTX *mem_ctx,
                      const char *cdb_file,
                      const char *config_file,
+                     const char *config_dir,
                      struct confdb_ctx **_cdb);
 
 #endif /* CONFDB_SETUP_H_ */

src/external/libini_config.m4
@@ -19,6 +19,18 @@ PKG_CHECK_MODULES(INI_CONFIG_V0, [
                         INI_CONFIG_LIBS="$INI_CONFIG_V1_1_LIBS"
                         HAVE_LIBINI_CONFIG_V1_1=1
                         AC_DEFINE_UNQUOTED(HAVE_LIBINI_CONFIG_V1_1, 1, [libini_config version 1.1.0 or greater])
+                        PKG_CHECK_MODULES(INI_CONFIG_V1_3, [
+                            ini_config >= 1.3.0], [
+
+                                INI_CONFIG_CFLAGS="$INI_CONFIG_V1_3_CFLAGS"
+                                INI_CONFIG_LIBS="$INI_CONFIG_V1_3_LIBS"
+                                HAVE_LIBINI_CONFIG_V1_3=1
+                                AC_DEFINE_UNQUOTED(HAVE_LIBINI_CONFIG_V1_3, 1,
+                                                   [libini_config version 1.3.0 or greater])
+                            ], [
+                                AC_MSG_WARN([libini_config-devel >= 1.3.0 not available, using older version])
+                            ]
+                        )
                     ], [
                         AC_MSG_WARN([libini_config-devel >= 1.1.0 not available, using older version])
                     ]

src/monitor/monitor.c
@@ -1872,6 +1872,7 @@ static int monitor_ctx_destructor(void *mem)
  */
 errno_t load_configuration(TALLOC_CTX *mem_ctx,
                            const char *config_file,
+                           const char *config_dir,
                            struct mt_ctx **monitor)
 {
     errno_t ret;
@@ -1892,7 +1893,7 @@ errno_t load_configuration(TALLOC_CTX *mem_ctx,
         goto done;
     }
 
-    ret = confdb_setup(ctx, cdb_file, config_file, &ctx->cdb);
+    ret = confdb_setup(ctx, cdb_file, config_file, config_dir, &ctx->cdb);
     if (ret != EOK) {
         DEBUG(SSSDBG_FATAL_FAILURE, "Unable to setup ConfDB [%d]: %s\n",
              ret, sss_strerror(ret));
@@ -3147,7 +3148,8 @@ int main(int argc, const char *argv[])
     }
 
     /* Parse config file, fail if cannot be done */
-    ret = load_configuration(tmp_ctx, config_file, &monitor);
+    ret = load_configuration(tmp_ctx, config_file, CONFDB_DEFAULT_CONFIG_DIR,
+                             &monitor);
     if (ret != EOK) {
         switch (ret) {
         case ERR_MISSING_CONF:

src/tools/common/sss_tools.c
@@ -103,7 +103,9 @@ static errno_t sss_tool_confdb_init(TALLOC_CTX *mem_ctx,
         return ENOMEM;
     }
 
-    ret = confdb_setup(mem_ctx, path, SSSD_CONFIG_FILE, &confdb);
+    ret = confdb_setup(mem_ctx, path,
+                       SSSD_CONFIG_FILE, CONFDB_DEFAULT_CONFIG_DIR,
+                       &confdb);
     talloc_zfree(path);
     if (ret != EOK) {
         DEBUG(SSSDBG_FATAL_FAILURE, "Unable to setup ConfDB [%d]: %s\n",

commit aa7d5cc2a414da954cab7e992788bba5d35e55e6

CONFDB: get rid of "lastUpdate"
Don't bother, just always re-create ldb. Service restart doesn't
happen often.

It's broken since cca497b4cbbbf05c4f9181b7d8113cde81754831 anyway.

src/confdb/confdb_setup.c
@@ -130,11 +130,9 @@ static int confdb_ldif_from_ini_file(TALLOC_CTX *mem_ctx,
                                      const char *config_dir,
                                      const char *only_section,
                                      struct sss_ini *init_data,
-                                     const char **_timestr,
                                      const char **_ldif)
 {
     errno_t ret;
-    char timestr[21] = "1";
     int version;
 
     ret = sss_ini_read_sssd_conf(init_data,
@@ -143,30 +141,6 @@ static int confdb_ldif_from_ini_file(TALLOC_CTX *mem_ctx,
     if (ret != EOK) {
         return ret;
     }
-
-    if (sss_ini_exists(init_data)) {
-        ret = sss_ini_get_stat(init_data);
-        if (ret != EOK) {
-            ret = errno;
-            DEBUG(SSSDBG_FATAL_FAILURE,
-              "Status check on config file failed.\n");
-            return ret;
-        }
-
-        errno = 0;
-        ret = sss_ini_get_mtime(init_data, sizeof(timestr), timestr);
-        if (ret <= 0 || ret >= (int)sizeof(timestr)) {
-            ret = errno ? errno : EFAULT;
-            DEBUG(SSSDBG_FATAL_FAILURE,
-                  "Failed to convert time_t to string??\n");
-            return ret;
-        }
-    }
-
-    /* FIXME: Determine if the conf file or any snippet has changed
-     * since we last updated the confdb or if some snippet was
-     * added or removed.
-     */
 
     ret = sss_ini_call_validators(init_data,
                                   SSSDDATADIR"/cfg_rules.ini");
@@ -213,11 +187,6 @@ static int confdb_ldif_from_ini_file(TALLOC_CTX *mem_ctx,
     if (ret != EOK) {
         DEBUG(SSSDBG_FATAL_FAILURE, "Could not create LDIF for confdb\n");
         return ret;
-    }
-
-    *_timestr = talloc_strdup(mem_ctx, timestr);
-    if (*_timestr == NULL) {
-        return ENOMEM;
     }
 
     return EOK;
@@ -273,9 +242,7 @@ static int confdb_init_db(const char *config_file,
     int ret;
     int sret = EOK;
     bool in_transaction = false;
-    const char *timestr = NULL;
     const char *config_ldif;
-    const char *vals[2] = { NULL, NULL };
     struct sss_ini *init_data;
 
     tmp_ctx = talloc_new(cdb);
@@ -296,7 +263,6 @@ static int confdb_init_db(const char *config_file,
                                     config_dir,
                                     only_section,
                                     init_data,
-                                    &timestr,
                                     &config_ldif);
     if (ret != EOK) {
         DEBUG(SSSDBG_CRIT_FAILURE,
@@ -332,17 +298,6 @@ static int confdb_init_db(const char *config_file,
                             config_ldif,
                             only_section == NULL ? true : false);
     if (ret != EOK) {
-        goto done;
-    }
-
-    /* now store the lastUpdate time so that we do not re-init if nothing
-     * changed on restart */
-
-    vals[0] = timestr;
-    ret = confdb_add_param(cdb, true, "config", "lastUpdate", vals);
-    if (ret != EOK) {
-        DEBUG(SSSDBG_FATAL_FAILURE,
-              "Failed to set last update time on db!\n");
         goto done;
     }
 