24507c04eaab893cbe79a4d1c5f3333061daa4f4
Patrik Rosecky <prosecky@redhat.com>
2023-09-26 07:30:11 +0200

    Tests: converted alltests/test_pasword_policy.py to tests/test_ldap.py

--- src/tests/system/tests/test_ldap.py
+++ src/tests/system/tests/test_ldap.py
@@ -12,6 +12,7 @@ from sssd_test_framework.roles.ldap import LDAP
 from sssd_test_framework.topology import KnownTopology
 
 
+@pytest.mark.ticket(bz=[795044, 1695574])
 @pytest.mark.importance("critical")
 @pytest.mark.authentication
 @pytest.mark.parametrize("modify_mode", ["exop", "ldap_modify"])
@@ -20,7 +21,7 @@ def test_ldap__change_password(client: Client, ldap: LDAP, modify_mode: str):
     """
     :title: Change password with "ldap_pwmodify_mode" set to @modify_mode
     :setup:
-        1. Add user to SSSD, set his password
+        1. Add user to LDAP, set his password
         2. Allow user to change his password
         3. Set "ldap_pwmodify_mode"
         4. Start SSSD
@@ -34,7 +35,7 @@ def test_ldap__change_password(client: Client, ldap: LDAP, modify_mode: str):
         2. Password is changed successfully
         3. User is authenticated
         4. User is not authenticated
-    :customerscenario: False
+    :customerscenario: True
     """
     user = "user1"
     old_pass = "Secret123"
@@ -52,3 +53,94 @@ def test_ldap__change_password(client: Client, ldap: LDAP, modify_mode: str):
 
     assert client.auth.ssh.password(user, new_pass), "Authentication with new correct password failed"
     assert not client.auth.ssh.password(user, old_pass), "Authentication with old incorrect password did not fail"
+
+
+@pytest.mark.ticket(bz=[795044, 1695574])
+@pytest.mark.parametrize("modify_mode", ["exop", "ldap_modify"])
+@pytest.mark.topology(KnownTopology.LDAP)
+def test_ldap__change_password_new_pass_not_match(client: Client, ldap: LDAP, modify_mode: str):
+    """
+    :title: Change password with "ldap_pwmodify_mode" set to @modify_mode, but retyped password do not match
+    :setup:
+        1. Add user to LDAP, set his password
+        2. Allow user to change his password
+        3. Set "ldap_pwmodify_mode"
+        4. Start SSSD
+    :steps:
+        1. Change password to new password, but retyped password is different
+    :expectedresults:
+        1. Password change is not successful
+    :customerscenario: True
+    """
+    ldap.user("user1").add(password="Secret123")
+    ldap.aci.add('(targetattr="userpassword")(version 3.0; acl "pwp test"; allow (all) userdn="ldap:///self";)')
+
+    client.sssd.domain["ldap_pwmodify_mode"] = modify_mode
+    client.sssd.start()
+
+    assert not client.auth.passwd.password(
+        "user1", "Secret123", "Red123", "Hat000"
+    ), "Password changed successfully, which is not expected"
+
+
+@pytest.mark.ticket(bz=[795044, 1695574, 1795220])
+@pytest.mark.parametrize("modify_mode", ["exop", "ldap_modify"])
+@pytest.mark.topology(KnownTopology.LDAP)
+def test_ldap__change_password_lowercase(client: Client, ldap: LDAP, modify_mode: str):
+    """
+    :title: Change password to lower-case letters, password check fail
+    :setup:
+        1. Add user to LDAP, set his password
+        2. Allow user to change his password
+        3. Set "passwordCheckSyntax" to "on"
+        4. Set "ldap_pwmodify_mode"
+        5. Start SSSD
+    :steps:
+        1. Change password to new password, but all letters are lower-case
+        2. Check logs
+    :expectedresults:
+        1. Password change failed
+        2. Password change failure is logged
+    :customerscenario: True
+    """
+    ldap.user("user1").add(password="Secret123")
+    ldap.aci.add('(targetattr="userpassword")(version 3.0; acl "pwp test"; allow (all) userdn="ldap:///self";)')
+    ldap.ldap.modify("cn=config", replace={"passwordCheckSyntax": "on"})
+
+    client.sssd.domain["ldap_pwmodify_mode"] = modify_mode
+    client.sssd.start()
+
+    assert not client.auth.passwd.password(
+        "user1", "Secret123", "red_32"
+    ), "Password changed successfully, which is not expected"
+
+    assert (
+        "pam_sss(passwd:chauthtok): User info message: Password change failed."
+        in client.host.ssh.run("journalctl").stdout
+    )
+
+
+@pytest.mark.ticket(bz=[1695574, 1795220])
+@pytest.mark.parametrize("modify_mode", ["exop", "ldap_modify"])
+@pytest.mark.topology(KnownTopology.LDAP)
+def test_ldap__change_password_wrong_current(client: Client, ldap: LDAP, modify_mode: str):
+    """
+    :title: Password change failed because an incorrect password was used
+    :setup:
+        1. Add user to LDAP, set his password
+        2. Allow user to change his password
+        3. Set "ldap_pwmodify_mode"
+        4. Start SSSD
+    :steps:
+        1. Change password to new password, but enter wrong password
+    :expectedresults:
+        1. Password change is not successful
+    :customerscenario: True
+    """
+    ldap.user("user1").add(password="Secret123")
+    ldap.aci.add('(targetattr="userpassword")(version 3.0; acl "pwp test"; allow (all) userdn="ldap:///self";)')
+
+    client.sssd.domain["ldap_pwmodify_mode"] = modify_mode
+    client.sssd.start()
+
+    assert not client.auth.passwd.password("user1", "wrong123", "Newpass123"), "Password change did not fail"
