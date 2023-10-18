dd7a2bf61031e461878e75463eee70e094cb2f49
Alexey Tikhonov <atikhono@redhat.com>
2023-10-18 20:50:47 +0200

    SSS_INI: remove 'const' specifier from getter
    `sss_ini_get_string_config_value()` is a wrapper around
    `ini_get_string_config_value()`, whose docs says
    ```
    Returned value needs to be freed after use.
    ```
    But an attempt to free 'const char *' results in discarded-qualifiers
    warning.

--- src/util/sss_ini.h
+++ src/util/sss_ini.h
@@ -136,7 +136,7 @@ int sss_ini_get_int_config_value(struct sss_ini *self,
 /**
  * @brief Get string value
  */
-const char *sss_ini_get_string_config_value(struct sss_ini *self,
+char *sss_ini_get_string_config_value(struct sss_ini *self,
                                             int *error);
 
 /**
