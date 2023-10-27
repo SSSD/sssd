39a0de22daa8b95feb280427876732bcbbb22583
Iker Pedrosa <ipedrosa@redhat.com>
2023-10-27 12:34:55 +0200

    CI: clean distro.sh
    Support for Fedora 36- in master branch ended, so let's remove them.
    
    Signed-off-by: Iker Pedrosa <ipedrosa@redhat.com>
    
    Reviewed-by: Alexey Tikhonov <atikhono@redhat.com>
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>

--- contrib/ci/distro.sh
+++ contrib/ci/distro.sh
@@ -51,17 +51,7 @@ function distro_pkg_install()
 {
     declare prompt=$'Need root permissions to install packages.\n'
     prompt+="Enter sudo password for $USER: "
-    if [[ "$DISTRO_BRANCH" == -redhat-fedora-2[2-5]* ]]; then
-        # TODO switch fedora to DNF once
-        # https://bugzilla.redhat.com/show_bug.cgi?id=1215208 is fixed
-        [ $# != 0 ] && sudo -p "$prompt" \
-                            yum-deprecated --assumeyes install -- "$@" |&
-            # Pass input to output, fail if a missing package is reported
-            awk 'BEGIN {s=0}
-                 /^No package .* available.$/ {s=1}
-                 {print}
-                 END {exit s}'
-    elif [[ "$DISTRO_BRANCH" == -redhat-fedora-* ]]; then
+    if [[ "$DISTRO_BRANCH" == -redhat-fedora-* ]]; then
         [ $# != 0 ] && sudo -p "$prompt" \
                             /usr/bin/dnf --assumeyes --best \
                                          --setopt=install_weak_deps=False \
