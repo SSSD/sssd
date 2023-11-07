b6f44f103608b6c91372cc4ab502bbbef612c164
Alexey Tikhonov <atikhono@redhat.com>
2023-11-07 20:02:06 +0100

    INTG-TESTS: fake SO_PEERCRED on responder side as well
    Reviewed-by: Justin Stephenson <jstephen@redhat.com>
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    Reviewed-by: Sumit Bose <sbose@redhat.com>

--- src/tests/intg/getsockopt_wrapper.c
+++ src/tests/intg/getsockopt_wrapper.c
@@ -4,12 +4,15 @@
 #define _GNU_SOURCE 1
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <dlfcn.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 static bool is_dbus_socket(int fd)
 {
@@ -43,6 +46,38 @@ static bool peer_is_pam(int fd)
     unix_socket = (struct sockaddr_un *)&addr;
 
     return NULL != strstr(unix_socket->sun_path, "pipes/pam");
+}
+
+static bool peer_is_sssctl(const struct ucred *cr)
+{
+    char proc_path[32];
+    char cmd_line[255] = { 0 };
+    int proc_fd;
+    int ret;
+
+    if (cr->pid < 0) {
+        return false;
+    }
+
+    ret = snprintf(proc_path, sizeof(proc_path), "/proc/%d/cmdline",
+                   (int)cr->pid);
+    if ((ret < 0) || (ret >= sizeof(proc_path))) {
+        return false;
+    }
+
+    proc_fd = open(proc_path, O_RDONLY);
+    if (proc_fd != -1) {
+        ret = read(proc_fd, cmd_line, sizeof(cmd_line)-1);
+        close(proc_fd);
+        if (ret > 0) {
+            cmd_line[ret] = 0;
+            if (strncmp(cmd_line, "sssctl", 6) == 0) {
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
 
 static void fake_peer_uid_gid(uid_t *uid, gid_t *gid)
@@ -85,7 +120,7 @@ int getsockopt(int sockfd, int level, int optname,
         cr = optval;
         if (cr->uid != 0 && is_dbus_socket(sockfd)) {
             cr->uid = 0;
-        } else if (peer_is_pam(sockfd)) {
+        } else if (peer_is_pam(sockfd) || peer_is_sssctl(cr)) {
             fake_peer_uid_gid(&cr->uid, &cr->gid);
         }
     }
