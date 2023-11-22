be5399c15bd96998f20573c36bd7603b6a5694f5
Sumit Bose <sbose@redhat.com>
2023-11-22 16:10:54 +0100

    sssctl: do not require root for user-checks
    There is no requirement for root to run the test and if the user does
    not has the needed privileges to access the related services this is
    good as a test result as well. Additionally at least pam_chauthtok()
    behaves differently when being called as root compared to an ordinary
    user.
    
    Reviewed-by: Alejandro López <allopez@redhat.com>
    Reviewed-by: Alexey Tikhonov <atikhono@redhat.com>
    (cherry picked from commit 8ff7fdc127dafb8d4d98231e0f7d43af89f8595b)

--- src/tools/sssctl/sssctl.c
+++ src/tools/sssctl/sssctl.c
@@ -317,7 +317,7 @@ int main(int argc, const char **argv)
         SSS_TOOL_DELIMITER("SSSD Status:"),
         SSS_TOOL_COMMAND("domain-list", "List available domains", 0, sssctl_domain_list),
         SSS_TOOL_COMMAND("domain-status", "Print information about domain", 0, sssctl_domain_status),
-        SSS_TOOL_COMMAND("user-checks", "Print information about a user and check authentication", 0, sssctl_user_checks),
+        SSS_TOOL_COMMAND_FLAGS("user-checks", "Print information about a user and check authentication", 0, sssctl_user_checks, SSS_TOOL_FLAG_SKIP_CMD_INIT|SSS_TOOL_FLAG_SKIP_ROOT_CHECK),
         SSS_TOOL_COMMAND("access-report", "Generate access report for a domain", 0, sssctl_access_report),
         SSS_TOOL_DELIMITER("Information about cached content:"),
         SSS_TOOL_COMMAND("user-show", "Information about cached user", 0, sssctl_user_show),
