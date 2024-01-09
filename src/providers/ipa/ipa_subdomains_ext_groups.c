e862dc6eb60239d3ac1bb4ad0040434f354fe04c
Sumit Bose <sbose@redhat.com>
2024-01-09 11:14:42 +0100

    sdap: add set_non_posix parameter
    This patch adds a new parameter set_non_posix to the user and group
    lookup calls. Currently the domain type is used to determine if the
    search should be restricted to POSIX objects or not. The new option
    allows to drop this restriction explicitly to look up non-POSIX objects.
    
    Resolves: https://github.com/SSSD/sssd/issues/5708

--- src/providers/ipa/ipa_subdomains_ext_groups.c
+++ src/providers/ipa/ipa_subdomains_ext_groups.c
@@ -883,7 +883,7 @@ static void ipa_add_ad_memberships_get_next(struct tevent_req *req)
                                  state->sdap_id_ctx->conn,
                                  fq_name,
                                  BE_FILTER_NAME,
-                                 false, false);
+                                 false, false, false);
     if (subreq == NULL) {
         DEBUG(SSSDBG_OP_FAILURE, "groups_get_send failed.\n");
         ret = ENOMEM;
