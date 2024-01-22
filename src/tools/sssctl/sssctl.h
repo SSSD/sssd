e9fd9ad5f5ea81ae10069c1f95249f776864c127
Samuel Cabrero <scabrero@suse.de>
2024-01-22 14:50:23 +0100

    SSSCTL: Add sssctl gpo-list command
    Resolves: https://github.com/SSSD/sssd/issues/4523
    
    Signed-off-by: Samuel Cabrero <scabrero@suse.de>

--- src/tools/sssctl/sssctl.h
+++ src/tools/sssctl/sssctl.h
@@ -150,4 +150,8 @@ errno_t sssctl_gpo_show(struct sss_cmdline *cmdline,
                         struct sss_tool_ctx *tool_ctx,
                         void *pvt);
 
+errno_t sssctl_gpo_list(struct sss_cmdline *cmdline,
+                        struct sss_tool_ctx *tool_ctx,
+                        void *pvt);
+
 #endif /* _SSSCTL_H_ */
