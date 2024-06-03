a94ad26f59e14e378a2ba7d53d5d5312ec1140fc
Ondrej Valousek <ondrej.valousek.xm@renesas.com>
2024-06-03 12:42:13 +0200

    Revert "remove ldap_user_samaccountname option where not needed"
    This reverts commit 20493c57e516c98fb041029c411895b78d5d976f.

--- src/providers/ldap/ldap_opts.c
+++ src/providers/ldap/ldap_opts.c
@@ -196,6 +196,7 @@ struct sdap_attr_map rfc2307_user_map[] = {
     { "ldap_user_auth_type", NULL, SYSDB_AUTH_TYPE, NULL },
     { "ldap_user_certificate", "userCertificate;binary", SYSDB_USER_CERT, NULL },
     { "ldap_user_email", "mail", SYSDB_USER_EMAIL, NULL },
+    { "ldap_user_samaccountname", NULL, SYSDB_SAMACCOUNTNAME, NULL },
     { "ldap_user_passkey", "passkey", SYSDB_USER_PASSKEY, NULL },
     SDAP_ATTR_MAP_TERMINATOR
 };
@@ -256,6 +257,7 @@ struct sdap_attr_map rfc2307bis_user_map[] = {
     { "ldap_user_auth_type", NULL, SYSDB_AUTH_TYPE, NULL },
     { "ldap_user_certificate", "userCertificate;binary", SYSDB_USER_CERT, NULL },
     { "ldap_user_email", "mail", SYSDB_USER_EMAIL, NULL },
+    { "ldap_user_samaccountname", NULL, SYSDB_SAMACCOUNTNAME, NULL },
     { "ldap_user_passkey", "passkey", SYSDB_USER_PASSKEY, NULL },
     SDAP_ATTR_MAP_TERMINATOR
 };
