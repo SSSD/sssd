dcbf12b486ee51ba05f6e7716d38900ae766825b
Alexey Tikhonov <atikhono@redhat.com>
2024-06-21 19:09:29 +0200

    SYSDB: remove index on `dataExpireTimestamp`
    This index was only used in cleanup tasks that don't run often.
    On the other hand, this index is huge and degrades performance of libldb
    in general.

--- src/db/sysdb_init.c
+++ src/db/sysdb_init.c
@@ -582,6 +582,13 @@ static errno_t sysdb_domain_cache_upgrade(TALLOC_CTX *mem_ctx,
         }
     }
 
+    if (strcmp(version, SYSDB_VERSION_0_24) == 0) {
+        ret = sysdb_upgrade_24(sysdb, &version);
+        if (ret != EOK) {
+            goto done;
+        }
+    }
+
     ret = EOK;
 done:
     sysdb->ldb = save_ldb;
