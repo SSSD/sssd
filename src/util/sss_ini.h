1e68324c0687b4d9eb42210db741955999378cb5
Alexey Tikhonov <atikhono@redhat.com>
2024-10-23 19:21:32 +0200

    INI: remove unused helpers
    Btw, `sss_ini_get_mtime()` could access uninitialized 'self->cstat'

--- src/util/sss_ini.h
+++ src/util/sss_ini.h
@@ -93,18 +93,6 @@ int sss_ini_open(struct sss_ini *self,
 bool sss_ini_exists(struct sss_ini *self);
 
 /**
- * @brief get Cstat structure of the ini file
- */
-int sss_ini_get_stat(struct sss_ini *self);
-
-/**
- * @brief Get mtime of the ini file
- */
-int sss_ini_get_mtime(struct sss_ini *self,
-                      size_t timestr_len,
-                      char *timestr);
-
-/**
  * @brief Get pointer to list of snippet parsing errors
  */
 struct ref_array *
