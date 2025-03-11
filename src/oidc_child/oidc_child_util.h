133a13b76ef8302d0409ffaf56f0d2cc1e4c0511
Sumit Bose <sbose@redhat.com>
2025-03-11 19:34:56 +0100

    oidc_child: add user and group lookup
    oidc_child can now lookup user and group attributes from Keycloak and
    Entra ID.
    
    Resolves: https://github.com/SSSD/sssd/issues/7229
    
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    Reviewed-by: Scott Poore <spoore@redhat.com>

--- src/oidc_child/oidc_child_util.h
+++ src/oidc_child/oidc_child_util.h
@@ -28,6 +28,24 @@
 #include <jansson.h>
 #include "util/util.h"
 
+enum oidc_cmd {
+    NO_CMD = 0,
+    GET_DEVICE_CODE,
+    GET_ACCESS_TOKEN,
+    GET_USER,
+    GET_USER_GROUPS,
+    GET_GROUP,
+    GET_GROUP_MEMBERS,
+    CMD_SENTINEL
+};
+
+enum search_str_type {
+    TYPE_NAME = 1,
+    TYPE_OBJECT_ID = 2
+};
+
+struct rest_ctx;
+
 struct token_data {
     json_t *result;
     json_t *access_token;
@@ -40,15 +58,13 @@ struct token_data {
 };
 
 struct devicecode_ctx {
-    bool libcurl_debug;
-    const char *ca_db;
+    struct rest_ctx *rest_ctx;
     const char *device_authorization_endpoint;
     const char *token_endpoint;
     const char *userinfo_endpoint;
     const char *jwks_uri;
     const char *scope;
 
-    char *http_data;
     char *user_code;
     char *device_code;
     char *verification_uri;
@@ -60,12 +76,26 @@ struct devicecode_ctx {
     struct token_data *td;
 };
 
+struct name_and_type_identifier {
+    const char *user_identifier_attr;
+    const char *group_identifier_attr;
+    const char *user_name_attr;
+    const char *group_name_attr;
+};
+
 /* oidc_child_curl.c */
+struct rest_ctx *get_rest_ctx(TALLOC_CTX *mem_ctx, bool libcurl_debug,
+                              const char *ca_db);
+
+const char *get_http_data(struct rest_ctx *rest_ctx);
+
+errno_t set_http_data(struct rest_ctx *rest_ctx, const char *str);
+
 char *url_encode_string(TALLOC_CTX *mem_ctx, const char *inp);
 
 errno_t init_curl(void *p);
 
-void clean_http_data(struct devicecode_ctx *dc_ctx);
+void clean_http_data(struct rest_ctx *rest_ctx);
 
 errno_t get_openid_configuration(struct devicecode_ctx *dc_ctx,
                                         const char *issuer_url);
@@ -82,6 +112,18 @@ errno_t get_token(TALLOC_CTX *mem_ctx,
 
 errno_t get_userinfo(struct devicecode_ctx *dc_ctx);
 
+
+errno_t client_credentials_grant(struct rest_ctx *rest_ctx,
+                                 const char *token_endpoint,
+                                 const char *client_id,
+                                 const char *client_secret,
+                                 const char *scope);
+
+errno_t do_http_request(struct rest_ctx *rest_ctx, const char *uri,
+                        const char *post_data, const char *token);
+
+errno_t do_http_request_json_data(struct rest_ctx *rest_ctx, const char *uri,
+                                  const char *post_data, const char *token);
 
 /* oidc_child_json.c */
 errno_t parse_openid_configuration(struct devicecode_ctx *dc_ctx);
@@ -130,5 +172,13 @@ errno_t add_posix_to_json_string_array(TALLOC_CTX *mem_ctx,
                                        char domain_seperator,
                                        const char *in,
                                        char **out);
+
+/* oidc_child_id.c */
+errno_t oidc_get_id(TALLOC_CTX *mem_ctx, enum oidc_cmd oidc_cmd,
+                    char *idp_type,
+                    char *input, enum search_str_type input_type,
+                    bool libcurl_debug, const char *ca_db,
+                    const char *client_id, const char *client_secret,
+                    const char *token_endpoint, const char *scope, char **out);
 
 #endif /* __OIDC_CHILD_UTIL_H__ */
