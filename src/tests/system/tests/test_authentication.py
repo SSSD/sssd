922826c4a0e550cca8752a21a2896339bfbe7f77
Dan Lavu <dlavu@redhat.com>
2025-03-18 13:05:02 -0400

    tests: removed overlapping test scenarios from authentication tests
    * few scenarios have been removed
    * ppolicy tests have been made into ppolicy tests only, since normal ldap is covered by the generic provider now
    * renamed some of the test cases
    * removed su from a password change test
    * removed some test cases that are now covered by the new test cases

--- src/tests/system/tests/test_authentication.py
+++ src/tests/system/tests/test_authentication.py
@@ -30,11 +30,11 @@ def test_authentication__default_settings(
         1. Create user
         2. Start SSSD
     :steps:
-        1. Authenticate user with correct password
-        2. Authenticate user with incorrect password
+        1. Login as user
+        2. Login as user with bad password
     :expectedresults:
-        1. Authentication is successful
-        2. Authentication is unsuccessful
+        1. User can log in
+        2. User cannot log in
     :customerscenario: False
     """
     provider.user("user1").add(password="Secret123")
@@ -55,7 +55,7 @@ def test_authentication__change_password(
     provider: GenericProvider,
 ):
     """
-    :title: User logins and issues a password change
+    :title: User issues a password change after login
     :setup:
         1. Create user 'user'
         2. Start SSSD
@@ -67,8 +67,8 @@ def test_authentication__change_password(
         5. Login with new password
     :expectedresults:
         1. User is authenticated
-        2. Password is change is unsuccessful
-        3. Password change  is successful
+        2. Password change is unsuccessful
+        3. Password change is successful
         4. User cannot log in
         5. User can log in
     :customerscenario: True
@@ -98,7 +98,7 @@ def test_authentication__change_password_with_complexity_requirement(
     provider: GenericProvider,
 ):
     """
-    :title: Password change when the new passwords do not meet the complexity requirements
+    :title: User issues a password change after login with password policy complexity enabled
     :setup:
         1. Create user 'user'
         2. Enable password complexity requirements
@@ -111,8 +111,8 @@ def test_authentication__change_password_with_complexity_requirement(
         5. Login with new password
     :expectedresults:
         1. User is authenticated
-        2. Password is change is unsuccessful
-        3. Password change  is successful
+        2. Password change is unsuccessful
+        3. Password change is successful
         4. User cannot log in
         5. User can log in
     :customerscenario: True
@@ -204,14 +204,15 @@ def test_authentication__default_settings_when_the_provider_is_offline(
         2. Configure SSSD with "cache_credentials = true" and "krb5_store_password_if_offline = true" and
         "offline_credentials_expiration = 0"
         3 Start SSSD
-    :steps:
-        1. Authenticate user with correct password
-        2. Offline user authentication with correct password
+    :steps
+        1. Login as user
+        2. Offline, login as user
+        3. Offline, login as user with bad password
         3. Offline user authentication with incorrect password
     :expectedresults:
-        1. User authentication is successful
-        2. User authentication is successful
-        3. User authentication is unsuccessful
+        1. User can log in
+        2. User can log in
+        3. User cannot log in
     :customerscenario: False
     """
     user = "user1"
