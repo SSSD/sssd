fe8667290d93a5e3f26f5ed2dde0eddb4df6b328
krishnavema <krishnavemak@gmail.com>
2025-03-28 07:02:41 +0530

    my-first test

--- src/tests/system/tests/test_authentication.py
+++ src/tests/system/tests/test_authentication.py
@@ -155,3 +155,35 @@ def test_authentication__with_overriding_home_directory(client: Client, provider
         result = ssh.run("pwd").stdout
         assert result is not None, "Getting path failed!"
         assert result == home_exp, f"Current path {result} is not {home_exp}!"
+
+@pytest.mark.importance("medium")
+@pytest.mark.topology(KnownTopologyGroup.AnyProvider)
+def test_krishna():
+    pass
+
+@pytest.mark.topology(KnownTopologyGroup.AnyProvider)
+@pytest.mark.parametrize("method", ["su", "ssh"])
+@pytest.mark.parametrize("sssd_service_user", ("root", "sssd"))
+@pytest.mark.importance("critical")
+@pytest.mark.require(
+    lambda client, sssd_service_user: ((sssd_service_user == "root") or client.features["non-privileged"]),
+    "SSSD was built without support for running under non-root",
+)
+def test_authentication__with_default_settings_alternative(
+    client: Client, provider: GenericProvider, method: str, sssd_service_user: str
+):
+    user_credentials = {"username": "user1", "password": "Secret123", "wrong_password": "NOTSecret123"}
+    provider.user(user_credentials["username"]).add(password=user_credentials["password"])
+    
+    if method == "ssh" and "ssh" not in client.sssd.sssd["services"]:
+        client.sssd.sssd["services"] = "nss, pam, ssh"
+    
+    client.sssd.start(service_user=sssd_service_user)
+    
+    assert client.auth.parametrize(method).password(
+        user_credentials["username"], user_credentials["password"]
+    ), "User failed login!"
+    
+    assert not client.auth.parametrize(method).password(
+        user_credentials["username"], user_credentials["wrong_password"]
+    ), "User logged in with an invalid password!"
