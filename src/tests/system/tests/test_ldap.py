85bbd35f972018c07584e5c3f05c5c0676b83f9f
Dan Lavu <dlavu@redhat.com>
2025-04-08 09:55:09 -0400

    tests: removed overlapping test scenarios from authentication tests
    * few scenarios have been removed
    * ppolicy tests have been made into ppolicy tests only, since normal ldap is covered by the generic provider now
    * renamed some of the test cases
    * removed su from a password change test
    * removed some test cases that are now covered by the new test cases
    
    Amended-by: Dan Lavu <dan@redhat.com>
    * removed ppolicy tests since the feature has not been backported
    
    Reviewed-by: Alejandro López <allopez@redhat.com>
    Reviewed-by: Scott Poore <spoore@redhat.com>
    (cherry picked from commit f8f7f84)

--- src/tests/system/tests/test_ldap.py
+++ src/tests/system/tests/test_ldap.py
@@ -12,207 +12,71 @@ from sssd_test_framework.roles.ldap import LDAP
 from sssd_test_framework.topology import KnownTopology
 
 
-@pytest.mark.ticket(bz=[795044, 1695574])
-@pytest.mark.importance("critical")
-@pytest.mark.authentication
-@pytest.mark.parametrize("modify_mode", ["exop", "ldap_modify"])
-@pytest.mark.topology(KnownTopology.LDAP)
-def test_ldap__change_password(client: Client, ldap: LDAP, modify_mode: str):
-    """
-    :title: Change password with "ldap_pwmodify_mode" set to @modify_mode
-    :setup:
-        1. Add user to LDAP, set his password
-        2. Allow user to change his password
-        3. Set "ldap_pwmodify_mode"
-        4. Start SSSD
-    :steps:
-        1. Authenticate user with old password
-        2. Change password of user to new password
-        3. Authenticate user with new password
-        4. Authenticate user with old password
-    :expectedresults:
-        1. User is authenticated
-        2. Password is changed successfully
-        3. User is authenticated
-        4. User is not authenticated
-    :customerscenario: True
-    """
-    user = "user1"
-    old_pass = "Secret123"
-    new_pass = "New_password123"
-
-    ldap.user(user).add(password=old_pass)
-    ldap.aci.add('(targetattr="userpassword")(version 3.0; acl "pwp test"; allow (all) userdn="ldap:///self";)')
-
-    client.sssd.domain["ldap_pwmodify_mode"] = modify_mode
-    client.sssd.start()
-
-    assert client.auth.ssh.password(user, old_pass), "Authentication with old correct password failed"
-
-    assert client.auth.passwd.password(user, old_pass, new_pass), "Password change was not successful"
-
-    assert client.auth.ssh.password(user, new_pass), "Authentication with new correct password failed"
-    assert not client.auth.ssh.password(user, old_pass), "Authentication with old incorrect password did not fail"
-
-
-@pytest.mark.ticket(bz=[795044, 1695574])
-@pytest.mark.parametrize("modify_mode", ["exop", "ldap_modify"])
-@pytest.mark.topology(KnownTopology.LDAP)
-def test_ldap__change_password_new_pass_not_match(client: Client, ldap: LDAP, modify_mode: str):
-    """
-    :title: Change password with "ldap_pwmodify_mode" set to @modify_mode, but retyped password do not match
-    :setup:
-        1. Add user to LDAP, set his password
-        2. Allow user to change his password
-        3. Set "ldap_pwmodify_mode"
-        4. Start SSSD
-    :steps:
-        1. Change password to new password, but retyped password is different
-    :expectedresults:
-        1. Password change is not successful
-    :customerscenario: True
-    """
-    ldap.user("user1").add(password="Secret123")
-    ldap.aci.add('(targetattr="userpassword")(version 3.0; acl "pwp test"; allow (all) userdn="ldap:///self";)')
-
-    client.sssd.domain["ldap_pwmodify_mode"] = modify_mode
-    client.sssd.start()
-
-    assert not client.auth.passwd.password(
-        "user1", "Secret123", "Red123", "Hat000"
-    ), "Password changed successfully, which is not expected"
-
-
-@pytest.mark.ticket(bz=[795044, 1695574, 1795220])
-@pytest.mark.parametrize("modify_mode", ["exop", "ldap_modify"])
-@pytest.mark.topology(KnownTopology.LDAP)
-def test_ldap__change_password_lowercase(client: Client, ldap: LDAP, modify_mode: str):
-    """
-    :title: Change password to lower-case letters, password check fail
-    :setup:
-        1. Add user to LDAP, set his password
-        2. Allow user to change his password
-        3. Set "passwordCheckSyntax" to "on"
-        4. Set "ldap_pwmodify_mode"
-        5. Start SSSD
-    :steps:
-        1. Change password to new password, but all letters are lower-case
-        2. Check logs
-    :expectedresults:
-        1. Password change failed
-        2. Password change failure is logged
-    :customerscenario: True
-    """
-    ldap.user("user1").add(password="Secret123")
-    ldap.aci.add('(targetattr="userpassword")(version 3.0; acl "pwp test"; allow (all) userdn="ldap:///self";)')
-    ldap.ldap.modify("cn=config", replace={"passwordCheckSyntax": "on"})
-
-    client.sssd.domain["ldap_pwmodify_mode"] = modify_mode
-    client.sssd.start()
-
-    assert not client.auth.passwd.password(
-        "user1", "Secret123", "red_32"
-    ), "Password changed successfully, which is not expected"
-
-    assert (
-        "pam_sss(passwd:chauthtok): User info message: Password change failed."
-        in client.host.conn.run("journalctl").stdout
-    )
-
-
-@pytest.mark.ticket(bz=[1695574, 1795220])
-@pytest.mark.parametrize("modify_mode", ["exop", "ldap_modify"])
-@pytest.mark.topology(KnownTopology.LDAP)
-def test_ldap__change_password_wrong_current(client: Client, ldap: LDAP, modify_mode: str):
-    """
-    :title: Password change failed because an incorrect password was used
-    :setup:
-        1. Add user to LDAP, set his password
-        2. Allow user to change his password
-        3. Set "ldap_pwmodify_mode"
-        4. Start SSSD
-    :steps:
-        1. Change password to new password, but enter wrong password
-    :expectedresults:
-        1. Password change is not successful
-    :customerscenario: True
-    """
-    ldap.user("user1").add(password="Secret123")
-    ldap.aci.add('(targetattr="userpassword")(version 3.0; acl "pwp test"; allow (all) userdn="ldap:///self";)')
-
-    client.sssd.domain["ldap_pwmodify_mode"] = modify_mode
-    client.sssd.start()
-
-    assert not client.auth.passwd.password("user1", "wrong123", "Newpass123"), "Password change did not fail"
-
-
+@pytest.mark.importance("low")
 @pytest.mark.ticket(bz=[1067476, 1065534])
 @pytest.mark.topology(KnownTopology.LDAP)
-def test_ldap__user_with_whitespace(client: Client, ldap: LDAP):
+def test_ldap__user_login_with_whitespace_prefix_in_userid(client: Client, ldap: LDAP):
     """
-    :title: user with a whitespace at beginning is able to login and "id"
+    :title: Authenticate with a user containing a blank space in the userid
+    :description: This can only be tested on LDAP because most directories have
+    constraints on the values, protecting the integrity of the data. This scenario
+    is most likely created migrating and, or upgrading old databases.
     :setup:
-        1. Add users " space1" and "user1" to LDAP
-        2. Set uids and passwords to users
-        3. Clear memcache, logs and db
-        4. Start SSSD
+        1. Add users " space1" and "user1"
+        2. Start SSSD
     :steps:
-        1. Fetch user " space1" information using 'id'
-        2. Login user " space1" via ssh
-        3. Login user "space1" via ssh
-        4. Fetch "user1" user information using 'id'
-        5. Fetch " user1" user information using 'id'
+        1. Lookup user " space1"
+        2. Login user " space1"
+        3. Login user "space1"
+        4. Lookup "user1" user
+        5. Lookup " user1" user
     :expectedresults:
-        1. " space1" is fetched and has correct id
-        2. " space1" is able to login
-        3. "space1" is not able to login
-        4. "user1" is fetched and has correct id
-        5. " user1" is not fetched
+        1. " space1" is found and has correct id
+        2. " space1" is able to log in
+        3. "space1" is not able to log in
+        4. "user1" is found and has correct id
+        5. " user1" is not found
     :customerscenario: True
     """
     ldap.user(" space1").add(uid=10011, password="Secret123")
     ldap.user("user1").add(uid=10012, password="Secret123")
-    client.sssd.clear(db=True, memcache=True, logs=True)
     client.sssd.start()
 
     result = client.tools.id(" space1")
     assert result is not None, "User ' space1' was not found"
     assert result.user.id == 10011, "User ' space1' has wrong id"
 
-    assert client.auth.ssh.password(" space1", "Secret123"), "Authentication for user ' space1' failed"
-    assert not client.auth.ssh.password("space1", "Secret123"), "Authentication for user 'space1' did not fail"
+    assert client.auth.ssh.password(" space1", "Secret123"), "User ' space1' login failed!"
+    assert not client.auth.ssh.password("space1", "Secret123"), "User 'space1' login should have failed!"
 
     result = client.tools.id("user1")
-    assert result is not None, "User 'user1' was not found"
-    assert result.user.id == 10012, "User 'user1' has wrong id"
+    assert result is not None, "User 'user1' not found!"
+    assert result.user.id == 10012, "User 'user1' has the wrong uid!"
 
     result = client.tools.id(" user1")
-    assert result is None, "User ' user1' was found, not expected"
+    assert result is None, "User ' user1' should not be found!"
 
 
 @pytest.mark.importance("high")
-@pytest.mark.authentication
 @pytest.mark.ticket(bz=1507035)
 @pytest.mark.topology(KnownTopology.LDAP)
 @pytest.mark.parametrize("method", ["su", "ssh"])
-def test_ldap__password_change(client: Client, ldap: LDAP, method: str):
+def test_ldap__shadow_policy_user_login_then_changes_password(client: Client, ldap: LDAP, method: str):
     """
-    :title: Change password with shadow ldap password policy
+    :title: Change password with shadow ldap password policy is set to shadow
+    :description: Changing a password when the password policy is managed by the shadowAccount objectclass.
     :setup:
-        1. Allow user to change its own password in LDAP
-        2. Create LDAP user "tuser" with shadowLastChange = 0
-        3. Set ldap_pwd_policy to "shadow"
-        4. Set ldap_chpass_update_last_change to "True"
-        5. Start SSSD
+        1. Create user with shadowLastChange = 0, shadowMin = 0, shadowMax = 99999 and shadowWarning = 7
+        2. Set "ldap_pwd_policy = shadow" and "ldap_chpass_update_last_change = True"
+        3. Start SSSD
     :steps:
-        1. Autheticate as "tuser" with old password
-        2. Autheticate as "tuser" with new password
+        1. Authenticate as "tuser" with old password
+        2. Authenticate as "tuser" with new password
     :expectedresults:
-        1. Password was expired and new password was expected and provided
+        1. The password is expired, and the user is forced to change their password
         2. Authentication with new password was successful
     :customerscenario: True
     """
-    ldap.aci.add('(targetattr="userpassword")(version 3.0; acl "pwp test"; allow (all) userdn="ldap:///self";)')
     ldap.user("tuser").add(
         uid=999011, gid=999011, shadowMin=0, shadowMax=99999, shadowWarning=7, shadowLastChange=0, password="Secret123"
     )
@@ -221,8 +85,7 @@ def test_ldap__password_change(client: Client, ldap: LDAP, method: str):
     client.sssd.domain["ldap_chpass_update_last_change"] = "True"
     client.sssd.start()
 
-    # Password is expired, change it
-    assert client.auth.parametrize(method).password_expired("tuser", "Secret123", "Redhat@321")
-
-    # Authenticate with new password
-    assert client.auth.parametrize(method).password("tuser", "Redhat@321")
+    assert client.auth.parametrize(method).password_expired(
+        "tuser", "Secret123", "Redhat@321"
+    ), "Password change failed!"
+    assert client.auth.parametrize(method).password("tuser", "Redhat@321"), "User 'tuser' login failed!"
