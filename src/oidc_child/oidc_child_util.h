95f1a9c573112372d0dca6cfcc6ab1c08b0b804b
Sumit Bose <sbose@redhat>
2025-05-08 12:46:15 +0300

    oidc_child: change verify_token() to decode_token()
    Signed-off-by: Sumit Bose <sbose@redhat.com>
    
    Reviewed-by: Iker Pedrosa <ipedrosa@redhat.com>
    Reviewed-by: Scott Poore <spoore@redhat.com>
    Reviewed-by: Sumit Bose <sbose@redhat.com>

--- src/oidc_child/oidc_child_util.h
+++ src/oidc_child/oidc_child_util.h
@@ -91,7 +91,7 @@ errno_t parse_result(struct devicecode_ctx *dc_ctx);
 errno_t parse_token_result(struct devicecode_ctx *dc_ctx,
                            char **error_description);
 
-errno_t verify_token(struct devicecode_ctx *dc_ctx);
+errno_t decode_token(struct devicecode_ctx *dc_ctx, bool verify);
 
 const char *get_user_identifier(TALLOC_CTX *mem_ctx, json_t *userinfo,
                                 const char *user_identifier_attr,
