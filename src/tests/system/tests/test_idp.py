2f6c9b043768da1318b343259f2f481813d9a63f
Sumit Bose <sbose@redhat.com>
2025-06-02 09:33:34 +0200

    idp: add support and test for ignore_group_members option
    Reviewed-by: Alexey Tikhonov <atikhono@redhat.com>
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    Reviewed-by: Scott Poore <spoore@redhat.com>
    Reviewed-by: Tomáš Halman <thalman@redhat.com>

--- src/tests/system/tests/test_idp.py
+++ src/tests/system/tests/test_idp.py
@@ -127,3 +127,35 @@ def test_idp__group_members(client: Client, keycloak: Keycloak, use_fully_qualif
     out = client.host.conn.run(f"getent group group1{domain}")
     assert out.stdout.startswith(f"group1{domain}:*:")
     assert out.stdout.endswith(f":user1{domain}")
+
+
+@pytest.mark.parametrize("use_fully_qualified_names", ["true", "false"])
+@pytest.mark.topology(KnownTopology.Keycloak)
+@pytest.mark.builtwith(client="idp-provider")
+def test_idp__group_ignore_group_members(client: Client, keycloak: Keycloak, use_fully_qualified_names: str):
+    """
+    :title: Authenticate with default settings
+    :setup:
+        1. Create user
+        2. Create group with user as member
+        3. Add option 'ignore_group_members = true'
+    :steps:
+        1. Lookup group with 'getent group'
+    :expectedresults:
+        1. No members shown
+    :customerscenario: False
+    """
+
+    user = keycloak.user("user1").add(password="Secret123")
+    keycloak.group("group1").add().add_member(user)
+
+    client.sssd.domain["ignore_group_members"] = "true"
+    client.sssd.domain["use_fully_qualified_names"] = use_fully_qualified_names
+
+    domain = f"@{client.sssd.default_domain}" if use_fully_qualified_names == "true" else ""
+
+    client.sssd.start(check_config=False)
+
+    out = client.host.conn.run(f"getent group group1{domain}")
+    assert out.stdout.startswith(f"group1{domain}:*:")
+    assert out.stdout.endswith(":")
