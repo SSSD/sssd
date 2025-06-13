9c63baf99389a075809e2ead01950b752c5616ea
Sumit Bose <sbose@redhat.com>
2025-06-13 09:43:22 +0200

    CONFLICT! tests: lookup user with overrides with email
    Reviewed-by: Alexey Tikhonov <atikhono@redhat.com>
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    (cherry picked from commit 6d8f9d7e9c8858fc108bf0e5e4fc40fcdec7ed71)
    (cherry picked from commit f5f4591a3833b1952216f175207dfe4a4b66a915)

--- src/tests/system/tests/test_sss_override.py
+++ src/tests/system/tests/test_sss_override.py
@@ -147,6 +147,146 @@ def test_sss_overrides__root_uid_gid_cannot_be_used(client: Client, provider: Ge
     client.sssd.domain["ldap_id_mapping"] = "False"
     client.sssd.start()
 
+<<<<<<< HEAD
+=======
+    result = client.tools.getent.passwd("user1")
+    assert result is not None, "User 'user1' not found!"
+    assert result.uid == 999999, f"User's uid {result.uid} does not match override value!"
+    assert result.gid == 888888, f"User's gid {result.gid} does not match override value!"
+    assert result.home == "/home/o-user1", "User's homedir does not match override value!"
+
+    result = client.tools.getent.passwd("o-user1")
+    assert result is not None, "User 'o-user1' not found by override name!"
+    assert result.uid == 999999, f"Local override uid {result.uid} does not match override value!"
+    assert result.gid == 888888, f"Local override gid {result.gid} does not match override value!"
+    assert result.home == "/home/o-user1", "User's override name homedir does not match override value!"
+
+
+@pytest.mark.importance("high")
+@pytest.mark.topology([KnownTopology.LDAP, KnownTopology.AD, KnownTopology.Samba])
+def test_sss_overrides__overriding_username_and_attributes_lookup_by_email(client: Client, provider: GenericProvider):
+    """
+    :title: Locally overriding the name and POSIX attributes of a user and lookup with the email address
+    :setup:
+        1. Create POSIX user "user1" with email "email@example.com", email is
+           used because the UPN is not supported in all roles of the framework
+        2. Configure SSSD with "ldap_id_mapping = false" and start SSSD
+        3. Create local override for "user1"
+        4. Restart SSSD, this is necessary to enable local overrides
+    :steps:
+        1. Lookup user by the original name, check the uid and gid
+        2. Lookup user by the overridden name, check the uid and gid
+        3. Lookup user by email, check the uid and gid
+    :expectedresults:
+        1. User is found and uid and gid match new values
+        2. User is found and uid and gid match new values
+        3. User is found and uid and gid match new values
+    :customerscenario: True
+    """
+    provider.user("user1").add(
+        uid=999011,
+        gid=999011,
+        home="/home/user1",
+        gecos="user",
+        shell="/bin/bash",
+        password="Secret123",
+        email="email@example.com",
+    )
+
+    client.sssd.domain["ldap_id_mapping"] = "False"
+    client.sssd.start()
+
+    client.sss_override.user("user1").add(name="o-user1", uid=999999, gid=888888, home="/home/o-user1")
+
+    client.sssd.restart()
+
+    result = client.tools.getent.passwd("user1")
+    assert result is not None, "User not found!"
+    assert result.uid == 999999, "User's uid does not match override value!"
+    assert result.gid == 888888, "User's gid does not match override value!"
+    assert result.home == "/home/o-user1", "User's homedir does not match override value!"
+
+    result = client.tools.getent.passwd("o-user1")
+    assert result is not None, "User not found by override name!"
+    assert result.uid == 999999, "Local override uid does not match override value!"
+    assert result.gid == 888888, "Local override gid does not match override value!"
+    assert result.home == "/home/o-user1", "User's override name homedir does not match override value!"
+
+    result = client.tools.getent.passwd("email@example.com")
+    assert result is not None, "User not found by email!"
+    assert result.uid == 999999, "Local override uid does not match override value!"
+    assert result.gid == 888888, "Local override gid does not match override value!"
+    assert result.home == "/home/o-user1", "User's override name homedir does not match override value!"
+
+
+@pytest.mark.importance("high")
+@pytest.mark.topology([KnownTopology.LDAP, KnownTopology.AD, KnownTopology.Samba])
+@pytest.mark.preferred_topology(KnownTopology.LDAP)
+def test_sss_override__group_attributes_and_members(client: Client, provider: GenericProvider):
+    """
+    :title: Override group attributes and members
+    :setup:
+        1. Create user  and group, make the user a group member
+        2. Configure SSSD with "ldap_id_mapping = false" and start SSSD
+        3. Create local user and group override and restart SSSD
+    :steps:
+        1. Lookup group name and overridden name, check gid and group members
+        2. Update group gid value for override, check gid and group members
+    :expectedresults:
+        1. Group is found by both names, gid matches override and user is a member
+        2. Group is found by both names, gid matches new override value and user is a member
+    :customerscenario: False
+    """
+    user = provider.user("user1").add(uid=777777, gid=777777)
+    provider.group("group1").add(gid=999999).add_member(user)
+    client.sssd.domain["ldap_id_mapping"] = "False"
+    client.sssd.start()
+
+    client.sss_override.user("user1").add(name="o-user1")
+    client.sss_override.group("group1").add(name="o-group1")
+    client.sssd.restart()
+
+    for i in ["group1", "o-group1"]:
+        result = client.tools.getent.group(i)
+        assert result is not None, f"Group {i} not found!"
+        assert result.gid == 999999, f"Group gid {result.gid} does not match original value!"
+        assert "o-user1" in result.members, "Local override user 'o-user1' not found in group!"
+
+    client.sss_override.group("group1").add(name="o-group1", gid=888888)
+    for i in ["group1", "o-group1"]:
+        result = client.tools.getent.group(i)
+        assert result is not None, f"Group not {i} found!"
+        assert result.gid == 888888, f"Group gid {result.gid} does not match override value!"
+        assert "o-user1" in result.members, "Local override username 'o-user1' not found in group!"
+
+
+@pytest.mark.importance("high")
+@pytest.mark.topology([KnownTopology.LDAP, KnownTopology.AD, KnownTopology.Samba])
+@pytest.mark.preferred_topology(KnownTopology.LDAP)
+def test_sss_override__root_user_cannot_be_used(client: Client, provider: GenericProvider):
+    """
+    :title: Root user cannot be used for overrides
+    :setup:
+        1. Create users, one named root
+        2. Configure SSSD with "ldap_id_mapping = false", "use_fully_qualified_names  = False" and start SSSD
+        3. Override username to root and set override attributes to root values
+        4. Restart SSSD
+    :steps:
+        1. Lookup the root user and check his uid and gid
+        2. Lookup the root user and use the sss service
+        3. Lookup user and check his uid and gid
+    :expectedresults:
+        1. The root user uid and gid has not been modified
+        2. root user is not found
+        3. User found and uid and gid is not roots
+    :customerscenario: False
+    """
+    provider.user("user1").add(uid=999011, gid=999011)
+    provider.user("root").add(uid=999012, gid=999012)
+    client.sssd.domain["ldap_id_mapping"] = "False"
+    client.sssd.domain["use_fully_qualified_names"] = "False"
+    client.sssd.start()
+>>>>>>> f5f4591a3 (tests: lookup user with overrides with email)
     client.sss_override.user("user1").add(name="root", uid=0, gid=0)
 
     client.sssd.restart()
