8b780e179d8c8de9cfc2c2c2f29d3ba7b2f0a07f
Scott Poore <spoore@redhat.com>
2025-07-30 10:17:08 -0500

    Test: missing KnownTopology import
    test_access_control_simple.py is missing an import for KnownTopology for
    the last test case that was added later.   Adding the import here to
    resolve test failures.

--- src/tests/system/tests/test_access_control_simple.py
+++ src/tests/system/tests/test_access_control_simple.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 import pytest
 from sssd_test_framework.roles.client import Client
 from sssd_test_framework.roles.generic import GenericProvider
-from sssd_test_framework.topology import KnownTopologyGroup
+from sssd_test_framework.topology import KnownTopology, KnownTopologyGroup
 
 
 @pytest.mark.topology(KnownTopologyGroup.AnyProvider)
