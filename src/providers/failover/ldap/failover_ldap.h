bf6c086c63fc2821aa7e504cd019598f2e1b1b52
Pavel Březina <pbrezina@redhat.com>
2025-10-01 14:30:40 +0200

    failover: implement new failover interface
    This crafts and implements the new failover interface,
    it does not provide complete implementation of the failover
    mechanism yet. It brings the code to a state were the public
    and private interfaces are stable, working and testable so
    the following tasks can be split and work on in parallel.
    
    What is missing at this state:
    - server configuration and discovery
      (failover_server_group/batch/vtable_op)
    - server selection mechanism (sss_failover_vtable_op_server_next)
    - kerberos authentication
    - sharing servers between IPA/AD LDAP and KDC
    - online/offline callbacks (resolve callback should not be needed)
    
    But especially it is possible to start refactoring SSSD code to start
    using the new failover implementation.

---
+++ src/providers/failover/ldap/failover_ldap.h
@@ -0,0 +1,66 @@
+/*
+    Copyright (C) 2025 Red Hat
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _FAILOVER_LDAP_H_
+#define _FAILOVER_LDAP_H_
+
+#include <tevent.h>
+#include <talloc.h>
+
+#include "config.h"
+#include "resolv/async_resolv.h"
+#include "providers/failover/failover_server.h"
+#include "util/util.h"
+
+struct sss_failover_ldap_connection {
+    struct sdap_server_opts *srv_opts;
+    struct sdap_handle *sh;
+    char *uri;
+};
+
+struct tevent_req *
+sss_failover_ldap_kinit_send(TALLOC_CTX *mem_ctx,
+                             struct tevent_context *ev,
+                             struct sss_failover_ctx *fctx,
+                             struct sss_failover_server *server,
+                             bool addr_changed,
+                             void *pvt);
+
+errno_t
+sss_failover_ldap_kinit_recv(TALLOC_CTX *mem_ctx,
+                             struct tevent_req *req,
+                             time_t *_expiration_time);
+
+struct tevent_req *
+sss_failover_ldap_connect_send(TALLOC_CTX *mem_ctx,
+                               struct tevent_context *ev,
+                               struct sss_failover_ctx *fctx,
+                               struct sss_failover_server *server,
+                               bool addr_changed,
+                               bool reuse_connection,
+                               bool authenticate_connection,
+                               bool read_rootdse,
+                               enum sss_failover_transaction_tls force_tls,
+                               time_t kinit_expiration_time,
+                               void *pvt);
+
+errno_t
+sss_failover_ldap_connect_recv(TALLOC_CTX *mem_ctx,
+                               struct tevent_req *req,
+                               void **_connection);
+
+#endif /* _FAILOVER_LDAP_H_ */
