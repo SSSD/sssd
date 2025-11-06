101c2615eccff5319acb6d07a2c7928ba8176e3f
Alexey Tikhonov <atikhono@redhat.com>
2025-11-06 15:43:48 +0100

    Get rid of useless `SSSD_MAIN_OPTS` define.
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    Reviewed-by: Tomáš Halman <thalman@redhat.com>

--- src/tests/util-tests.c
+++ src/tests/util-tests.c
@@ -1231,7 +1231,7 @@ int main(int argc, const char *argv[])
 
     struct poptOption long_options[] = {
         POPT_AUTOHELP
-        SSSD_MAIN_OPTS
+        SSSD_DEBUG_OPTS
         POPT_TABLEEND
     };
 
