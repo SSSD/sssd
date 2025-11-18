1a1df6e2f037a00f837944a941297dc08744a9e2
Dan Lavu <dlavu@redhat.com>
2025-11-18 12:54:44 -0500

    adding subid test

--- src/tests/system/tests/test_ipa.py
+++ src/tests/system/tests/test_ipa.py
@@ -640,3 +640,45 @@ def test_ipa__switch_user_with_smartcard_authentication(client: Client, ipa: IPA
     assert (
         "ipacertuser1" in result.stdout
     ), f"'ipacertuser1' not found in 'whoami' output! Stdout content: {result.stdout}"
+
+
+@pytest.mark.importance("high")
+@pytest.mark.topology(KnownTopology.IPA)
+def test_ipa__subids_configured(client: Client, ipa: IPA):
+    """
+    :title: SSSD can read subid ranges configured in IPA
+    :setup:
+        1. Create a user with generated subids and one without
+        2. Configure and start SSSD with subids
+    :steps:
+        1. Lookup the uid start range and range size for all users
+        2. Lookup the gid start range and range size for all users
+    :expectedresults:
+        1. The values from the client matches the server values  when the user has subids
+        2. The values from the client matches the server values when the user has subids
+    :customerscenario: False
+    """
+    user = ipa.user("user1").add()
+    ipa.user("user2").add()
+    ipa_sub = user.subid().generate()
+
+    client.sssd.common.subid()
+    client.sssd.start()
+
+    subuid = client.tools.getsubid("user1")
+    assert (
+        ipa_sub.uid_start == subuid.range_start
+    ), f"Subordinate UID start range {subuid.range_start} does not match: {ipa_sub.uid_start}!"
+    assert (
+        ipa_sub.uid_size == subuid.range_size
+    ), f"Subordinate UID start range {subuid.range_size} does not match: {ipa_sub.uid_size}!"
+    assert client.tools.getsubid("user2") is None, "User has subids configured!"
+
+    subgid = client.tools.getsubid("user1", group=True)
+    assert (
+        ipa_sub.gid_start == subgid.range_start
+    ), f"Subordinate GID start range {subgid.range_start} does not match: {ipa_sub.gid_start}!"
+    assert (
+        ipa_sub.gid_size == subgid.range_size
+    ), f"Subordinate GID start range {subgid.range_size} does not match: {ipa_sub.gid_size}!"
+    assert client.tools.getsubid("user2", group=True) is None, "User has subids configured!"
