a77ceafcea80c8a54474eb87621613f63b36bb8d
Pavel Březina <pbrezina@redhat.com>
2025-12-18 13:30:13 +0100

    DO NOT PUSH TO MASTER minimal: push init_ctx instead of id_ctx so we can easily pass new fctx

--- src/providers/minimal/minimal.h
+++ src/providers/minimal/minimal.h
@@ -23,29 +23,17 @@
 */
 
 
-#ifndef _MINIMAL_ID_H_
-#define _MINIMAL_ID_H_
+#ifndef _MINIMAL_H_
+#define _MINIMAL_H_
 
 #include "config.h"
 #include <stdbool.h>
 
-#include "providers/backend.h"
 #include "providers/ldap/ldap_common.h"
-#include "util/util.h"
 
-struct minimal_id_ctx {
-    struct be_ctx *be_ctx;
-    struct minimal_init_ctx *init_ctx;
-    struct dp_option *minimal_options;
+struct minimal_init_ctx {
+    struct sdap_options *options;
+    struct sdap_id_ctx *id_ctx;
 };
 
-struct tevent_req *
-minimal_account_info_handler_send(TALLOC_CTX *mem_ctx,
-                                  struct minimal_init_ctx *init_ctx,
-                                  struct dp_id_data *data,
-                                  struct dp_req_params *params);
-
-errno_t minimal_account_info_handler_recv(TALLOC_CTX *mem_ctx,
-                                      struct tevent_req *req,
-                                      struct dp_reply_std *data);
 #endif
