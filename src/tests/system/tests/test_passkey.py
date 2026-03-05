6e0a80cb8f4f452fc38d45f5149896dd3e5f8771
Iker Pedrosa <ipedrosa@redhat.com>
2026-03-05 13:05:23 +0100

    tests: vfido for `test_passkey__su_fips_fido_key()`
    Transform `test_passkey__su_fips_fido_key()` to use vfido instead of
    umockdev
    
    Signed-off-by: Iker Pedrosa <ipedrosa@redhat.com>

--- src/tests/system/tests/test_passkey.py
+++ src/tests/system/tests/test_passkey.py
@@ -616,38 +616,36 @@ def test_passkey__su_user_when_add_with_ssh_key_and_mapping(
 
 @pytest.mark.importance("critical")
 @pytest.mark.topology(KnownTopologyGroup.AnyProvider)
-@pytest.mark.builtwith(client=["passkey", "umockdev"], provider="passkey")
-def test_passkey__su_fips_fido_key(client: Client, provider: GenericProvider, moduledatadir: str, testdatadir: str):
+@pytest.mark.builtwith(client=["passkey", "vfido"], provider="passkey")
+def test_passkey__su_fips_fido_key(client: Client, provider: GenericProvider):
     """
     :title: Check su authentication of user with LDAP, IPA, AD and Samba with FIPS Fido key
     :setup:
-        1. Add a user in LDAP, IPA, AD and Samba with passkey_mapping.
-        2. Setup SSSD client with FIDO and umockdev, start SSSD service.
+        1. Configure and start virtual passkey service
+        2. Add a user in LDAP, IPA, AD and Samba with passkey_mapping.
+        3. Configure and start SSSD service.
     :steps:
         1. Check su authentication of the user.
     :expectedresults:
         1. User su authenticates successfully.
     :customerscenario: False
     """
-    suffix = type(provider).__name__.lower()
+    client.vfido.reset()
+    client.vfido.pin_enable()
+    client.vfido.pin_set(123456)
+    client.vfido.start()
 
+    user = provider.user("user1").add()
+    mapping = client.sssctl.passkey_register(username="user1", domain=provider.domain, pin=123456, virt_type="vfido")
+    user.passkey_add(mapping)
+
+    client.sssd.import_domain(provider.domain, provider)
+    client.sssd.config.remove_section("domain/test")
+    client.sssd.default_domain = provider.domain
     client.sssd.domain["local_auth_policy"] = "enable:passkey"
-
-    # Recording files are created in FIPS enabled host with
-    # FIPS Fido key.
-
-    with open(f"{testdatadir}/passkey-mapping.{suffix}") as f:
-        provider.user("user1").add().passkey_add(f.read().strip())
-
     client.sssd.start(service_user="root")
 
-    assert client.auth.su.passkey(
-        username="user1",
-        pin=123456,
-        device=f"{moduledatadir}/umockdev.device",
-        ioctl=f"{moduledatadir}/umockdev.ioctl",
-        script=f"{testdatadir}/umockdev.script.{suffix}",
-    )
+    assert client.auth.su.passkey(username="user1", pin=123456, virt_type="vfido")
 
 
 @pytest.mark.importance("critical")
