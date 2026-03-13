655cb5b06b8ec48956c863f647d6eca4e6381477
Justin Stephenson <jstephen@redhat.com>
2026-03-13 14:27:30 -0400

    dp: Remove sdap_ret and sdap_error
    Assisted by: Claude code (Sonnet 4.5)

--- src/providers/ldap/sdap_async_initgroups.c
+++ src/providers/ldap/sdap_async_initgroups.c
@@ -3442,7 +3442,7 @@ static void sdap_get_initgr_pgid(struct tevent_req *subreq)
             tevent_req_callback_data(subreq, struct tevent_req);
     errno_t ret;
 
-    ret = groups_get_recv(subreq, NULL, NULL);
+    ret = groups_get_recv(subreq, NULL);
     talloc_zfree(subreq);
     if (ret != EOK) {
         tevent_req_error(req, ret);
