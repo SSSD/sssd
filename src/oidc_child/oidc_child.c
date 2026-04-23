f94e8f0a9bb4effccc93a076b1bed76c9611b330
Matthias Meidinger <matthias.meidinger@stackit.cloud>
2026-04-23 08:28:05 +0000

    fix updated function signature

--- src/oidc_child/oidc_child.c
+++ src/oidc_child/oidc_child.c
@@ -811,7 +811,7 @@ int main(int argc, const char *argv[])
     }
 
     if (opts.oidc_cmd == GET_DEVICE_CODE || opts.oidc_cmd == GET_ACCESS_TOKEN) {
-        ret = parse_result(dc_ctx);
+        ret = parse_result(dc_ctx, opts.idp_type);
         if (ret != EOK) {
             DEBUG(SSSDBG_OP_FAILURE, "Failed to parse device code reply.\n");
             goto done;
