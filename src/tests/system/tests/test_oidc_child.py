d217bba4613e381d75a8198305f063487eefe59f
Sumit Bose <sbose@redhat.com>
2026-05-15 10:08:01 +0200

    test: add tests for oidc_child 'get-device-code'
    This new test call oidc_child with the '--get-device-code' option with
    different client authentication methods.
    
    Reviewed-by: Alexey Tikhonov <atikhono@redhat.com>
    Reviewed-by: Pavel Březina <pbrezina@redhat.com>
    (cherry picked from commit 9a3487d757d9678b4474f53f76d9bf1832aa0083)

--- src/tests/system/tests/test_oidc_child.py
+++ src/tests/system/tests/test_oidc_child.py
@@ -21,6 +21,11 @@ args = (
     "--client-id=myclient --client-secret=ClientSecret123 --scope='profile'"
 )
 
+args_get_device_code = (
+    "--libcurl-debug -d 9 --logger=stderr "
+    "--get-device-code --issuer-url=https://master.keycloak.test:8443/auth/realms/master"
+)
+
 
 @pytest.mark.importance("critical")
 @pytest.mark.topology(KnownTopology.Keycloak)
@@ -112,3 +117,155 @@ def test_oidc_child__get_group_members(client: Client, keycloak: Keycloak):
     data = json.loads(out.stdout)
     assert data[0]["posixUsername"] == "user1"
     assert data[0]["posixObjectType"] == "user"
+
+
+@pytest.mark.importance("high")
+@pytest.mark.topology(KnownTopology.Keycloak)
+def test_oidc_child__get_device_code(client: Client, keycloak: Keycloak):
+    """
+    :title: Authenticate with default settings
+    :setup:
+        1. no specific setup needed
+    :steps:
+        1. Request device code with oidc_child
+    :expectedresults:
+        1. oidc_child is successful and device code and other data is returned
+    :customerscenario: False
+    """
+
+    out = client.host.conn.run(
+        oidc_child_path + " " + args_get_device_code + " " + "--client-id=myclient --client-secret=ClientSecret123"
+    )
+    data = json.loads(out.stdout_lines[0])
+    assert "device_code" in data, "Missing device_code!"
+    assert "expires_in" in data, "Missing expires_in!"
+    assert "interval" in data, "Missing interval!"
+
+    assert out.stdout_lines[1][:8] == "oauth2 {", "Second line does not start with 'oauth2 {'!"
+    data = json.loads(out.stdout_lines[1][7:])
+    assert "verification_uri" in data, "Missing verification_uri!"
+    assert "user_code" in data, "Missing user_code"
+
+
+@pytest.mark.parametrize("key_type", ["RSA", "EC"])
+@pytest.mark.importance("high")
+@pytest.mark.topology(KnownTopology.Keycloak)
+def test_oidc_child__get_device_code_jwt(client: Client, keycloak: Keycloak, key_type: str):
+    """
+    :title: Authenticate with default settings
+    :setup:
+        1. Create certificate, key and PKCS#12 file
+        2. Create JWT client in Keycloak with certificate
+    :steps:
+        1. Request device code with oidc_child with JWT client authentication
+    :expectedresults:
+        1. oidc_child is successful and device code and other data is returned
+    :customerscenario: False
+    """
+
+    p12_pwd = "Secret123"
+    p12_path = "/tmp/my.p12"
+
+    key, cert = client.smartcard.generate_cert(key_type=key_type)
+
+    client.host.conn.run(f"openssl pkcs12 -export -password pass:{p12_pwd} -inkey {key} -in {cert}  -out {p12_path}")
+    out = client.host.conn.run(f"openssl x509 -in {cert} -outform der | openssl base64 -A")
+    cert_b64 = out.stdout
+
+    # Create an IdP JWT client
+    keycloak.host.kclogin()
+    keycloak.host.conn.run(
+        "/opt/keycloak/bin/kcadm.sh create clients -r master "
+        '-b \'{"clientId": "my_jwt_client", "clientAuthenticatorType": "client-jwt", '
+        '"serviceAccountsEnabled": true, '
+        '"attributes": {"oauth2.device.authorization.grant.enabled": "true", '
+        f'"jwt.credential.certificate": "{cert_b64}"}}}}\' '
+    )
+
+    out = client.host.conn.run(
+        oidc_child_path
+        + " "
+        + args_get_device_code
+        + " "
+        + f"--client-id=my_jwt_client --client-secret={p12_pwd} --pkcs12-client-creds={p12_path} "
+        + "--client-auth-method=jwt"
+    )
+    data = json.loads(out.stdout_lines[0])
+    assert "device_code" in data, "Missing device_code!"
+    assert "expires_in" in data, "Missing expires_in!"
+    assert "interval" in data, "Missing interval!"
+
+    assert out.stdout_lines[1][:8] == "oauth2 {", "Second line does not start with 'oauth2 {'!"
+    data = json.loads(out.stdout_lines[1][7:])
+    assert "verification_uri" in data, "Missing verification_uri!"
+    assert "user_code" in data, "Missing user_code"
+
+
+@pytest.mark.parametrize("key_type", ["RSA", "EC"])
+@pytest.mark.importance("high")
+@pytest.mark.topology(KnownTopology.Keycloak)
+def test_oidc_child__get_device_code_mtls(client: Client, keycloak: Keycloak, key_type: str):
+    """
+    :title: Authenticate with default settings
+    :setup:
+        1. Create certificate, key and PKCS#12 file
+        2. Let keycloak trust the client certificate
+        3. Enable HTTPS client authentication in Keycloak
+        4. Create MTLS client in Keycloak with the subject DN of the certificate
+    :steps:
+        1. Request device code with oidc_child with MTLS client authentication
+    :expectedresults:
+        1. oidc_child is successful and device code and other data is returned
+    :customerscenario: False
+    """
+
+    p12_pwd = "Secret123"
+    p12_path = "/tmp/my.p12"
+
+    key, cert = client.smartcard.generate_cert(key_type=key_type)
+
+    client.host.conn.run(f"openssl pkcs12 -export -password pass:{p12_pwd} -inkey {key} -in {cert}  -out {p12_path}")
+    out = client.host.conn.run(f"openssl x509 -in {cert} -noout -subject")
+    assert out.stdout[:8] == "subject=", "Unexpected output!"
+    subject_dn = out.stdout[8:]
+    cert_content = client.host.fs.read(cert)
+
+    # Add client certificate to Keycloak's kestore
+    keycloak.fs.write(cert, cert_content)
+    keycloak.host.conn.run(
+        "keytool -storepass Secret123 -keystore /var/data/certs/master.keycloak.test.keystore -noprompt "
+        + f"-importcert -file {cert} -alias mtls"
+    )
+    keycloak.svc.restart("keycloak.service")
+
+    # Create an IdP MTLS client
+    keycloak.host.kclogin()
+    keycloak.host.conn.run(
+        "/opt/keycloak/bin/kcadm.sh create clients -r master "
+        '-b \'{"clientId": "my_mtls_client", "clientAuthenticatorType": "client-x509", '
+        '"serviceAccountsEnabled": true, '
+        '"attributes": {"oauth2.device.authorization.grant.enabled": "true", '
+        f'"x509.subjectdn": "{subject_dn}"}}}}\' '
+    )
+
+    out = client.host.conn.run(
+        oidc_child_path
+        + " "
+        + args_get_device_code
+        + " "
+        + f"--client-id=my_mtls_client --client-secret={p12_pwd} --pkcs12-client-creds={p12_path} "
+        + "--client-auth-method=mtls"
+    )
+    # Currently the keystore is not restored by the framework
+    keycloak.host.conn.run(
+        "keytool -storepass Secret123 -keystore /var/data/certs/master.keycloak.test.keystore -delete -alias mtls"
+    )
+    data = json.loads(out.stdout_lines[0])
+    assert "device_code" in data, "Missing device_code!"
+    assert "expires_in" in data, "Missing expires_in!"
+    assert "interval" in data, "Missing interval!"
+
+    assert out.stdout_lines[1][:8] == "oauth2 {", "Second line does not start with 'oauth2 {'!"
+    data = json.loads(out.stdout_lines[1][7:])
+    assert "verification_uri" in data, "Missing verification_uri!"
+    assert "user_code" in data, "Missing user_code"
