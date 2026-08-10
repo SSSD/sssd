bcbd7d6d4fd8ed89e5509c9bcd684c27c2dd5e2d
Justin Stephenson <jstephen@redhat.com>
2026-08-10 14:36:26 -0400

    ad: FIX AI finding unused `ret` variable
    `ret = EOK;` is set but never used before `tevent_req_done(req)`.

--- src/providers/ad/ad_id.c
+++ src/providers/ad/ad_id.c
@@ -885,7 +885,6 @@ static void ad_get_account_domain_evaluate(struct tevent_req *req)
           "Found object in domain %s\n", obj_dom->name);
     state->found_domain_name = obj_dom->name;
 
-    ret = EOK;
     tevent_req_done(req);
 }
 
